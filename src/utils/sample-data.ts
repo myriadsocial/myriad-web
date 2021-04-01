e996b0f725c9709b31de975ef6269b0c193254d7
mucktar <mucktar.pakpahan@gmail.com>
2021-04-01 22:39:30 +0700

    remove unused file

--- src/utils/sample-data.ts
+++
@@ -1,9 +0,0 @@
-import { User } from '../interfaces';
-
-/** Dummy user data. */
-export const sampleUserData: User[] = [
-  { name: 'Alice', avatar: 'A' },
-  { name: 'Bob', avatar: 'B' },
-  { name: 'Caroline', avatar: 'C' },
-  { name: 'Dave', avatar: 'D' }
-];
