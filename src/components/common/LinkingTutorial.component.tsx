ff5098447a1ab979ac682d708b10f7ddf72ea625
ical10 <mochhusnir@gmail.com>
2021-04-05 12:50:51 +0700

    build ok for MYR-102

--- src/components/common/LinkingTutorial.component.tsx
+++ src/components/common/LinkingTutorial.component.tsx
@@ -31,7 +31,7 @@ const LinkingTutorialComponent = forwardRef((_, ref) => {
           So what to do now?
         </DialogTitle>
         <DialogContent dividers>
-          <Typography left>
+          <Typography>
             Stay calm.
             <br /> We will soon provide tutorials on how you can link your social media account(s),
             <br /> so that you can focus on enjoying Myriad instead. <br />
