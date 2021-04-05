1f6f4e82fb75dc5ee49e09e70cdb799436f339b8
mhusnirizal <mochhusnir@gmail.com>
2021-04-05 12:49:13 +0700

    Revert "MYR-102 Show lorem ipsum when user click tell me more on import"

--- src/components/connect/connect.component.tsx
+++ src/components/connect/connect.component.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useMemo, useCallback } from 'react';
+import React, { useMemo, useCallback } from 'react';
 import { FacebookShareButton, RedditShareButton, TwitterShareButton } from 'react-share';
 
 import { User } from 'next-auth';
@@ -20,7 +20,6 @@ import TwitterIcon from '@material-ui/icons/Twitter';
 
 import { SocialsEnum } from '../../interfaces';
 import DialogTitle from '../common/DialogTitle.component';
-import LinkingTutorialComponent from '../common/LinkingTutorial.component';
 import ShowIf from '../common/show-if.component';
 
 import { WithAdditionalParams } from 'next-auth/_utils';
@@ -76,7 +75,6 @@ const copy: Record<SocialsEnum, string> = {
 
 export default function ConnectComponent({ user, social, open, handleClose }: Props) {
   const classes = useStyles();
-  const childRef = useRef<any>();
   const { shared, shareOnTwitter } = useShareSocial();
 
   const share = useCallback(() => {
@@ -106,69 +104,57 @@ export default function ConnectComponent({ user, social, open, handleClose }: Pr
   const message = 'Saying hi to #MyriadNetwork\n\nPublic Key: 13N2NpDg6kU1vAGuPv9MkTj4YsaDmf7BKyr3TTxhV5sFmuhd';
 
   return (
-    <>
-      <Dialog open={open} onClose={handleClose} maxWidth="xs">
-        <DialogTitle id="connect-social" onClose={handleClose}>
+    <Dialog open={open} onClose={handleClose} maxWidth="xs">
+      <DialogTitle id="connect-social" onClose={handleClose}>
+        {' '}
+        Link Your {social} Account
+      </DialogTitle>
+      <DialogContent dividers>
+        <Card className={classes.card}>
+          <CardHeader avatar={config.step1} title={config.copyTitle} />
+          <CardContent>
+            <TextField className={classes.dark} multiline variant="outlined" rows={6} fullWidth={true} value={message} />
+          </CardContent>
+        </Card>
+        <Card className={classes.card}>
+          <CardHeader avatar={config.step2} title={config.shareTitle} />
+          <CardContent className={classes.share}>
+            <ShowIf condition={social === SocialsEnum.FACEBOOK}>
+              <FacebookShareButton url="https://myriad-fe.herokuapp.com" quote={message}>
+                <Button variant="contained" size="large" onClick={share} startIcon={<FacebookIcon />} className={classes.facebook}>
+                  Share
+                </Button>
+              </FacebookShareButton>
+            </ShowIf>
+
+            <ShowIf condition={social === SocialsEnum.TWITTER}>
+              <TwitterShareButton url="https://myriad-fe.herokuapp.com" title={message}>
+                <Button variant="contained" size="large" onClick={share} startIcon={<TwitterIcon />} className={classes.twitter}>
+                  Share
+                </Button>
+              </TwitterShareButton>
+            </ShowIf>
+
+            <ShowIf condition={social === SocialsEnum.REDDIT}>
+              <RedditShareButton url="https://myriad-fe.herokuapp.com" title={message}>
+                <Button variant="contained" size="large" onClick={share} startIcon={<RedditIcon />} className={classes.reddit}>
+                  Share
+                </Button>
+              </RedditShareButton>
+            </ShowIf>
+          </CardContent>
+        </Card>
+
+        <Button color="default" size="large" variant="contained" className={classes.info} fullWidth>
+          Tell me more about linking my social media account
+        </Button>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={closeShare} disabled={!shared} fullWidth={true} size="large" variant="contained" color="secondary">
           {' '}
-          Link Your {social} Account
-        </DialogTitle>
-        <DialogContent dividers>
-          <Card className={classes.card}>
-            <CardHeader avatar={config.step1} title={config.copyTitle} />
-            <CardContent>
-              <TextField className={classes.dark} multiline variant="outlined" rows={6} fullWidth={true} value={message} />
-            </CardContent>
-          </Card>
-          <Card className={classes.card}>
-            <CardHeader avatar={config.step2} title={config.shareTitle} />
-            <CardContent className={classes.share}>
-              <ShowIf condition={social === SocialsEnum.FACEBOOK}>
-                <FacebookShareButton url="https://myriad-fe.herokuapp.com" quote={message}>
-                  <Button variant="contained" size="large" onClick={share} startIcon={<FacebookIcon />} className={classes.facebook}>
-                    Share
-                  </Button>
-                </FacebookShareButton>
-              </ShowIf>
-
-              <ShowIf condition={social === SocialsEnum.TWITTER}>
-                <TwitterShareButton url="https://myriad-fe.herokuapp.com" title={message}>
-                  <Button variant="contained" size="large" onClick={share} startIcon={<TwitterIcon />} className={classes.twitter}>
-                    Share
-                  </Button>
-                </TwitterShareButton>
-              </ShowIf>
-
-              <ShowIf condition={social === SocialsEnum.REDDIT}>
-                <RedditShareButton url="https://myriad-fe.herokuapp.com" title={message}>
-                  <Button variant="contained" size="large" onClick={share} startIcon={<RedditIcon />} className={classes.reddit}>
-                    Share
-                  </Button>
-                </RedditShareButton>
-              </ShowIf>
-            </CardContent>
-          </Card>
-
-          <Button
-            onClick={() => {
-              childRef.current.triggerLinkingTutorial();
-            }}
-            color="default"
-            size="large"
-            variant="contained"
-            className={classes.info}
-            fullWidth>
-            Tell me more about linking my social media account
-          </Button>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={closeShare} disabled={!shared} fullWidth={true} size="large" variant="contained" color="secondary">
-            {' '}
-            I'm done, thanks
-          </Button>
-        </DialogActions>
-      </Dialog>
-
-      <LinkingTutorialComponent ref={childRef} />
-    </>
+          I'm done, thanks
+        </Button>
+      </DialogActions>
+    </Dialog>
   );
 }
