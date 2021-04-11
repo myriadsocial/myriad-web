ac34d9c349e03b78667aea46f76ce4f48cf80b2c
ical10 <mochhusnir@gmail.com>
2021-04-11 15:07:48 +0700

    polkadot api now connected

--- src/helpers/polkadotApi.js
+++ src/helpers/polkadotApi.js
@@ -1,5 +1,5 @@
 export const connectToBlockchain = async () => {
-  const { ApiPromise, WsProvider } = await import('@@polkadot/api');
+  const { ApiPromise, WsProvider } = await import('@polkadot/api');
   const wsProvider = new WsProvider('wss://rpc.myriad.systems/');
   console.log('wsProvider:', wsProvider);
   console.log('the ApiPromise', ApiPromise);
