b45d48922044638eeff905462b8e9010cd7316de
ical10 <mochhusnir@gmail.com>
2021-04-12 10:35:54 +0700

    moved getBalance to helpers, set proper types on variables

--- src/helpers/polkadotApi.js
+++ src/helpers/polkadotApi.js
@@ -7,3 +7,13 @@ export const connectToBlockchain = async () => {
   });
   return api;
 };
+
+export const getBalance = async () => {
+  const DECIMAL_PLACES = 10000000000;
+  const ADDR = '5CS8upU5c44NaPu7qiSXGwna7oeDGG3vifM5nZAbwx3nTGTm';
+  const api = await connectToBlockchain();
+  const {
+    data: { free: previousFree }
+  } = await api.query.system.account(ADDR);
+  return (Number(previousFree) / DECIMAL_PLACES).toFixed(3);
+};
