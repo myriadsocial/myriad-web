8c499ba61185a2f5f95a7d3f1f4f14bdbe599ff5
ical10 <mochhusnir@gmail.com>
2021-04-13 14:25:20 +0700

    sendTip show extension pop-up applied

--- src/components/timeline/post/post.component.tsx
+++ src/components/timeline/post/post.component.tsx
@@ -11,8 +11,7 @@ import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import CommentIcon from '@material-ui/icons/Comment';
 
-//import { ApiPromise, WsProvider } from '@polkadot/api';
-//import { web3FromAddress } from '@polkadot/extension-dapp';
+import { sendTip } from '../../../helpers/polkadotApi';
 import CommentComponent from '../comment/comment.component';
 import ReplyCommentComponent from '../comment/reply.component';
 import FacebookReactionComponent from '../reactions/facebook.component';
@@ -36,26 +35,7 @@ type Props = {
   sendTip?: () => void;
 };
 
-// snippets to send transaction
-// finds an injector for an address
-//const injector = await web3FromAddress('5DTestUPts3kjeXSTMyerHihn1uwMfLj8vU8sqF7qYrFabHE');
-//const WS_PROVIDER = 'wss://dev-node.substrate.dev:9944';
-//// set provider
-//const provider = new WsProvider(WS_PROVIDER);
-//// create api to connect node
-//const api = await ApiPromise.create({ provider });
-
-//// sets the signer for the address on the @polkadot/api
-//api.setSigner(injector.signer);
-
-//// sign and send out transaction - notice here that the address of the account (as retrieved injected)
-//// is passed through as the param to the `signAndSend`, the API then calls the extension to present
-//// to the user and get it signed. Once completex, the api sends the tx + signature via the normal process
-//api.tx.balances
-//.transfer('5C5555yEXUcmEJ5kkcCMvdZjUo7NGJiQJMS7vZXEeoMhj3VQ', 123456)
-//.signAndSend('5DTestUPts3kjeXSTMyerHihn1uwMfLj8vU8sqF7qYrFabHE', (status) => { ... });
-
-export default function PostComponent({ post, open = false, disable = false, reply, sendTip, loadComments }: Props) {
+export default function PostComponent({ post, open = false, disable = false, reply, loadComments }: Props) {
   const style = useStyles();
 
   const [expanded, setExpanded] = useState(open);
@@ -65,8 +45,10 @@ export default function PostComponent({ post, open = false, disable = false, rep
     setExpanded(!expanded);
   };
 
-  const tipPostUser = () => {
-    //sendTip && sendTip();
+  const tipPostUser = async () => {
+    // sendTip will open a pop-up from polkadot.js extension,
+    // tx signing is done by supplying a password
+    await sendTip();
   };
 
   const replyPost = (comment: string) => {
@@ -82,7 +64,7 @@ export default function PostComponent({ post, open = false, disable = false, rep
     window.open(post.link, '_blank');
   };
 
-  const PostAction = (
+  const PostActionTipUser = (
     <Button className={style.action} onClick={tipPostUser} aria-label="tip-post-user" color="primary" variant="contained" size="small">
       Send Tip
     </Button>
@@ -93,13 +75,14 @@ export default function PostComponent({ post, open = false, disable = false, rep
     <Card className={style.root}>
       <CardHeader
         avatar={<PostAvatar origin={post.platform} avatar={detail.user.avatar} />}
-        action={PostAction}
+        action={PostActionTipUser}
         title={detail.user.name}
         subheader={detail.createdOn}
-        onClick={openContentSource}
+        //onClick={openContentSource}
+        //onClick method moved to CardContent, overlapping with tipPostUser method
       />
 
-      <CardContent className={style.content}>
+      <CardContent className={style.content} onClick={openContentSource}>
         <Typography variant="body1" color="textSecondary" component="p">
           {detail.text}
         </Typography>
