4d705945388c503a3db84d6035523eb57a702c66
ical10 <mochhusnir@gmail.com>
2021-04-15 00:08:37 +0700

    MyriadAccountProvider encapsulated by the whole app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ import Head from 'next/head';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
 
+import { MyriadAccountProvider } from '../src/components/wallet/wallet.context';
 import theme from '../src/themes/default';
 
 const App = ({ Component, pageProps }: AppProps) => {
@@ -27,31 +28,33 @@ const App = ({ Component, pageProps }: AppProps) => {
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
       <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Provider
-          // Provider options are not required but can be useful in situations where
-          // you have a short session maxAge time. Shown here with default values.
-          options={{
-            // Client Max Age controls how often the useSession in the client should
-            // contact the server to sync the session state. Value in seconds.
-            // e.g.
-            // * 0  - Disabled (always use cache value)
-            // * 60 - Sync session state with server if it's older than 60 seconds
-            clientMaxAge: 0,
-            // Keep Alive tells windows / tabs that are signed in to keep sending
-            // a keep alive request (which extends the current session expiry) to
-            // prevent sessions in open windows from expiring. Value in seconds.
-            //
-            // Note: If a session has expired when keep alive is triggered, all open
-            // windows / tabs will be updated to reflect the user is signed out.
-            keepAlive: 0
-          }}
-          session={pageProps.session}>
-          <CookiesProvider>
-            <Component {...pageProps} />
-          </CookiesProvider>
-        </Provider>
+        <MyriadAccountProvider>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Provider
+            // Provider options are not required but can be useful in situations where
+            // you have a short session maxAge time. Shown here with default values.
+            options={{
+              // Client Max Age controls how often the useSession in the client should
+              // contact the server to sync the session state. Value in seconds.
+              // e.g.
+              // * 0  - Disabled (always use cache value)
+              // * 60 - Sync session state with server if it's older than 60 seconds
+              clientMaxAge: 0,
+              // Keep Alive tells windows / tabs that are signed in to keep sending
+              // a keep alive request (which extends the current session expiry) to
+              // prevent sessions in open windows from expiring. Value in seconds.
+              //
+              // Note: If a session has expired when keep alive is triggered, all open
+              // windows / tabs will be updated to reflect the user is signed out.
+              keepAlive: 0
+            }}
+            session={pageProps.session}>
+            <CookiesProvider>
+              <Component {...pageProps} />
+            </CookiesProvider>
+          </Provider>
+        </MyriadAccountProvider>
       </ThemeProvider>
     </React.Fragment>
   );
