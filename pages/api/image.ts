36d19535803b0fc7be055e18b109ddb38f6616b2
mucktar <mucktar.pakpahan@gmail.com>
2021-04-19 19:23:52 +0700

    verify credential

---
+++ pages/api/image.ts
@@ -0,0 +1,28 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+import { v2 as cloudinary } from 'cloudinary';
+
+type ResponseImageUpload = {
+  url: string;
+};
+
+cloudinary.config({
+  cloud_name: 'dsget80gs',
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET
+});
+
+const uploadToCloudinary = (file: File) => {
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader.upload_stream({});
+  });
+};
+
+export default async (req: NextApiRequest, res: NextApiResponse<ResponseImageUpload>) => {
+  req.file;
+  const url = await uploadToCloudinary();
+
+  res.status(200).json({
+    url
+  });
+};
