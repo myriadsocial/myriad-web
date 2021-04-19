4bb0131ee6920f0607c091bdf484f73e7ff793cc
mucktar <mucktar.pakpahan@gmail.com>
2021-04-19 12:30:32 +0700

    editable profile

--- src/components/common/ImageUpload.component.tsx
+++ src/components/common/ImageUpload.component.tsx
@@ -1,43 +1,66 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import InputLabel from '@material-ui/core/InputLabel';
+import { createStyles, makeStyles, Theme, fade } from '@material-ui/core/styles';
+import Edit from '@material-ui/icons/Edit';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: {
-      display: 'flex',
-      flexWrap: 'wrap',
-      padding: 50
+    root: {},
+    button: {
+      position: 'absolute',
+      left: 40,
+      top: 160,
+      zIndex: 10
+    },
+    label: {
+      backgroundColor: fade(theme.palette.primary.main, 0.7)
+    },
+    icon: {
+      color: '#FFF'
     }
   })
 );
 
 type Props = {
   value: string;
-  onSelected: (file: File) => void;
+  children: React.ReactNode;
+  onSelected: (previewUrl: string) => void;
 };
 
-export const FileUpliad = ({ onSelected, value }: Props) => {
+export const ImageUpload = ({ onSelected, value, children }: Props) => {
   const styles = useStyles();
 
-  const [filePreview, setFilePreview] = useState<string>(value);
+  const uploadFieldRef = React.useRef<HTMLInputElement | null>(null);
+
+  const selectFile = (): void => {
+    const uploadField: any = uploadFieldRef?.current;
+    if (!uploadField) {
+      return;
+    }
+    uploadField.click();
+  };
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files.length > 0) {
       const image = event.target.files[0];
       const imageUrl = URL.createObjectURL(image);
 
-      setFilePreview(imageUrl);
-
-      onSelected(image);
+      onSelected(imageUrl);
     }
   };
 
   return (
     <div className={styles.root}>
-      <input type="file" onChange={handleFileChange} />
+      <input type="file" ref={uploadFieldRef} onChange={handleFileChange} style={{ display: 'none' }} />
+      <InputLabel className={styles.button}>
+        <IconButton onClick={selectFile} className={styles.label} disableRipple disableFocusRipple>
+          <Edit className={styles.icon} />
+        </IconButton>
+      </InputLabel>
 
-      <img style={{ width: '100%' }} src={filePreview} />
+      {children}
     </div>
   );
 };
