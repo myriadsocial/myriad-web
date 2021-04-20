a0d58dc5330061ae8cf5c2be841d409d9f3ecbd7
ical10 <mochhusnir@gmail.com>
2021-04-20 16:50:55 +0700

    build succeed

--- src/components/common/SendTipModal.tsx
+++ src/components/common/SendTipModal.tsx
@@ -118,7 +118,9 @@ const SendTipModal = forwardRef(({ postId }: Props, ref) => {
     const currentAddress = session?.user.address;
     const freeBalance = await getBalance(currentAddress);
     //console.log('the freeBalance is: ', Number(freeBalance) / 100);
-    setBalance(Number((freeBalance / 100).toFixed(3)));
+    if (freeBalance) {
+      setBalance(Number((freeBalance / 100).toFixed(3)));
+    }
   };
 
   const postTxHistory = async ({ from, to, amount, trxHash }: PostTxHistory) => {
