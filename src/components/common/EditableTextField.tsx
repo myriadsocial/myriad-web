c11cce000ea83bd60b9ff56335960180015e5882
ical10 <mochhusnir@gmail.com>
2021-04-26 12:04:55 +0700

    fixed build error

--- src/components/common/EditableTextField.tsx
+++ src/components/common/EditableTextField.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
