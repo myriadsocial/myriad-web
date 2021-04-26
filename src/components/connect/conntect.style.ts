01f022134cbd9c591a928996c59391b201edcc18
mucktar <mucktar.pakpahan@gmail.com>
2021-04-26 19:18:52 +0700

    update relogin

---
+++ src/components/connect/conntect.style.ts
@@ -0,0 +1,36 @@
+import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
+
+export const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    share: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    card: {
+      boxShadow: 'none'
+    },
+    purple: {
+      backgroundColor: '#A942E9'
+    },
+    dark: {
+      backgroundColor: theme.palette.primary.main,
+      borderRadius: 8
+    },
+    info: {
+      textTransform: 'none'
+    },
+    facebook: {
+      color: '#3b5998',
+      minWidth: 40
+    },
+    twitter: {
+      color: '#1DA1F2',
+      minWidth: 40
+    },
+    reddit: {
+      color: '#FF5700',
+      minWidth: 40
+    }
+  })
+);
