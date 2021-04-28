e17b1abe30df2a9f51298af575e503126a277bd1
mucktar <mucktar.pakpahan@gmail.com>
2021-04-28 18:58:19 +0700

    get twitter following on connect

--- pages/api/share/twitter.ts
+++ pages/api/share/twitter.ts
@@ -1,24 +1,27 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { getSession } from 'next-auth/client';
 
-import Axios from 'axios';
 import { SocialsEnum } from 'src/interfaces';
+import * as PeopleAPI from 'src/lib/api/people';
+import * as UserAPI from 'src/lib/api/user';
 import { TwitterClient } from 'twitter-api-client';
 
 type ResponeTwitter = {
   shared: boolean;
 };
 
-const MyriadAPI = Axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://34.101.124.163:3000'
-});
-
 export default async (req: NextApiRequest, res: NextApiResponse<ResponeTwitter>) => {
   const session = await getSession({ req });
 
+  if (!session) {
+    res.status(200).json({
+      shared: false
+    });
+  }
+
   //@ts-ignore
-  const credential = session?.user.userCredentials.find(item => item.platform === SocialsEnum.TWITTER);
-  console.log('credential twitter', credential);
+  const credential = session.user.userCredentials.find(item => item.platform === SocialsEnum.TWITTER);
+
   if (!credential) {
     res.status(200).json({
       shared: false
@@ -32,66 +35,31 @@ export default async (req: NextApiRequest, res: NextApiResponse<ResponeTwitter>)
     accessTokenSecret: credential.refreshToken
   });
 
-  // const tweets = await twitterClient.tweets.statusesUserTimeline({
-  //   include_rts: false,
-  //   count: 15
-  // });
+  const following = await twitterClient.accountsAndUsers.friendsList();
 
-  // tweets.forEach(async tweet => {
-  //   try {
-  //     await MyriadAPI({
-  //       method: 'POST',
-  //       url: `/posts`,
-  //       data: {
-  //         tags: tweet.entities.hashtags,
-  //         platform: SocialsEnum.TWITTER,
-  //         text: tweet.text,
-  //         textId: tweet.id_str,
-  //         hasMedia: false,
-  //         createdAt: new Date(),
-  //         updatedAt: new Date(),
-  //         link: `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id}`
-  //       }
-  //     });
-  //   } catch (error) {
-  //     console.log('error create post', error);
-  //   }
-  // });
-
-  const followers = await twitterClient.accountsAndUsers.followersList({
-    count: 10
-  });
-
-  for (const user of followers.users) {
+  for (const user of following.users) {
     try {
-      await MyriadAPI({
-        url: '/people',
-        method: 'POST',
-        data: {
-          username: user.name,
-          platform: SocialsEnum.TWITTER,
-          platform_account_id: user.id_str,
-          hide: false
-        }
+      await PeopleAPI.createPeople({
+        username: user.screen_name,
+        platform: SocialsEnum.TWITTER,
+        platform_account_id: user.id_str,
+        hide: false
       });
     } catch (error) {
-      console.log('error create people', error);
+      console.log('[connect][twitter]error create people', error.response.data);
     }
   }
 
   try {
-    await MyriadAPI.request({
-      method: 'POST',
-      url: '/user-credentials/verify',
-      data: {
-        userId: session?.user.id,
-        peopleId: credential.platformUserId
-      }
-    });
+    const userId = session?.user.address as string;
 
-    console.log('twitter user-credentials verify sent');
+    if (userId) {
+      await UserAPI.verifyCredentials(userId, credential.platformUserId);
+    }
+
+    console.log('[connect][twitter] user-credentials verify sent');
   } catch (error) {
-    console.error('twitter user-credentials verify', error);
+    console.error('[connect][twitter] user-credentials verify error', error);
   }
 
   res.status(200).json({
