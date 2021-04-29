1bf989531f0162c3f744b1ffa81d3b641e43654a
ical10 <mochhusnir@gmail.com>
2021-04-29 12:22:43 +0700

    styled balance button

--- src/components/wallet/balance.component.tsx
+++ src/components/wallet/balance.component.tsx
@@ -2,12 +2,16 @@ import React, { useState, useEffect } from 'react';
 
 import { useSession } from 'next-auth/client';
 
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Divider from '@material-ui/core/Divider';
 import Grid from '@material-ui/core/Grid';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Typography from '@material-ui/core/Typography';
 import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
+import RefreshIcon from '@material-ui/icons/Refresh';
 
 import { getBalance } from '../../helpers/polkadotApi';
 
@@ -25,16 +29,6 @@ const useStyles = makeStyles((theme: Theme) =>
       paddingRight: theme.spacing(2),
       fontSize: 24
     },
-    titleHidden: {
-      paddingTop: theme.spacing(3),
-      paddingLeft: theme.spacing(2),
-      paddingBottom: theme.spacing(1),
-      paddingRight: theme.spacing(2),
-      fontSize: 24,
-      textShadowColor: 'rgba(0, 0, 0, 0.75)',
-      textShadowOffset: { width: -1, height: 1 },
-      textShadowRadius: 10
-    },
     subtitle: {
       textTransform: 'uppercase',
       fontSize: 12
@@ -43,36 +37,54 @@ const useStyles = makeStyles((theme: Theme) =>
       border: `1px solid`,
       borderColor: '#A942E9',
       borderRadius: 8,
-      margin: theme.spacing(2),
-      padding: 0
+      marginBottom: theme.spacing(1)
     },
-    buttonText: {
-      height: 24,
-      lineHeight: 10,
-      textAlign: 'center',
-      border: 1,
-      borderRadius: 8
+    icon: {
+      minWidth: 40
+    },
+    showText: {
+      width: '100%',
+      padding: '2px',
+      fontWeight: 600
+    },
+    balanceText: {
+      width: '100%',
+      padding: '2px',
+      fontWeight: 700
+    },
+    container: {
+      width: '100%',
+      backgroundColor: 'transparent',
+      position: 'relative',
+      margin: '0 4px'
+    },
+    spinner: {
+      color: '#A942E9',
+      position: 'relative',
+      top: 4,
+      left: 6
     }
   })
 );
-
-const typographyProps = { style: { fontSize: 10, padding: '5px 0', fontWeight: 500 } };
 
 export const BalanceComponent = React.memo(function Wallet() {
   const style = useStyles();
   const [session] = useSession();
   const [isHidden, setIsHidden] = useState(true);
   const [balance, setBalance] = useState(0);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     getBalanceForComponent();
   }, [balance]);
 
   const getBalanceForComponent = async () => {
+    setLoading(true);
     const currentAddress = session?.user.address;
     const freeBalance = await getBalance(currentAddress);
     if (freeBalance) {
       setBalance(Number((freeBalance / 100).toFixed(3)));
+      setLoading(false);
     }
   };
 
@@ -86,24 +98,34 @@ export const BalanceComponent = React.memo(function Wallet() {
 
   return (
     <div className={style.root}>
-      <Grid container direction="row" justify="center" alignItems="center">
+      <Grid container direction="column" justify="center" alignItems="center">
         <Grid item>
           <Typography className={style.title} variant="h5">
             Total Balance
           </Typography>
         </Grid>
         <Grid item>
-          <Typography className={style.title} variant="h5" onClick={handleIsHidden}>
-            {isHidden ? '... ' : balance}
-            <span className={style.subtitle}>Myria</span>
-          </Typography>
-        </Grid>
-        <Grid item>
           <List>
-            <ListItem button className={style.gutters} onClick={handleClick}>
-              <ListItemText primaryTypographyProps={typographyProps} className={style.buttonText}>
-                <Typography>Refresh</Typography>
-              </ListItemText>
+            <ListItem button className={style.gutters}>
+              <ListItemIcon className={style.icon}>
+                <RefreshIcon onClick={handleClick} />
+              </ListItemIcon>
+              <Divider orientation="vertical" flexItem />
+              <div className={style.container}>
+                <ListItemText>
+                  {isHidden ? (
+                    <Typography className={style.showText} onClick={handleIsHidden}>
+                      Show balance
+                    </Typography>
+                  ) : loading ? (
+                    <CircularProgress className={style.spinner} size={20} />
+                  ) : (
+                    <Typography className={style.balanceText} onClick={handleIsHidden}>
+                      {balance} <span className={style.subtitle}>Myria</span>
+                    </Typography>
+                  )}
+                </ListItemText>
+              </div>
             </ListItem>
           </List>
         </Grid>
