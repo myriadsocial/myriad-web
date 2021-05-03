0aed9164c46b38135f0e6ef2e298727af4d95adb
mucktar <mucktar.pakpahan@gmail.com>
2021-05-03 09:18:15 +0700

    filter social media

--- src/components/Layout/use-layout.hook.ts
+++ src/components/Layout/use-layout.hook.ts
@@ -1,11 +1,12 @@
 import { useLayoutSetting, LayoutSettingActionType } from './layout.context';
 
+import { SocialsEnum } from 'src/interfaces/index';
 import { LayoutFilterType } from 'src/interfaces/setting';
 
 export const useLayout = () => {
   const { state: setting, dispatch } = useLayoutSetting();
 
-  const changeSetting = (key: LayoutFilterType, value: boolean) => {
+  const changeSetting = (key: LayoutFilterType | SocialsEnum, value: boolean) => {
     dispatch({
       type: LayoutSettingActionType.CHANGE_SETTING,
       key,
