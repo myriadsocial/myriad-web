e5696e4cab668f8bc7493d9b37a4696d2df1511d
ical10 <mochhusnir@gmail.com>
2021-05-18 11:49:02 +0700

    rephrased sentence for anonymous users in comment section

--- src/helpers/polkadotApi.js
+++ src/helpers/polkadotApi.js
@@ -76,27 +76,3 @@ export const sendTip = async (fromAddress, toAddress, amountSent) => {
     return error;
   }
 };
-
-export const getWalletHistory = async () => {
-  const api = await connectToBlockchain();
-  //Subscribe to system events via storage
-  api.query.system.events(events => {
-    console.log(`\nReceived ${events.length} events:`);
-
-    //Loop through the Vec<EventRecord>
-    events.forEach(record => {
-      //Extract the phase, event and the event types
-      const { event, phase } = record;
-      const types = event.typeDef;
-
-      //Show what we are busy with
-      console.log(`\t${event.section}:${event.method}:: (phase=${phase.toString()})`);
-      console.log(`\t\t${event.meta.documentation.toString()}`);
-
-      //Loop through each of the parameters, displaying the type and data
-      event.data.forEach((data, index) => {
-        console.log(`\t\t\t${types[index].type}: ${data.toString()}`);
-      });
-    });
-  });
-};
