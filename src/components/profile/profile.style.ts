5461062bc3662793aabf904044a80c5cd0bb5393
imam hermawan <immd.flores@gmail.com>
2021-05-20 11:49:06 +0700

    add interface profile hook for fetching data user profile

--- src/components/profile/profile.style.ts
+++ src/components/profile/profile.style.ts
@@ -17,6 +17,7 @@ export const useStyles = makeStyles((theme: Theme) =>
       height: '100%',
       width: '100%',
       overflowY: 'auto',
+      overflowX: 'hidden',
       display: 'flex',
       flexDirection: 'column'
     },
@@ -33,6 +34,7 @@ export const useStyles = makeStyles((theme: Theme) =>
     headerPicture: {
       display: 'flex',
       justifyContent: 'space-between',
+      alignItems: 'center',
       padding: 20,
       backgroundColor: '#424242',
       borderBottom: `1px solid`,
