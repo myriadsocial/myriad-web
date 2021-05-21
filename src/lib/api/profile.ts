ddcf72e01e870826c252c3937351f4f5aa0f2cdd
imam hermawan <immd.flores@gmail.com>
2021-05-21 16:33:26 +0700

    temporary update profile posts

--- src/lib/api/profile.ts
+++ src/lib/api/profile.ts
@@ -1,5 +1,6 @@
 import Axios from 'axios';
 import { ExtendedUserPost } from 'src/interfaces/user';
+import { User } from 'src/interfaces/user';
 
 const MyriadAPI = Axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://34.101.124.163:3000'
@@ -13,11 +14,35 @@ export const getUserProfile = async (id: string): Promise<ExtendedUserPost> => {
       filter: {
         include: [
           {
-            relation: 'posts'
+            relation: 'posts',
+            scope: {
+              include: [
+                {
+                  relation: 'comments'
+                  // scope: {
+                  //   include: [
+                  //     {
+                  //       relation: 'user'
+                  //     }
+                  //   ]
+                  // }
+                }
+              ]
+            }
           }
         ]
       }
     }
+  });
+
+  return data;
+};
+
+export const updateUserProfile = async (id: string, attributes: Partial<User>) => {
+  const { data } = await MyriadAPI({
+    url: `/users/${id}`,
+    method: 'PATCH',
+    data: attributes
   });
 
   return data;
