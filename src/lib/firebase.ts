9441d39b29f55abd0d37ffc249532244b37aa0c7
mucktar <mucktar.pakpahan@gmail.com>
2021-05-22 14:35:27 +0700

    init firebase web push

---
+++ src/lib/firebase.ts
@@ -0,0 +1,35 @@
+import firebase from 'firebase/app';
+import 'firebase/messaging';
+import localforage from 'localforage';
+
+const firebaseCloudMessaging = {
+  tokenInlocalforage: async () => {
+    return localforage.getItem('fcm_token');
+  },
+
+  init: async function (): Promise<void | boolean> {
+    firebase.initializeApp({
+      apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+    });
+
+    try {
+      if ((await this.tokenInlocalforage()) !== null) {
+        return false;
+      }
+
+      const messaging = firebase.messaging();
+      await Notification.requestPermission();
+      const token = await messaging.getToken();
+
+      localforage.setItem('fcm_token', token);
+      console.log('fcm_token', token);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+};
+
+export { firebaseCloudMessaging };
