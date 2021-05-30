8c0a4fdc7c1f9cf81c4d6df7191598137e62cee5
mucktar <mucktar.pakpahan@gmail.com>
2021-05-30 21:31:00 +0700

    update layout

--- src/components/Layout/layout.style.ts
+++ src/components/Layout/layout.style.ts
@@ -15,42 +15,10 @@ export const useStyles = makeStyles((theme: Theme) =>
       flex: '1 1 auto',
       marginTop: 32
     },
-    user: {
-      flex: '0 0 327px',
-      marginRight: 0,
-      // height: '100vh',
-      // 'overflow-y': 'scroll !important',
-      'scrollbar-color': '#A942E9 #171717',
-      'scrollbar-width': 'thin !important'
-    },
-    content: {
-      // flex: '1 1 auto',
-      marginLeft: 'auto',
-      marginRight: 'auto',
-      padding: '0 24px 0 24px',
-      height: '100vh',
-      maxWidth: 726,
-      [theme.breakpoints.up('xl')]: {
-        maxWidth: 926
-      }
-    },
     experience: {
       flex: '0 0 331px',
-      // height: '100vh',
-      // overflowY: 'scroll',
       'scrollbar-color': '#A942E9 #171717 ',
       'scrollbar-width': 'thin !important'
     },
-    wallet: {
-      width: 327
-    },
-
-    fullheight: {
-      height: '100vh'
-    },
-
-    profile: {
-      flexGrow: 1
-    }
   })
 );
