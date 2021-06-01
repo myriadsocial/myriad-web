f41e8c506ebfb827f55e74852678427e424a772b
mucktar <mucktar.pakpahan@gmail.com>
2021-06-01 15:01:39 +0700

    update friend component on profil detail

--- src/components/profile/profile.style.ts
+++ src/components/profile/profile.style.ts
@@ -3,9 +3,8 @@ import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 export const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
-      width: '100%',
-      maxWidth: '881px',
-      minWidth: '590px',
+      width: 1024,
+      background: theme.palette.background.default,
       paddingRight: '32px',
       marginRight: 'auto',
       height: '100vh',
@@ -43,6 +42,11 @@ export const useStyles = makeStyles((theme: Theme) =>
       width: 678,
       height: '100%'
     },
+
+    tabContent: {
+      filter: 'drop-shadow(0px 4px 8px rgba(0, 0, 0, 0.16))',
+      background: theme.palette.background.paper
+    },
     tabHeader: {
       // background: '#C4C4C4'
       // borderRadius: 8
@@ -50,15 +54,15 @@ export const useStyles = makeStyles((theme: Theme) =>
     tabItem: {
       // width: 130,
       // height: 56,
-      color: '#8629E9',
+      color: theme.palette.primary.main,
       minHeight: 'unset',
       minWidth: 'unset',
 
       '&.Mui-selected': {
         // background: 'white',
         borderRadius: 8,
-        background: '#8629E9',
-        color: '#FFFFFF',
+        background: theme.palette.primary.main,
+        color: theme.palette.text.secondary,
         // width: 157,
 
         '& .MuiTypography-root': {
---
+++ src/components/profile/user-friends.component.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+import Typography from '@material-ui/core/Typography';
+
+import FriendListComponent from '../friends/friend-list.component';
+import { useFriendsHook } from '../friends/use-friends-hook';
+
+import { debounce } from 'lodash';
+import SearchComponent from 'src/components/common/search.component';
+import { useFriends } from 'src/components/friends/friends.context';
+import { useUser } from 'src/components/user/user.context';
+
+interface UserFriendProps {}
+
+const UserFriendComponent: React.FC<UserFriendProps> = props => {
+  const {
+    state: { user }
+  } = useUser();
+  const {
+    state: { friends }
+  } = useFriends();
+  const [search, setSearchQuery] = useState('');
+
+  if (!user) return null;
+  //@ts-ignore
+  const { searchFriend } = useFriendsHook(user);
+
+  const handleSearchFriend = debounce((query: string) => {
+    console.log('SEARCH', query), setSearchQuery(query);
+    searchFriend(query);
+  }, 300);
+
+  return (
+    <div style={{ padding: 8 }}>
+      <div style={{ paddingTop: 16, paddingBottom: 8 }}>
+        <Typography variant="h4" style={{ marginBottom: 16 }}>
+          Friends with {friends.length} people
+        </Typography>
+
+        <SearchComponent value={search} placeholder="Find a Friend" onSubmit={handleSearchFriend} />
+      </div>
+
+      <FriendListComponent user={user} />
+    </div>
+  );
+};
+
+export default UserFriendComponent;
