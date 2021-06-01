13953a064fbe5e4f1aa45dfcb8d4a5b8e4ea5583
mucktar <mucktar.pakpahan@gmail.com>
2021-06-01 09:36:43 +0700

    add mobile menu

---
+++ src/context/app-context.provider.tsx
@@ -0,0 +1,21 @@
+// import React from 'react';
+
+// import { ConverstionProvider } from '../components/conversation/conversation.context';
+// import { ExperienceProvider } from '../components/experience/experience.context';
+// import { TimelineProvider } from '../components/timeline/timeline.context';
+// import { TransactionProvider } from '../components/tippingJar/transaction.context';;
+// import { LayoutSettingProvider } from '../components/Layout/layout.context';
+
+// import { combineProviders} from './combine-providers.component';
+
+// const providers = [
+//   ConverstionProvider,
+//   ExperienceProvider,
+//   TimelineProvider,
+//   TransactionProvider,
+//   LayoutSettingProvider,
+// ];
+
+// export const AppContextProvider: React.FC = combineProviders(...providers);
+
+export const status = 'a';
