f4296acd4dfab8dfc2dde5abdea6a23e1125ccb6
mucktar <mucktar.pakpahan@gmail.com>
2021-06-17 21:58:19 +0700

    add post tipping summary

--- src/interfaces/transaction.ts
+++ src/interfaces/transaction.ts
@@ -1,8 +1,14 @@
+import { User } from './user';
+
 export interface Transaction {
+  id: string;
   trxHash: string;
   from: string;
   to: string;
   value: number;
   state: string;
   tokenId: string;
+  createdAt: string;
+  fromUser?: User;
+  toUser?: User;
 }
