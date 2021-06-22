b0beaf9c1bad42f391e79afd1196d65687c342d8
ical10 <mochhusnir@gmail.com>
2021-06-22 15:42:46 +0700

    Added interface on getWalletAddress

--- src/lib/api/wallet.ts
+++ src/lib/api/wallet.ts
@@ -1,11 +1,12 @@
 import Axios from 'axios';
+import { Wallet } from 'src/interfaces/wallet';
 
 const MyriadAPI = Axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL
 });
 
-export const getWalletAddress = async (postId: string) => {
-  const { data } = await MyriadAPI({
+export const getWalletAddress = async (postId: string): Promise<Wallet> => {
+  const { data } = await MyriadAPI.request<Wallet>({
     url: `/posts/${postId}/walletaddress`,
     method: 'GET'
   });
