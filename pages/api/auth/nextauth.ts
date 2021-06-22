fa4290cfeaa9be95af9cf1f832f8c9879b2860b2
ical10 <mochhusnir@gmail.com>
2021-06-22 15:41:50 +0700

    added tokenId

---
+++ pages/api/auth/nextauth.ts
@@ -0,0 +1,210 @@
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+
+import APIAdapter from '../../../adapters/api';
+
+import * as UserAPI from 'src/lib/api/user';
+import { userToSession } from 'src/lib/serializers/session';
+
+type Credentials = {
+  platform: string;
+  platformUserId: string;
+  username: string;
+  accessToken: string;
+};
+
+// For more information on each option (and a full list of options) go to
+// https://next-auth.js.org/configuration/options
+export default NextAuth({
+  adapter: APIAdapter.Adapter(),
+  // https://next-auth.js.org/configuration/providers
+  providers: [
+    Providers.Twitter({
+      clientId: process.env.TWITTER_API_KEY || '',
+      clientSecret: process.env.TWITTER_API_KEY_SECRET || ''
+    }),
+    Providers.Facebook({
+      clientId: process.env.FACEBOKK_APP_ID || '',
+      clientSecret: process.env.FACEBOOK_APP_SECRET || '',
+      scope: 'user_posts,user_friends'
+    }),
+    Providers.Reddit({
+      clientId: process.env.REDDIT_APP_ID || '',
+      clientSecret: process.env.REDDIT_SECRET || '',
+      scope: 'identity read'
+    }),
+    Providers.Credentials({
+      // The name to display on the sign in form (e.g. 'Sign in with...')
+      name: 'Address',
+      // The credentials is used to generate a suitable form on the sign in page.
+      // You can specify whatever fields you are expecting to be submitted.
+      // e.g. domain, username, password, 2FA token, etc.
+      credentials: {
+        name: { label: 'Name', type: 'text' },
+        address: { label: 'Address', type: 'text' }
+      },
+      async authorize(credentials: Record<string, string>) {
+        console.log('[next-auth][debug][authorize] credentials', credentials);
+
+        if (credentials.anonymous === 'false') {
+          try {
+            const user = await UserAPI.getUserDetail(credentials.address);
+
+            console.log('[next-auth][debug][authorize] user exist', user);
+
+            return userToSession(user);
+          } catch (error) {
+            try {
+              const user = await UserAPI.createUser({
+                id: credentials.address,
+                name: credentials.name,
+                username: credentials.name.replace(/\s/g, ''),
+                profilePictureURL: '',
+                anonymous: false,
+                bio: '',
+                createdAt: new Date()
+              });
+
+              console.log('[next-auth][debug][authorize] user create', user);
+
+              return userToSession(user);
+            } catch (error) {
+              console.error('[next-auth][debug][authorize] user create error', error.response.data.error);
+              throw new Error('Failed to login');
+            }
+          }
+        }
+
+        return {
+          userId: credentials.address,
+          name: credentials.name,
+          address: credentials.address,
+          anonymous: credentials.anonymous === 'true'
+        };
+      }
+    })
+  ],
+  // Database optional. MySQL, Maria DB, Postgres and MongoDB are supported.
+  // https://next-auth.js.org/configuration/databases
+  //
+  // Notes:
+  // * You must to install an appropriate node_module for your database
+  // * The Email provider requires a database (OAuth providers do not)
+  // database: process.env.DATABASE_URL,
+
+  // The secret should be set to a reasonably long random string.
+  // It is used to sign cookies and to sign and encrypt JSON Web Tokens, unless
+  // a separate secret is defined explicitly for encrypting the JWT.
+  secret: process.env.SECRET,
+  useJwtSession: true,
+  session: {
+    // Use JSON Web Tokens for session instead of database sessions.
+    // This option can be used with or without a database for users/accounts.
+    // Note: `jwt` is automatically set to `true` if no database is specified.
+    jwt: true
+
+    // Seconds - How long until an idle session expires and is no longer valid.
+    // maxAge: 30 * 24 * 60 * 60, // 30 days
+
+    // Seconds - Throttle how frequently to write to database to extend a session.
+    // Use it to limit write operations. Set to 0 to always update the database.
+    // Note: This option is ignored if using JSON Web Tokens
+    // updateAge: 24 * 60 * 60, // 24 hours
+  },
+
+  // JSON Web tokens are only used for sessions if the `jwt: true` session
+  // option is set - or by default if no database is specified.
+  // https://next-auth.js.org/configuration/options#jwt
+  jwt: {
+    // A secret to use for key generation (you should set this explicitly)
+    // secret: 'INp8IvdIyeMcoGAgFGoA61DdBglwwSqnXJZkgz8PSnw',
+    // Set to true to use encryption (default: false)
+    // encryption: true,
+    // You can define your own encode/decode functions for signing and encryption
+    // if you want to override the default behaviour.
+    // encode: async ({ secret, token, maxAge }) => {},
+    // decode: async ({ secret, token, maxAge }) => {},
+  },
+
+  // You can define custom pages to override the built-in ones. These will be regular Next.js pages
+  // so ensure that they are placed outside of the '/api' folder, e.g. signIn: '/auth/mycustom-signin'
+  // The routes shown here are the default URLs that will be used when a custom
+  // pages is not specified for that route.
+  // https://next-auth.js.org/configuration/pages
+  pages: {
+    signIn: '/', // Displays signin buttons
+    signOut: '/', // Displays form with sign out button
+    error: '/' // Error code passed in query string as ?error=
+
+    // verifyRequest: '/auth/verify-request', // Used for check email page
+    // newUser: null // If set, new users will be directed here on first sign in
+  },
+
+  // Callbacks are asynchronous functions you can use to control what happens
+  // when an action is performed.
+  // https://next-auth.js.org/configuration/callbacks
+  callbacks: {
+    // async signIn(user, account, profile) { return true },
+    // async redirect(url, baseUrl) { return baseUrl },
+    // @ts-ignore
+    async session(session, user: Record<string, string>) {
+      console.log('[next-auth][debug][session]', session, user);
+
+      return {
+        ...session,
+        user
+      };
+    },
+    async jwt(token, user, account, profile, isNewUser) {
+      token = { ...token, ...user };
+
+      if (account && account.type === 'credentials') {
+        token.userId = profile.address;
+        token.address = profile.address;
+        token.anonymous = profile.anonymous;
+      }
+
+      if (account && account.type === 'oauth') {
+        const credentials = {
+          platform: account.provider,
+          platformUserId: account.id,
+          username: profile.username || profile.name,
+          accessToken: account.accessToken,
+          refreshToken: account.refreshToken
+        };
+        //@ts-ignore
+        if (!token.userCredentials || token.userCredentials.length === 0) {
+          token.userCredentials = [] as Credentials[];
+        }
+        //@ts-ignore
+        token.userCredentials.push(credentials);
+
+        console.log('[next-auth][debug][jwt] token', token);
+      }
+
+      return token;
+    }
+  },
+
+  // Events are useful for logging
+  // https://next-auth.js.org/configuration/events
+  events: {
+    async error(message) {
+      console.error(message);
+    }
+  },
+
+  // Enable debug messages in the console if you are having problems
+  debug: true,
+  logger: {
+    error(code, ...message) {
+      console.error(code, message);
+    },
+    warn(code, ...message) {
+      console.warn(code, message);
+    },
+    debug(code, ...message) {
+      console.log(code, message);
+    }
+  }
+});
--- src/interfaces/transaction.ts
+++ src/interfaces/transaction.ts
@@ -1,14 +1,8 @@
-import { User } from './user';
-
 export interface Transaction {
-  id: string;
   trxHash: string;
   from: string;
   to: string;
   value: number;
   state: string;
   tokenId: string;
-  createdAt: string;
-  fromUser?: User;
-  toUser?: User;
 }
