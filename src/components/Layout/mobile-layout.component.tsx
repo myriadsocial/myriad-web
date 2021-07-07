d05ef719917741cb8cfaee80fba5cd5d20dbcc16
ical10 <mochhusnir@gmail.com>
2021-07-07 10:25:07 +0700

    Fetched user`s tokens and passed down to timeline

--- src/components/Layout/mobile-layout.component.tsx
+++ src/components/Layout/mobile-layout.component.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 
 import { User } from 'next-auth';
+import { useSession } from 'next-auth/client';
 import dynamic from 'next/dynamic';
 
 import { useTheme } from '@material-ui/core/styles';
@@ -13,6 +14,7 @@ import { FriendsProvider } from 'src/context/friends.context';
 import { NotifProvider } from 'src/context/notif.context';
 import { useUser } from 'src/context/user.context';
 import { useLayout } from 'src/hooks/use-layout.hook';
+import { useToken } from 'src/hooks/use-token.hook';
 import { useUserHook } from 'src/hooks/use-user.hook';
 import { SidebarTab } from 'src/interfaces/sidebar';
 
@@ -35,6 +37,20 @@ const MobileLayoutComponent = ({ children, user }: Props) => {
   const { getUserDetail, loadFcmToken, setAsAnonymous } = useUserHook(user.address as string);
   const [value, setValue] = React.useState(0);
   const isAnonymous = Boolean(user.anonymous);
+  const [session, sessionLoading] = useSession();
+  let userId = session?.user.userId as string;
+
+  useEffect(() => {
+    if (session !== null && !sessionLoading) {
+      userId = session?.user.userId as string;
+    }
+  }, [sessionLoading]);
+
+  const { loadAllUserTokens, loading, userTokens, errorUserTokens } = useToken(userId);
+
+  useEffect(() => {
+    loadAllUserTokens();
+  }, []);
 
   useEffect(() => {
     if (value !== selectedSidebar) {
@@ -64,7 +80,7 @@ const MobileLayoutComponent = ({ children, user }: Props) => {
         <NotifProvider>
           <AppBar />
           <TabPanel value={value} index={SidebarTab.HOME} dir={theme.direction}>
-            <TimelineComponent isAnonymous={isAnonymous} />
+            <TimelineComponent isAnonymous={isAnonymous} availableTokens={userTokens} />
           </TabPanel>
           <TabPanel value={value} index={SidebarTab.WALLET} dir={theme.direction}>
             <WalletComponent />
