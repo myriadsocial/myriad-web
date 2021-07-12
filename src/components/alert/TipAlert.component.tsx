1180e62a2e20292063ff42284f9a861bccbd8298
ical10 <mochhusnir@gmail.com>
2021-07-12 14:22:04 +0700

    Creation of TipAlert Component: created own Alert component for tipping

---
+++ src/components/alert/TipAlert.component.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+
+import Button from '@material-ui/core/Button';
+import Snackbar from '@material-ui/core/Snackbar';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import Alert from '@material-ui/lab/Alert';
+import AlertTitle from '@material-ui/lab/AlertTitle';
+
+import { useAlertHook } from 'src/hooks/use-alert.hook';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: '100%',
+      '& > * + *': {
+        marginTop: theme.spacing(2)
+      }
+    },
+    alert: {
+      width: 400,
+      borderRadius: theme.spacing(1),
+
+      '& .MuiAlert-icon': {
+        fontSize: 32
+      },
+      '& .MuiAlert-message': {
+        fontSize: 14,
+        fontWeight: 500
+      },
+      '& .MuiAlertTitle-root': {
+        fontSize: 16,
+        fontWeight: 600
+      }
+    }
+  })
+);
+
+const TipAlertComponent: React.FC = () => {
+  const style = useStyles();
+  const { error, isTipping, clearAlert } = useAlertHook();
+
+  return (
+    <div className={style.root}>
+      <Snackbar
+        open={isTipping}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'center'
+        }}
+        autoHideDuration={6000}
+        onClose={clearAlert}>
+        <Alert className={style.alert} severity={error.severity || 'info'}>
+          <AlertTitle>{error.title}</AlertTitle>
+          {error.severity === 'success' && error.message ? (
+            <Button target="_blank" href={`https://acala-testnet.subscan.io/extrinsic/${error.message}`}>
+              TxHash: {error.message.substring(0, 5)}....{' '}
+            </Button>
+          ) : (
+            error.message
+          )}
+        </Alert>
+      </Snackbar>
+    </div>
+  );
+};
+
+export default TipAlertComponent;
