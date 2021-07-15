abd23e51a4eff89b5948a9c7c4142a05e880ae3f
Irman Nur Muhammad Alamsyah <irmannmal@gmail.com>
2021-07-15 21:08:02 +0700

    MYR-000: update build script

--- .maintain/docker/build-image.sh
+++ .maintain/docker/build-image.sh
@@ -10,13 +10,15 @@ cd $PROJECT_ROOT
 IMAGE_NAME=myriadsocial/myriad-web
 VCS_REF=`git rev-parse --short HEAD`
 VERSION=`awk -F \" '/"version": ".+"/ { print $4; exit; }' package.json`
+# BUILD_ENV -dev, -stg, keep blank for production
+BUILD_ENV=
 BUILD_DATE=`date -u +"%Y%m%d"`
 
 # Build the image
-echo "Building ${IMAGE_NAME}:latest docker image, hang on!"
-time docker build -f ./.maintain/docker/Dockerfile --build-arg VCS_REF=${VCS_REF} --build-arg BUILD_DATE=${BUILD_DATE} -t ${IMAGE_NAME}:latest .
+echo "Building ${IMAGE_NAME}:latest${BUILD_ENV} docker image, hang on!"
+time docker build -f ./.maintain/docker/Dockerfile --build-arg VCS_REF=${VCS_REF} --build-arg BUILD_DATE=${BUILD_DATE} -t ${IMAGE_NAME}:latest${BUILD_ENV} .
 # Build verison
-time docker tag ${IMAGE_NAME}:latest ${IMAGE_NAME}:${VERSION}
+time docker tag ${IMAGE_NAME}:latest${BUILD_ENV} ${IMAGE_NAME}:${VERSION}${BUILD_ENV}
 
 # Show the list of available images for this repo
 echo "Image is ready"
