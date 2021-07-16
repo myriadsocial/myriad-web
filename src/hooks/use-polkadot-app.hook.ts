da2150b226caeff2700e440ab74affed5186def2
mucktar <mucktar.pakpahan@gmail.com>
2021-07-16 09:57:45 +0700

    MYR-000: update env usage

--- src/hooks/use-polkadot-app.hook.ts
+++ src/hooks/use-polkadot-app.hook.ts
@@ -12,7 +12,7 @@ export const usePolkadotExtension = () => {
   const getPolkadotAccounts = async () => {
     const { web3Accounts } = await import('@polkadot/extension-dapp');
 
-    const format = process.env.NEXT_PUBLIC_MYRIAD_CRYPTO_TYPE ? Number(process.env.NEXT_PUBLIC_MYRIAD_CRYPTO_TYPE) : 214;
+    const format = process.env.NEXT_PUBLIC_MYRIAD_ADDRESS_PREFIX ? Number(process.env.NEXT_PUBLIC_MYRIAD_ADDRESS_PREFIX) : 214;
 
     const allAccounts = await web3Accounts({
       ss58Format: format
