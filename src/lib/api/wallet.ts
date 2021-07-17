cdc0c3ec47fa2cbb9a9e18fe8d35c7887e5daf06
ical10 <mochhusnir@gmail.com>
2021-07-17 16:07:20 +0700

    MYR-384: Unused Code - removed unused interface

--- src/lib/api/wallet.ts
+++ src/lib/api/wallet.ts
@@ -1,12 +1,11 @@
 import Axios from 'axios';
-import { Wallet } from 'src/interfaces/wallet';
 
 const MyriadAPI = Axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL
 });
 
-export const getWalletAddress = async (postId: string): Promise<Wallet> => {
-  const { data } = await MyriadAPI.request<Wallet>({
+export const getWalletAddress = async (postId: string) => {
+  const { data } = await MyriadAPI({
     url: `/posts/${postId}/walletaddress`,
     method: 'GET'
   });
