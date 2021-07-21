1e38c8867675e31b0f106635a95143a2a2b16143
ical10 <mochhusnir@gmail.com>
2021-07-21 17:15:37 +0700

    MYR-287: Added Children Props - added children as props

--- src/components/profile/myWallet/tipping/styledTabs.component.tsx
+++ src/components/profile/myWallet/tipping/styledTabs.component.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
 import Tabs from '@material-ui/core/Tabs';
+<<<<<<< HEAD
 import {withStyles} from '@material-ui/core/styles';
+=======
+import {makeStyles, createStyles} from '@material-ui/core/styles';
+>>>>>>> 68bf2eb (MYR-287: Added Children Props - added children as props)
 
 interface StyledTabsProps {
   value: number;
   onChange: (event: React.ChangeEvent<{}>, newValue: number) => void;
+  children?: React.ReactNode;
 }
 
+<<<<<<< HEAD
 export const StyledTabs = withStyles({
   indicator: {
     display: 'flex',
@@ -15,3 +21,23 @@ export const StyledTabs = withStyles({
     backgroundColor: 'transparent',
   },
 })((props: StyledTabsProps) => <Tabs {...props} TabIndicatorProps={{children: <span />}} />);
+=======
+const StyledTabsComponent = ({value, onChange, children}: StyledTabsProps) => {
+  return (
+    <Tabs
+      value={value}
+      onChange={onChange}
+      TabIndicatorProps={{
+        style: {
+          display: 'flex',
+          justifyContent: 'center',
+          backgroundColor: 'transparent',
+        },
+      }}>
+      {children}
+    </Tabs>
+  );
+};
+
+export default StyledTabsComponent;
+>>>>>>> 68bf2eb (MYR-287: Added Children Props - added children as props)
