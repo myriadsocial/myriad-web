6f304cfe5deae3962931b992aaee1f6892103393
mucktar <mucktar.pakpahan@gmail.com>
2021-07-22 22:06:32 +0700

    MYR-449: update connect social success message

--- src/helpers/string.ts
+++ src/helpers/string.ts
@@ -2,3 +2,9 @@ export const acronym = (value: string): string => {
   if (!value) return '';
   return value.split(/\s/).reduce((response, word) => (response += word.slice(0, 1)), '');
 };
+
+export const capitalize = (text: string): string => {
+  const word = text.toLowerCase();
+
+  return word[0].toUpperCase() + word.substring(1);
+};
