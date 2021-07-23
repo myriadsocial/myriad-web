b1ff325224ce49ce00e44ced550e27d183c1904e
imam hermawan <73300321+immhrmwn@users.noreply.github.com>
2021-07-23 16:05:14 +0700

    Update src/reducers/timeline/actions.ts
    fix wording variable
    
    Co-authored-by: Rei <dynamicfrogman@gmail.com>

--- src/reducers/timeline/actions.ts
+++ src/reducers/timeline/actions.ts
@@ -34,7 +34,7 @@ export interface LikePost extends Action {
   postId: string;
 }
 
-export interface UnlikePost extends Action {
+export interface UnLikePost extends Action {
   type: constants.UNLIKE_POST;
   postId: string;
 }
