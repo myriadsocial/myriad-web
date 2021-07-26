fa2f94a391487bb4fd065f71a98cebf4699f807d
ical10 <mochhusnir@gmail.com>
2021-07-26 16:27:03 +0700

    MYR-476: Add Missing Import - added useRef

--- src/components/post/comment/comment.component.tsx
+++ src/components/post/comment/comment.component.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {useSelector} from 'react-redux';
 
 import {useRouter} from 'next/router';
