d9faaa06997213ea01ae15f558d74b8f112c50de
imam hermawan <immd.flores@gmail.com>
2021-07-26 09:21:33 +0700

    MYR-461: Change the wording for 'Demo Version' modal message

--- src/components/common/banner-demo.component.tsx
+++ src/components/common/banner-demo.component.tsx
@@ -109,12 +109,15 @@ const BannerDemo: React.FC = () => {
             Great Things Take Time
           </Typography>
           <Typography className={`${style.paragraph} ${style.mb}`}>
-            This current demo isn't a fully functional product yet. We’re still working to
-            connecting the blocks . This means you may stumble upon many changes and the occasional
-            bug here and there.
+            Welcome to the Myriad MVP (Minimum Viable Product)! The app you are currently viewing is
+            still under heavy development and is not a fully functional product yet. We’re still
+            working on connecting the blocks (blockchains?) which means the app will continuously
+            change and you may come across the occasional bug. You can subscribe to our mailing list
+            where we’ll keep you updated on the latest developments and when we’ve moved to the
+            (hopefully) bug-free Alpha version.
           </Typography>
           <Typography className={`${style.paragraph} ${style.mb}`}>
-            Feel free to click around, and don't forget to join our :
+            Feel free to click around, and don{"'"}t forget to join our:
           </Typography>
           <div className={`${style.mb} ${style['flex-col']}`}>
             <Button
@@ -135,8 +138,9 @@ const BannerDemo: React.FC = () => {
             </Button>
           </div>
           <Typography className={`${style.paragraph} ${style.mb}`}>
-            Contact us if you have any query, feedback, find some bug or just want to say hi.
-            Anything from you will make this a better app, Thank you!
+            Contact us if you have any queries, feedback, bug reports or if you just want to say
+            “Hi!”, we’d love to hear from you! Being a Web 3.0 Decentralized Application, any input
+            from our beloved users will help us build a better app. Thank you!
           </Typography>
         </DialogContent>
       </Dialog>
