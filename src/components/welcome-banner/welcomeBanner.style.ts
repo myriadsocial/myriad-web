42173d2c1a6b18f098fb950bad4e4c56ca422fc2
imam hermawan <immd.flores@gmail.com>
2021-07-30 16:03:52 +0700

    MYR-444: update pop banner welcome after tour

--- src/components/welcome-banner/welcomeBanner.style.ts
+++ src/components/welcome-banner/welcomeBanner.style.ts
@@ -6,5 +6,25 @@ export const useStyles = makeStyles((theme: Theme) =>
       justifyContent: 'center',
       background: theme.palette.background.default,
     },
+    paragraph: {
+      letterSpacing: '0em',
+      lineHeight: '28px',
+      fontWeight: 400,
+      color: '#4B4851',
+      fontSize: 16,
+    },
+    center: {
+      textAlign: 'center',
+    },
+    mb: {
+      marginBottom: theme.spacing(1),
+    },
+    mb2: {
+      marginBottom: theme.spacing(4),
+    },
+    primary: {
+      fontWeight: 'bold',
+      color: '#8629E9',
+    },
   }),
 );
