1b168fbb184f07aa5f08fd157cf96709d2f33923
mucktar <mucktar.pakpahan@gmail.com>
2021-08-04 23:25:41 +0700

    MYR-513: enable add and delete photos during post creation

--- src/components/post/create/create-post-expanded.component.tsx
+++ src/components/post/create/create-post-expanded.component.tsx
@@ -1,4 +1,4 @@
-import React, {forwardRef, useImperativeHandle, useState, useRef} from 'react';
+import React, {useState, useRef} from 'react';
 
 import {Tooltip} from '@material-ui/core';
 import Avatar from '@material-ui/core/Avatar';
@@ -23,241 +23,245 @@ import {PreviewImageComponent} from './preview-image.component';
 import ShowIf from 'src/components/common/show-if.component';
 import {acronym} from 'src/helpers/string';
 import {Experience} from 'src/interfaces/experience';
+import {Post} from 'src/interfaces/post';
+import {UpoadedFile} from 'src/interfaces/post';
 import {User} from 'src/interfaces/user';
 import theme from 'src/themes/default';
 
-type UpoadedFile = {
-  file: File;
-  preview: string;
-};
-
-type CreatePostExpandedComponentProps = {
+type CreatePostExpandedProps = {
   user: User;
-  text: string;
-  onSubmit: (text: string, tags: string[], files: File[]) => void;
-  experiences: Experience[];
+  post: Partial<Post> | null;
+  onSubmit: (post: Partial<Post>, images: File[]) => void;
+  experiences?: Experience[];
 };
 
-export type CreatePostExpandedComponentRefProps = {
-  clearForm: () => void;
-};
+const CreatePostExpandedComponent: React.FC<CreatePostExpandedProps> = ({
+  onSubmit,
+  post: initialPost,
+  user,
+}) => {
+  const styles = useStyles();
 
-export const CreatePostExpandedComponent = forwardRef(
-  (
-    {onSubmit, text, user}: CreatePostExpandedComponentProps,
-    ref: React.Ref<CreatePostExpandedComponentRefProps>,
+  const uploadImageRef = useRef<HTMLInputElement | null>(null);
+  const uploadVideoRef = useRef<HTMLInputElement | null>(null);
+  const [images, setImages] = useState<UpoadedFile[]>([]);
+  const [post, setPost] = useState<Partial<Post> | null>(initialPost);
+  const [submitting, setSubmitting] = useState(false);
+
+  const updatePostText = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const text = event.target.value;
+
+    setPost(prevPost => ({
+      ...prevPost,
+      text,
+    }));
+  };
+
+  const selectVideo = (): void => {
+    const uploadField: any = uploadVideoRef?.current;
+
+    if (!uploadField) return;
+
+    uploadField.click();
+  };
+
+  const selectImages = (): void => {
+    const uploadField: any = uploadImageRef?.current;
+
+    if (!uploadField) return;
+
+    uploadField.click();
+  };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files.length > 0) {
+      const files = event.target.files;
+
+      setImages(prevImages => [
+        ...prevImages,
+        ...Array.from(files)
+          .filter(file => file.name.match(/\.(jpg|jpeg|png|gif)$/))
+          .map((file: File) => ({
+            file,
+            preview: URL.createObjectURL(file),
+          })),
+      ]);
+    }
+  };
+
+  const addToTags = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    if (event.code === 'Enter') {
+      console.log('addToTags', event.target);
+    }
+  };
+
+  const handleTagsChange = (
+    event: React.ChangeEvent<{}>,
+    value: (string | string[])[],
+    reason: AutocompleteChangeReason,
   ) => {
-    const styles = useStyles();
-
-    const uploadImageRef = useRef<HTMLInputElement | null>(null);
-    const uploadVideoRef = useRef<HTMLInputElement | null>(null);
-    const [files, setFiles] = useState<UpoadedFile[]>([]);
-    const [postText, setPostText] = useState(text);
-    const [tags, setTags] = useState<string[]>([]);
-    const [submitting, setSubmitting] = useState(false);
-
-    useImperativeHandle(
-      ref,
-      () => ({
-        clearForm() {
-          setPostText('');
-          setFiles([]);
-        },
-      }),
-      [],
-    );
-
-    const updatePostText = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-      const text = event.target.value;
-
-      setPostText(text);
-    };
-
-    const selectVideo = (): void => {
-      const uploadField: any = uploadVideoRef?.current;
-
-      if (!uploadField) return;
-
-      uploadField.click();
-    };
-
-    const selectImages = (): void => {
-      const uploadField: any = uploadImageRef?.current;
-
-      if (!uploadField) return;
-
-      uploadField.click();
-    };
-
-    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      if (event.target.files && event.target.files.length > 0) {
-        const files = event.target.files;
-
-        setFiles(
-          Array.from(files)
-            .filter(file => file.name.match(/\.(jpg|jpeg|png|gif)$/))
-            .map((file: File) => ({
-              file,
-              preview: URL.createObjectURL(file),
-            })),
-        );
-      }
-    };
-
-    const addToTags = (event: React.KeyboardEvent<HTMLDivElement>) => {
-      if (event.code === 'Enter') {
-        console.log('addToTags', event.target);
-      }
-    };
-
-    const handleTagsChange = (
-      event: React.ChangeEvent<{}>,
-      value: (string | string[])[],
-      reason: AutocompleteChangeReason,
-    ) => {
-      if (reason === 'remove-option') {
-        if (typeof value === 'string') {
-          setTags([value]);
-        }
-
-        if (typeof value === 'object') {
-          setTags(value.flat());
-        }
+    if (reason === 'remove-option') {
+      if (typeof value === 'string') {
+        setPost(prevPost => ({
+          ...prevPost,
+          tags: [value],
+        }));
       }
 
-      if (reason === 'create-option') {
-        const createdTags: string[] = [];
-
-        if (typeof value === 'string') {
-          createdTags.push(value);
-        }
-
-        if (typeof value === 'object') {
-          createdTags.push(...value.flat());
-        }
-
-        setTags([...createdTags]);
+      if (typeof value === 'object') {
+        setPost(prevPost => ({
+          ...prevPost,
+          tags: value.flat(),
+        }));
       }
-    };
+    }
 
-    const savePost = () => {
-      setSubmitting(true);
+    if (reason === 'create-option') {
+      const createdTags: string[] = [];
 
+      if (typeof value === 'string') {
+        createdTags.push(value);
+      }
+
+      if (typeof value === 'object') {
+        createdTags.push(...value.flat());
+      }
+
+      setPost(prevPost => ({
+        ...prevPost,
+        tags: createdTags,
+      }));
+    }
+  };
+
+  const handleRemoveImage = (index: number) => {
+    const prevImages = [...images];
+
+    prevImages.splice(index, 1);
+
+    setImages(prevImages);
+  };
+
+  const savePost = () => {
+    setSubmitting(true);
+
+    if (post) {
       onSubmit(
-        postText,
-        tags,
-        files.map(file => file.file),
+        post,
+        images.map(image => image.file),
       );
-    };
+    }
+  };
 
-    return (
-      <Card className={styles.card}>
-        <CardHeader
-          className={styles.cardHeader}
-          avatar={
-            <Avatar
-              aria-label={user.name}
-              src={user.profilePictureURL}
-              style={{height: 55, width: 55}}>
-              {acronym(user.name)}
-            </Avatar>
-          }
-          action={
-            <Tooltip title="Coming soon" arrow>
-              <Button aria-label="post-settings" variant="contained" size="medium" color="primary">
-                Post Setting
-              </Button>
-            </Tooltip>
-          }
-          title={user.name}
-          subheader=""
+  return (
+    <Card className={styles.card}>
+      <CardHeader
+        className={styles.cardHeader}
+        avatar={
+          <Avatar
+            aria-label={user.name}
+            src={user.profilePictureURL}
+            style={{height: 55, width: 55}}>
+            {acronym(user.name)}
+          </Avatar>
+        }
+        action={
+          <Tooltip title="Coming soon" arrow>
+            <Button aria-label="post-settings" variant="contained" size="medium" color="primary">
+              Post Setting
+            </Button>
+          </Tooltip>
+        }
+        title={user.name}
+        subheader=""
+      />
+      <CardContent style={{padding: theme.spacing(1)}}>
+        <TextareaAutosize
+          rowsMin={5}
+          placeholder={`What's on your mind ${user.name}?`}
+          className={styles.postTextArea}
+          spellCheck={false}
+          value={post?.text}
+          onChange={updatePostText}
         />
-        <CardContent style={{padding: theme.spacing(1)}}>
-          <TextareaAutosize
-            rowsMin={5}
-            placeholder={`What's on your mind ${user.name}?`}
-            className={styles.postTextArea}
-            spellCheck={false}
-            value={postText}
-            onChange={updatePostText}
-          />
 
-          <Autocomplete
-            id="post-tags"
-            className={styles.tags}
-            freeSolo
+        <Autocomplete
+          id="post-tags"
+          className={styles.tags}
+          freeSolo
+          multiple
+          style={{paddingTop: 8}}
+          value={post?.tags}
+          options={[]}
+          disableClearable
+          onChange={handleTagsChange}
+          renderInput={params => (
+            <TextField
+              {...params}
+              placeholder="# Add Tags"
+              variant="outlined"
+              onKeyDown={addToTags}
+            />
+          )}
+        />
+
+        <div className={styles.additionalAction}>
+          <Typography variant="caption" style={{marginRight: 24}}>
+            Add something to your post
+          </Typography>
+          <input
+            type="file"
             multiple
-            style={{paddingTop: 8}}
-            value={tags}
-            options={[]}
-            disableClearable
-            onChange={handleTagsChange}
-            renderInput={params => (
-              <TextField
-                {...params}
-                placeholder="# Add Tags"
-                variant="outlined"
-                onKeyDown={addToTags}
-              />
-            )}
+            ref={uploadImageRef}
+            onChange={handleFileChange}
+            style={{display: 'none'}}
+            accept="image/*"
           />
-
-          <div className={styles.additionalAction}>
-            <Typography variant="caption" style={{marginRight: 24}}>
-              Add something to your post
-            </Typography>
-            <input
-              type="file"
-              multiple
-              ref={uploadImageRef}
-              onChange={handleFileChange}
-              style={{display: 'none'}}
-              accept="image/*"
-            />
-            <IconButton color="primary" aria-label="upload images" onClick={selectImages}>
-              <ImageIcon />
+          <IconButton color="primary" aria-label="upload images" onClick={selectImages}>
+            <ImageIcon />
+          </IconButton>
+          <input
+            type="file"
+            multiple
+            ref={uploadVideoRef}
+            onChange={handleFileChange}
+            style={{display: 'none'}}
+            accept="image/*"
+          />
+          <Tooltip title="Coming soon" arrow>
+            <IconButton color="primary" aria-label="upload-video" onClick={selectVideo}>
+              <VideocamIcon />
             </IconButton>
-            <input
-              type="file"
-              multiple
-              ref={uploadVideoRef}
-              onChange={handleFileChange}
-              style={{display: 'none'}}
-              accept="image/*"
-            />
-            <Tooltip title="Coming soon" arrow>
-              <IconButton color="primary" aria-label="upload-video" onClick={selectVideo}>
-                <VideocamIcon />
-              </IconButton>
-            </Tooltip>
-            <Tooltip title="Coming soon" arrow>
-              <IconButton color="primary" aria-label="add-people">
-                <PeopleIcon />
-              </IconButton>
-            </Tooltip>
-            <Tooltip title="Coming soon" arrow>
-              <IconButton color="primary" aria-label="add-link">
-                <LinkIcon />
-              </IconButton>
-            </Tooltip>
-          </div>
+          </Tooltip>
+          <Tooltip title="Coming soon" arrow>
+            <IconButton color="primary" aria-label="add-people">
+              <PeopleIcon />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Coming soon" arrow>
+            <IconButton color="primary" aria-label="add-link">
+              <LinkIcon />
+            </IconButton>
+          </Tooltip>
+        </div>
 
-          <ShowIf condition={files.length > 0}>
-            <PreviewImageComponent files={files} />
-          </ShowIf>
-        </CardContent>
-        <CardActions className={styles.action}>
-          <Button
-            variant="contained"
-            size="large"
-            color="primary"
-            disabled={submitting || postText.length === 0}
-            className={styles.postButton}
-            onClick={savePost}>
-            Post Now
-          </Button>
-        </CardActions>
-      </Card>
-    );
-  },
-);
+        <ShowIf condition={images.length > 0}>
+          <PreviewImageComponent files={images} onRemoveItem={handleRemoveImage} />
+        </ShowIf>
+      </CardContent>
+      <CardActions className={styles.action}>
+        <Button
+          variant="contained"
+          size="large"
+          color="primary"
+          disabled={submitting || !post?.text || post.text.length === 0}
+          className={styles.postButton}
+          onClick={savePost}>
+          Post Now
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default CreatePostExpandedComponent;
