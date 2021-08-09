bc895ca8646d6fa2f0cb147e9263b8e3e0c56371
ical10 <mochhusnir@gmail.com>
2021-08-09 15:09:52 +0700

    MYR-482: created redux interface for balance state

--- src/reducers/balance/reducer.ts
+++ src/reducers/balance/reducer.ts
@@ -1,9 +1,11 @@
 import {HYDRATE} from 'next-redux-wrapper';
 
+import * as BaseConstants from '../base/constants';
 import {State as BaseState} from '../base/state';
 import {Actions} from './actions';
 import * as constants from './constants';
 
+import update from 'immutability-helper';
 import * as Redux from 'redux';
 import {BalanceDetail} from 'src/interfaces/balance';
 
@@ -25,11 +27,17 @@ export const BalanceReducer: Redux.Reducer<BalanceState, Actions> = (
       return action.payload.balanceState;
     }
 
-    case constants.INIT_BALANCE: {
+    case constants.FETCH_BALANCES: {
       return {
         ...state,
         balanceDetails: action.balanceDetails,
       };
+    }
+
+    case BaseConstants.ACTION_LOADING: {
+      return update(state, {
+        loading: {$set: action.loading},
+      });
     }
 
     default: {
