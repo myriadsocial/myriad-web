d5fa78a759f6b328b5d58c8f6fa4562d91f9ebe2
ical10 <mochhusnir@gmail.com>
2021-08-10 14:33:44 +0700

    MYR-482: changed variable names to match interface

--- src/lib/api/polkadot-js.ts
+++ src/lib/api/polkadot-js.ts
@@ -11,17 +11,17 @@ export const connectToBlockchain = async (wsProvider: string): Promise<ApiPromis
 };
 
 interface signAndSendExtrinsicProps {
-  fromAddress: string;
-  toAddress: string;
-  amount: number;
+  from: string;
+  to: string;
+  value: number;
   currencyId: string;
   wsAddress: string;
 }
 
 export const signAndSendExtrinsic = async ({
-  fromAddress,
-  toAddress,
-  amount,
+  from,
+  to,
+  value,
   currencyId,
   wsAddress,
 }: signAndSendExtrinsicProps): Promise<string | null> => {
@@ -34,7 +34,7 @@ export const signAndSendExtrinsic = async ({
     const keyring = new Keyring();
 
     const baseAddress = keyring.encodeAddress(
-      fromAddress,
+      from,
       Number(process.env.NEXT_PUBLIC_MYRIAD_ADDRESS_PREFIX),
     );
 
@@ -61,8 +61,8 @@ export const signAndSendExtrinsic = async ({
           // here we use the api to create a balance transfer to some account of a value of 12345678
           const transferExtrinsic =
             currencyId === 'ACA'
-              ? api.tx.balances.transfer(toAddress, amount)
-              : api.tx.currencies.transfer(toAddress, {TOKEN: currencyId}, amount);
+              ? api.tx.balances.transfer(to, value)
+              : api.tx.currencies.transfer(to, {TOKEN: currencyId}, value);
 
           // to be able to retrieve the signer interface from this account
           // we can use web3FromSource which will return an InjectedExtension type
@@ -72,7 +72,7 @@ export const signAndSendExtrinsic = async ({
             // passing the injected account address as the first argument of signAndSend
             // will allow the api to retrieve the signer and the user will see the extension
             // popup asking to sign the balance transfer transaction
-            const txInfo = await transferExtrinsic.signAndSend(fromAddress, {
+            const txInfo = await transferExtrinsic.signAndSend(from, {
               signer: injector.signer,
             });
 
