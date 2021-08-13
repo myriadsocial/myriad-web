cc8c9be004a7650f086770326d01927ab0679a41
Mucktar Pakpahan <mucktar.pakpahan@mclinica.com>
2021-08-13 12:26:45 +0700

    update post interface

--- src/lib/api/post.ts
+++ src/lib/api/post.ts
@@ -1,26 +1,30 @@
+import {BaseList} from './interfaces/base-list.interface';
+
 import Axios from 'axios';
 import {Post, Comment, CreateCommentProps, ImportPost, Like, Dislike} from 'src/interfaces/post';
 import {TimelineSortMethod, TimelineFilter} from 'src/interfaces/timeline';
 
 const MyriadAPI = Axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: 'http://34.101.229.56:3007',
 });
 
 const LIMIT = 10;
+
+type PostList = BaseList<Post>;
 
 export const getPost = async (
   page: number,
   sort?: TimelineSortMethod,
   filters?: TimelineFilter,
 ): Promise<Post[]> => {
-  let orderField = 'platformCreatedAt';
+  let orderField = 'originCreatedAt';
 
   switch (sort) {
     case 'comment':
-      orderField = 'publicMetric.comment';
+      orderField = 'metric.comment';
       break;
     case 'like':
-      orderField = 'publicMetric.liked';
+      orderField = 'metric.liked';
       break;
     case 'trending':
     default:
@@ -79,7 +83,10 @@ export const getPost = async (
             },
           },
           {
-            relation: 'publicMetric',
+            relation: 'user',
+          },
+          {
+            relation: 'people',
           },
         ],
       },
@@ -93,9 +100,9 @@ export const getFriendPost = async (
   userId: string,
   page: number,
   sort?: TimelineSortMethod,
-): Promise<Post[]> => {
-  const path = `/users/${userId}/timeline`;
-  let orderField = 'platformCreatedAt';
+): Promise<PostList> => {
+  const path = `/posts`;
+  let orderField = 'platformCreatedAt DESC';
 
   if (sort) {
     switch (sort) {
@@ -111,13 +118,16 @@ export const getFriendPost = async (
     }
   }
 
-  const {data} = await MyriadAPI.request<Post[]>({
+  const {data} = await MyriadAPI.request<PostList>({
     url: path,
     method: 'GET',
     params: {
-      offset: Math.max(page - 1, 0) * LIMIT,
-      limit: LIMIT,
-      orderField,
+      filter: {
+        page,
+        limit: LIMIT,
+        orderField,
+        include: ['user', 'people'],
+      },
     },
   });
 
