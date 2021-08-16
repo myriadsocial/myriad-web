30a9d2a64bf79198801a13393e160070a7fba77d
Mucktar Pakpahan <mucktar.pakpahan@mclinica.com>
2021-08-16 11:59:41 +0700

    like and dislike with new schema

--- src/lib/api/interaction.ts
+++ src/lib/api/interaction.ts
@@ -8,6 +8,7 @@ export const like = async (userId: string, reference: Post | Comment): Promise<L
   const type = 'platform' in reference ? ReferenceType.POST : ReferenceType.COMMENT;
 
   const attributes: LikeProps = {
+    state: true,
     userId,
     type,
     referenceId: reference.id,
@@ -22,18 +23,28 @@ export const like = async (userId: string, reference: Post | Comment): Promise<L
   return data;
 };
 
-export const dislike = async (userId: string, reference: Post | Comment): Promise<void> => {
+export const dislike = async (userId: string, reference: Post | Comment): Promise<Like> => {
   const type = 'platform' in reference ? ReferenceType.POST : ReferenceType.COMMENT;
 
   const attributes: LikeProps = {
+    state: false,
     userId,
     type,
     referenceId: reference.id,
   };
 
-  await MyriadAPI.request({
-    url: `/dislikes`,
+  const {data} = await MyriadAPI.request<Like>({
+    url: `/likes`,
     method: 'POST',
     data: attributes,
+  });
+
+  return data;
+};
+
+export const removeLike = async (id: string): Promise<void> => {
+  await MyriadAPI.request({
+    url: `/likes/${id}`,
+    method: 'DELETE',
   });
 };
