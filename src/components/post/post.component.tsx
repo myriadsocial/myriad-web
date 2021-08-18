29b43153d1dd5759c26e7529acf6131e14e5ebad
ical10 <mochhusnir@gmail.com>
2021-08-18 17:50:42 +0700

    MYR-000: properly checked isTippingEnabled

--- src/components/post/post.component.tsx
+++ src/components/post/post.component.tsx
@@ -157,6 +157,15 @@ const PostComponent: React.FC<PostComponentProps> = ({
     });
   };
 
+  const isTippingEnabled = () => {
+    if (anonymous) {
+      return false;
+    } else if (user) {
+      if (!isOwnPost(post, user)) return true;
+      return false;
+    }
+  };
+
   return (
     <>
       <Card className={style.root}>
@@ -248,7 +257,7 @@ const PostComponent: React.FC<PostComponentProps> = ({
           <PostActionComponent
             metric={post.metric}
             commentExpanded={expanded}
-            tippingEnabled={!isOwnPost(post, user)}
+            tippingEnabled={isTippingEnabled()}
             expandComment={handleExpandClick}
             likePost={likePostHandle}
             dislikePost={dislikePostHandle}
