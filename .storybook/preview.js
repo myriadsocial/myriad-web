439333ce03fafd68f2280888c235c63f092552f1
ical10 <mochhusnir@gmail.com>
2021-08-31 12:26:08 +0700

    MYR-714: integrated StyledThemeProvider and ThemeProvider

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,33 @@
+import React from 'react';
+
+import {ThemeProvider, StylesProvider} from '@material-ui/styles';
+import {ThemeProvider as StyledThemeProvider} from 'styled-components';
+
+
+import LightTheme from '../src/themes/light-theme';
+
+// Global decorator to apply the styles to all stories
+export const decorators = [
+  Story => (
+    <>
+      <StylesProvider injectFirst>
+        <StyledThemeProvider theme={LightTheme}>
+          <ThemeProvider theme={LightTheme}>
+            <Story />
+          </ThemeProvider>
+        </StyledThemeProvider>
+      </StylesProvider>
+    </>
+  ),
+];
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: {argTypesRegex: '^on[A-Z].*'},
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
