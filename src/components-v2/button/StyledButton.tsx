cae7cd93b1abb0019275fec884bbc7b1eae86a38
ical10 <mochhusnir@gmail.com>
2021-08-31 11:45:12 +0700

    MYR-714: created styled MUI button and its stories

---
+++ src/components-v2/button/StyledButton.tsx
@@ -0,0 +1,15 @@
+import MatButton from '@material-ui/core/Button';
+
+import styled from 'styled-components';
+
+const StyledButton = styled(MatButton)`
+  background: linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%);
+  border-radius: 3px;
+  border: 0;
+  color: white;
+  height: 48px;
+  padding: 0 30px;
+  box-shadow: 0 3px 5px 2px rgba(255, 105, 135, 0.3);
+`;
+
+export default StyledButton;
