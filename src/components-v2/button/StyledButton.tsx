9761bdf80fee5b9bd8a4e91853b1c9bcf66c24a7
ical10 <mochhusnir@gmail.com>
2021-08-31 12:30:21 +0700

    MYR-714: changed color to see if StylesProvider works

--- src/components-v2/button/StyledButton.tsx
+++ src/components-v2/button/StyledButton.tsx
@@ -6,7 +6,7 @@ const StyledButton = styled(MatButton)`
   background: linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%);
   border-radius: 3px;
   border: 0;
-  color: white;
+  color: yellow;
   height: 48px;
   padding: 0 30px;
   box-shadow: 0 3px 5px 2px rgba(255, 105, 135, 0.3);
