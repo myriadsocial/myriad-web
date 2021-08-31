bbaa1a8c64d6a77e2f8a5cacd7c18c212eca485b
ical10 <mochhusnir@gmail.com>
2021-08-31 22:20:01 +0700

    MYR-736: defined reusable colors and button CSS props

--- src/themes/light-theme-v2.ts
+++ src/themes/light-theme-v2.ts
@@ -32,16 +32,35 @@ declare module '@material-ui/core/styles' {
   }
 }
 
-const primaryPurple = '#6E3FC3';
-const primaryOrange = '#FFC857';
+//Primary colors
+const primaryPurple = '#7342CC';
+const primaryOrange = '#FFD24D';
 
 const primaryGreen = '#39BF87';
 const primaryRed = '#FE3333';
 const primaryBlue = '#1070CA';
 
+//Surface colors
+const surfacePurple = '#EBE0FF';
+const surfaceOrange = '#FFF2CC';
+
+const surfaceGreen = '#DAF1E6';
+const surfaceRed = '#FFD7D7';
+const surfaceBlue = '#CFE2F4';
+
+//Border colors
+const borderPurple = '#DECCFF';
+const borderOrange = '#FFECB2';
+
+const borderGreen = '#C2E7D5';
+const borderRed = '#FFBCBC';
+const borderBlue = '#AFCFED';
+
+//Font weight
 const textRegular = 400;
 const textMedium = 600;
 
+//Font size
 const textXs = 10;
 const textSm = 12;
 const textBs = 14;
@@ -102,8 +121,40 @@ const theme = createTheme({
     MuiButton: {
       root: {
         textTransform: 'none',
-        fontSize: 14,
+        boxSizing: 'border-box',
+        borderRadius: 20,
       },
+      sizeSmall: {
+        width: 180,
+        height: 40,
+      },
+      sizeLarge: {
+        width: 583,
+        height: 40,
+      },
+      containedPrimary: {
+        border: `1px solid ${primaryPurple}`,
+        backgroundColor: primaryPurple,
+        color: '#FFF',
+      },
+      containedSecondary: {
+        border: `1px solid ${borderPurple}`,
+        backgroundColor: `${surfacePurple}`,
+        color: primaryPurple,
+      },
+      outlinedPrimary: {
+        border: `1px solid ${primaryPurple}`,
+        backgroundColor: '#FFF',
+      },
+      outlinedSecondary: {
+        border: `1px solid ${primaryOrange}`,
+        backgroundColor: '#FFF',
+      },
+    },
+  },
+  props: {
+    MuiButton: {
+      disableElevation: true,
     },
   },
 });
