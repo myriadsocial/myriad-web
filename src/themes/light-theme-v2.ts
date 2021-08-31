572af1a8aad9a45894265373f067e208213c11f9
ical10 <mochhusnir@gmail.com>
2021-08-31 18:10:48 +0700

    MYR-736: added status main colors

--- src/themes/light-theme-v2.ts
+++ src/themes/light-theme-v2.ts
@@ -2,10 +2,55 @@ import '@fontsource/mulish';
 
 import {createTheme} from '@material-ui/core/styles';
 
+declare module '@material-ui/core/styles' {
+  interface Theme {
+    status: {
+      success: {
+        main: string;
+      };
+      warning: {
+        main: string;
+      };
+      info: {
+        main: string;
+      };
+    };
+  }
+  // allow configuration using `createTheme`
+  interface ThemeOptions {
+    status?: {
+      success?: {
+        main: string;
+      };
+      warning?: {
+        main: string;
+      };
+      info?: {
+        main: string;
+      };
+    };
+  }
+}
+
 const primaryPurple = '#6E3FC3';
 const primaryOrange = '#FFC857';
 
+const primaryGreen = '#39BF87';
+const primaryRed = '#FE3333';
+const primaryBlue = '#1070CA';
+
 const theme = createTheme({
+  status: {
+    success: {
+      main: primaryGreen,
+    },
+    warning: {
+      main: primaryRed,
+    },
+    info: {
+      main: primaryBlue,
+    },
+  },
   palette: {
     primary: {
       main: primaryPurple,
