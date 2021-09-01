a0a64aee24985d37f3ef178a7dac6df345de4432
Mucktar Pakpahan <mucktar.pakpahan@gmail.com>
2021-09-01 08:36:38 +0700

    MYR-703: box component

--- src/themes/default.ts
+++ src/themes/default.ts
@@ -1,169 +1,55 @@
-import {createMuiTheme, fade} from '@material-ui/core/styles';
+import {createMuiTheme} from '@material-ui/core/styles';
 
 // Create a theme instance.
 const theme = createMuiTheme({
   palette: {
-    type: 'dark',
-    primary: {
-      main: '#2D2D2D',
-      light: '#424242',
-    },
+    type: 'light',
     background: {
-      default: '#171717',
+      default: '#E5E5E5',
+    },
+    primary: {
+      main: '#6E3FC3',
+    },
+    secondary: {
+      main: '#FFC857',
     },
     text: {
-      primary: '#E0E0E0',
+      primary: '#12130F',
+      secondary: '#898888',
     },
   },
   typography: {
-    fontSize: 12,
-    caption: {
-      fontSize: 14,
-      fontWeight: 400,
-    },
-    h4: {
-      fontSize: 18,
-      fontWeight: 300,
-      lineHeight: '140%',
-    },
-    h5: {
-      fontSize: 16,
-      fontWeight: 300,
-    },
-    h6: {
-      fontSize: 12,
-      fontWeight: 'bold',
-    },
+    fontSize: 14,
   },
   overrides: {
     MuiCssBaseline: {
       '@global': {
         a: {
           textDecoration: 'none',
-          color: '#A942E9',
+          color: '#6E3FC3',
         },
         '*::-webkit-scrollbar': {
           width: '0.4em',
         },
         '*::-webkit-scrollbar-track': {
-          '-webkit-box-shadow': 'inset 0 0 6px #171717',
+          '-webkit-box-shadow': 'inset 0 0 6px #898888',
         },
         '*::-webkit-scrollbar-thumb': {
-          backgroundColor: '#A942E9',
-          outline: '1px solid #424242',
+          backgroundColor: '#6E3FC3',
+          outline: '1px solid #898888',
         },
       },
     },
 
-    MuiToolbar: {
-      gutters: {
-        paddingRight: 32,
-        paddingLeft: 32,
-      },
-    },
-
-    MuiDialog: {
-      scrollPaper: {
-        alignItems: 'flex-start',
-      },
-    },
-
-    MuiAvatar: {
+    MuiIconButton: {
       root: {
-        width: 32,
-        height: 32,
+        fontSize: 18,
       },
-    },
-    MuiInputBase: {
-      root: {
-        color: 'inherit',
-      },
-    },
-    MuiInput: {
-      colorSecondary: {
-        backgroundColor: fade('#FFF', 0.9),
-        color: '#2D2D2D',
-        '&.MuiInput-underline:after': {
-          borderBottomColor: '#E849BD',
-        },
-        '& .MuiInputBase-input': {
-          marginLeft: 4,
-        },
-      },
-    },
-    MuiFormLabel: {
-      colorSecondary: {
-        zIndex: 1,
-        marginLeft: 4,
-        color: fade('#000', 0.7),
-        '&.Mui-focused': {
-          marginLeft: 0,
-          color: '#E849BD',
-        },
-      },
-    },
-    MuiSwitch: {
+
       colorPrimary: {
-        '&&.Mui-checked': {
-          color: '#A942E9',
+        '&:hover': {
+          backgroundColor: 'inherit',
         },
-      },
-      colorSecondary: {
-        '&&.Mui-checked': {
-          color: '#171717',
-        },
-      },
-    },
-    MuiButton: {
-      sizeSmall: {
-        marginLeft: 4,
-        marginRight: 4,
-        padding: 4,
-        fontSize: 10,
-        textTransform: 'capitalize',
-      },
-      sizeLarge: {
-        borderRadius: 20,
-        marginBottom: 16,
-      },
-      containedPrimary: {
-        backgroundColor: '#8629E9',
-      },
-      containedSecondary: {
-        backgroundColor: '#B1AEB7',
-      },
-    },
-    MuiChip: {
-      colorPrimary: {
-        backgroundColor: '#A942E9',
-      },
-      colorSecondary: {
-        backgroundColor: '#171717',
-      },
-    },
-    MuiAccordion: {
-      root: {
-        background: 'transparent',
-        border: 0,
-        color: '#E0E0E0',
-      },
-    },
-    MuiFormControlLabel: {
-      label: {
-        fontSize: 12,
-        padding: '0 4px',
-      },
-    },
-    MuiCardHeader: {
-      root: {
-        borderBottom: `1px solid #171717`,
-        padding: '8px 16px',
-        fontSize: 14,
-      },
-    },
-    MuiTypography: {
-      colorSecondary: {
-        color: '#A942E9',
       },
     },
   },
