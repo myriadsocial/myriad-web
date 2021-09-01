142bac5ea782fdaa981c019f46057bce45a98124
Mucktar Pakpahan <mucktar.pakpahan@gmail.com>
2021-09-01 08:36:38 +0700

    MYR-703: box component

--- src/components-v2/SocialMediaList/index.tsx
+++ src/components-v2/SocialMediaList/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import {IconButton} from '@material-ui/core';
 import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
 
+<<<<<<< HEAD
 import {BoxComponent} from '../../components-v2/atoms/Box';
 import {SocialMedia} from '../../interfaces/social';
 import {SocialDetail, useSocialMediaList} from './use-social-media-list.hook';
@@ -11,6 +12,15 @@ type SocialMediaListProps = {
   connected: Array<SocialMedia>;
   toggleVerify: (social: SocialDetail) => void;
   openSocialPage: () => void;
+=======
+import {BoxComponent} from '../../components-v2/common/Box';
+import {SocialMedia, SocialsEnum} from '../../interfaces/social';
+import {useSocialMediaList} from './use-social-media-list.hook';
+
+type SocialMediaListProps = {
+  connected: Array<SocialMedia>;
+  toggleVerify: (social: SocialsEnum) => void;
+>>>>>>> da15b546 (MYR-703: box component)
 };
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -18,6 +28,7 @@ const useStyles = makeStyles((theme: Theme) =>
     root: {},
     list: {
       display: 'flex',
+<<<<<<< HEAD
       flexWrap: 'wrap',
       '& * >': {
         marginRight: theme.spacing(1),
@@ -37,19 +48,33 @@ const useStyles = makeStyles((theme: Theme) =>
         width: 24,
         height: 24,
       },
+=======
+    },
+    icon: {
+      borderRadius: 5,
+      background: theme.palette.primary.main,
+      marginRight: theme.spacing(1),
+      width: 24,
+      height: 24,
+>>>>>>> da15b546 (MYR-703: box component)
     },
   }),
 );
 
+<<<<<<< HEAD
 export const SocialMediaList: React.FC<SocialMediaListProps> = ({
   connected,
   toggleVerify,
   openSocialPage,
 }) => {
+=======
+export const SocialMediaList: React.FC<SocialMediaListProps> = ({connected, toggleVerify}) => {
+>>>>>>> da15b546 (MYR-703: box component)
   const styles = useStyles();
 
   const socials = useSocialMediaList(connected);
 
+<<<<<<< HEAD
   const handleSocialClicked = (social: SocialDetail) => () => {
     if (social.connected) {
       toggleVerify(social);
@@ -58,13 +83,26 @@ export const SocialMediaList: React.FC<SocialMediaListProps> = ({
 
   return (
     <BoxComponent title="Social Media" onClick={openSocialPage}>
+=======
+  const handleSocialClicked = (social: SocialsEnum) => () => {
+    toggleVerify(social);
+  };
+
+  return (
+    <BoxComponent title="Social Media">
+>>>>>>> da15b546 (MYR-703: box component)
       <div className={styles.list}>
         {socials.map(social => (
           <IconButton
             key={social.id}
             size="small"
+<<<<<<< HEAD
             color={social.connected ? 'primary' : 'default'}
             onClick={handleSocialClicked(social)}
+=======
+            color={social.connected ? 'primary' : 'secondary'}
+            onClick={handleSocialClicked(social.id)}
+>>>>>>> da15b546 (MYR-703: box component)
             className={styles.icon}>
             {social.icon}
           </IconButton>
---
+++ src/components-v2/common/Box/Box.stories.tsx
@@ -0,0 +1,51 @@
+import {ComponentStory, ComponentMeta} from '@storybook/react';
+
+import React from 'react';
+
+import {Button} from '@material-ui/core';
+
+import {BoxComponent} from '.';
+import FacebookIcon from '../../../images/socials/facebook.svg';
+
+export default {
+  title: 'UI Revamp v2.0/core/Box',
+  component: BoxComponent,
+  argTypes: {
+    backgroundColor: {control: 'color'},
+  },
+} as ComponentMeta<typeof BoxComponent>;
+
+const Template: ComponentStory<typeof BoxComponent> = args => <BoxComponent {...args} />;
+
+export const TextBox = Template.bind({});
+TextBox.args = {
+  title: 'Box with string',
+  children:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dapibus turpis id erat sagittis, a semper nibh sodales',
+};
+
+export const ComponentBox = Template.bind({});
+ComponentBox.args = {
+  title: 'Box with component',
+  children: (
+    <div
+      style={{
+        display: 'flex',
+        marginRight: 8,
+        flexDirection: 'row',
+      }}>
+      <Button variant="contained">
+        <FacebookIcon />
+      </Button>
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
+      <Button variant="contained" color="secondary">
+        Secondary
+      </Button>
+      <Button variant="contained" disabled>
+        Disabled
+      </Button>
+    </div>
+  ),
+};
