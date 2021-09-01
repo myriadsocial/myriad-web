d6c964330d2ad36509d4237de3fcaf7d544ec914
Mucktar Pakpahan <mucktar.pakpahan@gmail.com>
2021-09-01 17:21:47 +0700

    MYR-000: fix build error

--- src/components-v2/WalletBalance/index.tsx
+++ src/components-v2/WalletBalance/index.tsx
@@ -1,21 +1,14 @@
 import React from 'react';
 
-<<<<<<< HEAD
 import {useRouter} from 'next/router';
 
 import {Typography} from '@material-ui/core';
 import CircularProgress from '@material-ui/core/CircularProgress';
-import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
 
 import {BalanceDetail} from '../../interfaces/balance';
 import {BoxComponent} from '../atoms/Box';
 import {ListItemComponent} from '../atoms/ListItem';
-=======
 import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
-
-import {BoxComponent} from 'src/componentsV2/common/Box';
-import {BalanceDetail} from 'src/interfaces/balance';
->>>>>>> da15b546 (MYR-703: box component)
 
 type WalletProps = {
   balances: Array<BalanceDetail>;
@@ -24,22 +17,18 @@ type WalletProps = {
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {},
-<<<<<<< HEAD
     loading: {
       display: 'flex',
       justifyContent: 'center',
       alignItems: 'center',
       height: 360,
     },
-=======
->>>>>>> da15b546 (MYR-703: box component)
   }),
 );
 
 export const WalletBalances: React.FC<WalletProps> = ({balances}) => {
   const styles = useStyles();
 
-<<<<<<< HEAD
   const router = useRouter();
 
   const changeToWalletPage = () => {
@@ -64,7 +53,9 @@ export const WalletBalances: React.FC<WalletProps> = ({balances}) => {
       ))}
     </BoxComponent>
   );
-=======
-  return <BoxComponent title="Wallet">hi</BoxComponent>;
->>>>>>> da15b546 (MYR-703: box component)
+  return (
+    <BoxComponent title="Wallet" className={styles.root}>
+      hi
+    </BoxComponent>
+  );
 };
