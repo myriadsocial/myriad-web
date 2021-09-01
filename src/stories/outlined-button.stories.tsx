c416a5b2931e7e1b32f1518273aed368f9f2ae62
ical10 <mochhusnir@gmail.com>
2021-09-01 14:59:26 +0700

    MYR-736: created OutlinedButton and its stories

---
+++ src/stories/outlined-button.stories.tsx
@@ -0,0 +1,47 @@
+import {ComponentStory, ComponentMeta} from '@storybook/react';
+
+import React from 'react';
+
+import {OutlinedButton, ButtonSize, ButtonColor} from '../../src/components-v2/button';
+
+type ButtonSizeValue = `${ButtonSize}`;
+type ButtonColorValue = `${ButtonColor}`;
+
+const buttonSizes: ButtonSizeValue[] = Object.values(ButtonSize);
+const buttonColors: ButtonColorValue[] = Object.values(ButtonColor);
+
+export default {
+  title: 'UI Revamp v2.0/Outlined Button',
+  component: OutlinedButton,
+  argTypes: {
+    color: {
+      options: [...buttonColors],
+      control: {type: 'radio'},
+    },
+    size: {
+      options: [...buttonSizes],
+      control: {type: 'radio'},
+    },
+  },
+} as ComponentMeta<typeof OutlinedButton>;
+
+const Template: ComponentStory<typeof OutlinedButton> = args => <OutlinedButton {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  children: 'Default',
+};
+
+export const PrimarySmall = Template.bind({});
+PrimarySmall.args = {
+  color: ButtonColor.PRIMARY,
+  size: ButtonSize.SMALL,
+  children: 'Primary Small',
+};
+
+export const SecondaryLarge = Template.bind({});
+SecondaryLarge.args = {
+  color: ButtonColor.SECONDARY,
+  size: ButtonSize.LARGE,
+  children: 'Secondary Large',
+};
