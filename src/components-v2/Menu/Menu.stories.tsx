bf3c645cc31a382bb995e28482d35e1bee06282d
Mucktar Pakpahan <mucktar.pakpahan@gmail.com>
2021-09-02 21:42:42 +0700

    MYR-723: create menu component

---
+++ src/components-v2/Menu/Menu.stories.tsx
@@ -0,0 +1,16 @@
+import {ComponentStory, ComponentMeta} from '@storybook/react';
+
+import React from 'react';
+
+import {Menu} from '.';
+
+export default {
+  title: 'UI Revamp v2.0/component',
+  component: Menu,
+  argTypes: {},
+} as ComponentMeta<typeof Menu>;
+
+const Template: ComponentStory<typeof Menu> = args => <Menu {...args} />;
+
+export const SidebarMenu = Template.bind({});
+SidebarMenu.args = {};
