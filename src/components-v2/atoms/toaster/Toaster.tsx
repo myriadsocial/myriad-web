b91e351e8a92bacfb8ec41208d649228bf31a082
ical10 <mochhusnir@gmail.com>
2021-09-02 20:02:33 +0700

    MYR-728: fixed build error

--- src/components-v2/atoms/toaster/Toaster.tsx
+++ src/components-v2/atoms/toaster/Toaster.tsx
@@ -6,24 +6,12 @@ import Paper from '@material-ui/core/Paper';
 import Snackbar from '@material-ui/core/Snackbar';
 import SvgIcon from '@material-ui/core/SvgIcon';
 import Typography from '@material-ui/core/Typography';
-import {useTheme, createStyles, makeStyles} from '@material-ui/core/styles';
-import CloseIcon from '@material-ui/icons/Close';
+import {useTheme} from '@material-ui/core/styles';
 
 import {ToasterProps, Status} from './';
 
-export const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    paperOnToaster: {
-      boxSizing: 'border-box',
-      width: '100%', //or any percentage width you want
-      padding: 50,
-    },
-  }),
-);
-
 const Toaster = ({status, message}: ToasterProps): JSX.Element => {
   const theme = useTheme();
-  const classes = useStyles();
   const [open, setOpen] = React.useState(false);
 
   const handleClick = () => {
