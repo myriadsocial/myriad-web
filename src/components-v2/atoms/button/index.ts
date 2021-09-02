5ea949d20bb3dd4ec2f01811f957edd58d7c4a26
ical10 <mochhusnir@gmail.com>
2021-09-02 19:57:16 +0700

    MYR-728: removed StyledButton

--- src/components-v2/atoms/button/index.ts
+++ src/components-v2/atoms/button/index.ts
@@ -1,16 +1,7 @@
 import {default as ContainedButton} from './ContainedButton';
 import {default as OutlinedButton} from './OutlinedButton';
-import {default as StyledButton} from './StyledButton';
 import {ButtonSize, ButtonColor, buttonSizes, buttonColors, ButtonProps} from './button.interfaces';
 
-export {
-  ContainedButton,
-  OutlinedButton,
-  StyledButton,
-  ButtonSize,
-  ButtonColor,
-  buttonSizes,
-  buttonColors,
-};
+export {ContainedButton, OutlinedButton, ButtonSize, ButtonColor, buttonSizes, buttonColors};
 
 export type {ButtonProps};
