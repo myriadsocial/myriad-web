9b0235acc95831a87e906cac983f99eba8401649
ical10 <mochhusnir@gmail.com>
2021-09-03 14:59:04 +0700

    MYR-705: used debounced search

--- src/components-v2/atoms/search/SearchBox.tsx
+++ src/components-v2/atoms/search/SearchBox.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import {SearchIcon} from '@heroicons/react/solid';
+
+import React, {useState} from 'react';
 
 import IconButton from '@material-ui/core/IconButton';
 import InputBase from '@material-ui/core/InputBase';
 import Paper from '@material-ui/core/Paper';
+import SvgIcon from '@material-ui/core/SvgIcon';
 import {makeStyles, Theme, createStyles} from '@material-ui/core/styles';
-import SearchIcon from '@material-ui/icons/Search';
 
-import {InputBaseProps, InputBaseColor} from './';
+import {SearchBoxProps, SearchBoxColor} from './';
+
+import {debounce} from 'lodash';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -33,22 +37,47 @@ const useStyles = makeStyles((theme: Theme) =>
 );
 
 const SearchBox = ({
-  color = InputBaseColor.PRIMARY,
+  color = SearchBoxColor.PRIMARY,
   ariaLabel = 'search-box',
   placeholder = 'Search something here',
   isDisabled = false,
-}: InputBaseProps): JSX.Element => {
+  onSubmit,
+  ...props
+}: SearchBoxProps): JSX.Element => {
   const classes = useStyles();
 
+  const [input, setInput] = useState('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(event.target.value);
+  };
+
+  const submitSearch = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      const debouncedSubmit = debounce(() => {
+        onSubmit(input);
+      }, 500);
+
+      debouncedSubmit();
+    }
+  };
+
   return (
-    <Paper component="form" className={classes.root}>
-      <IconButton type="submit" className={classes.iconButton} aria-label="search">
-        <SearchIcon />
+    <Paper className={classes.root}>
+      <IconButton
+        className={classes.iconButton}
+        aria-label="search"
+        onClick={() => onSubmit(input)}>
+        <SvgIcon component={SearchIcon} viewBox="0 0 24 24" />
       </IconButton>
       <InputBase
+        onKeyUp={submitSearch}
         className={classes.input}
+        value={input}
+        onChange={handleChange}
         placeholder={placeholder}
         inputProps={{'aria-label': ariaLabel}}
+        {...props}
       />
     </Paper>
   );
