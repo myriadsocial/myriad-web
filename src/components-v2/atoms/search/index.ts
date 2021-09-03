70e6b6b7cb4a9fc92c7d306c04d57f3bc929e3c7
ical10 <mochhusnir@gmail.com>
2021-09-03 11:30:29 +0700

    MYR-705: moved search box to atoms folder

---
+++ src/components-v2/atoms/search/index.ts
@@ -0,0 +1,6 @@
+import {default as SearchBox} from './SearchBox';
+import {InputBaseColor, inputBaseColors, InputBaseProps} from './SearchBox.interfaces';
+
+export {SearchBox, InputBaseColor, inputBaseColors};
+
+export type {InputBaseProps};
