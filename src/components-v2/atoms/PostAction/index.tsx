0732ab4debfaf760a64998bf7d0b45f3c379d6c1
imam hermawan <immd.flores@gmail.com>
2021-09-05 22:52:53 +0700

    MYR-727: updated post - metric post component

--- src/components-v2/atoms/PostAction/index.tsx
+++ src/components-v2/atoms/PostAction/index.tsx
@@ -1,23 +1,27 @@
+import {ShareIcon} from '@heroicons/react/outline';
+import {action} from '@storybook/addon-actions';
+
 import React, {useState} from 'react';
 
 import {Typography} from '@material-ui/core';
 import {IconButton} from '@material-ui/core';
+import SvgIcon from '@material-ui/core/SvgIcon';
 import {createStyles, makeStyles} from '@material-ui/core/styles';
-import ForwardIcon from '@material-ui/icons/Forward';
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
 
 const useStyles = makeStyles(() =>
   createStyles({
     root: {},
-    flex: {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    flexColumn: {
-      flexDirection: 'column',
+    section: {
+      display: 'inline-block',
+      marginLeft: '5px',
+      marginRight: '5px',
     },
     action: {
       padding: 0,
+    },
+    text: {
+      fontSize: '12px',
     },
   }),
 );
@@ -26,17 +30,25 @@ type Props = {};
 
 export const PostActionComponent: React.FC<Props> = () => {
   const styles = useStyles();
-  const [share] = useState(99);
-  const [comment] = useState(99);
+  const [share] = useState(98);
+  const [comment] = useState(840);
   return (
     <div className={styles.root}>
-      <div>
-        <IconButton className={styles.action}></IconButton>
-        <Typography component="span">{comment} Comments</Typography>
+      <div className={styles.section}>
+        <IconButton onClick={action('comment')} className={styles.action} color="primary">
+          <ChatBubbleOutlineIcon />
+        </IconButton>
+        <Typography className={styles.text} component="span" color="textPrimary">
+          {comment} Comments
+        </Typography>
       </div>
-      <div>
-        <IconButton className={styles.action}></IconButton>
-        <Typography component="span">{share} Shares</Typography>
+      <div className={styles.section}>
+        <IconButton onClick={action('share')} className={styles.action} color="primary">
+          <SvgIcon component={ShareIcon} viewBox="0 0 24 24" />
+        </IconButton>
+        <Typography className={styles.text} component="span" color="textPrimary">
+          {share} Shares
+        </Typography>
       </div>
     </div>
   );
