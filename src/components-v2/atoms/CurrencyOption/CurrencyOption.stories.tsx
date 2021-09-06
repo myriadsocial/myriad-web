7d8382e1de771906137899a91fbc8741b7d8e3ec
imam hermawan <immd.flores@gmail.com>
2021-09-06 18:34:32 +0700

    MYR-707: move story to atoms folder

--- src/components-v2/atoms/CurrencyOption/CurrencyOption.stories.tsx
+++ src/components-v2/atoms/CurrencyOption/CurrencyOption.stories.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 import {CurrencyOptionComponent} from '.';
 
 export default {
-  title: 'UI Revamp v2.0/components/Currency option',
+  title: 'UI Revamp v2.0/atoms/Currency option',
   component: CurrencyOptionComponent,
 } as ComponentMeta<typeof CurrencyOptionComponent>;
 
