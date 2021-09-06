f161f5e119a1389e968054a775922c2d6580c1cd
imam hermawan <immd.flores@gmail.com>
2021-09-06 17:58:01 +0700

    MYR-707: move style component to own file .style.ts

--- src/components-v2/atoms/CurrencyOption/index.tsx
+++ src/components-v2/atoms/CurrencyOption/index.tsx
@@ -9,59 +9,16 @@ import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import SvgIcon from '@material-ui/core/SvgIcon';
 import Typography from '@material-ui/core/Typography';
-import {makeStyles, Theme, createStyles} from '@material-ui/core/styles';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
-import {debounce} from 'lodash';
+import {useStyles} from './currencyOption.style';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      width: '200px',
-      borderRadius: '10px',
-    },
-    header: {
-      boxSizing: 'border-box',
-      display: 'flex',
-      justifyContent: 'space-between',
-      width: '100%',
-      padding: '6px 16px',
-    },
-    tokenColumn: {
-      display: 'flex',
-      alignItems: 'center',
-      width: '100%',
-    },
-    flex: {
-      display: 'flex',
-      justifyContent: 'space-around',
-      alignItems: 'center',
-      width: '100%',
-    },
-    avatar: {
-      width: theme.spacing(1.5),
-      height: theme.spacing(1.5),
-      marginRight: theme.spacing(1),
-    },
-    text: {
-      fontWeight: 400,
-      fontSize: '12px',
-    },
-    input: {
-      width: '160px',
-      height: '32px',
-      border: 'solid grey 1px',
-      borderRadius: '20px',
-      paddingLeft: theme.spacing(1),
-      paddingRight: theme.spacing(1),
-    },
-  }),
-);
+import {debounce} from 'lodash';
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 type Props = {};
 
-// TODO CHANGE TEMP DATA
+// TODO CHANGE TEMP DATA. CAN GET DATA FROM STATE MANAGEMENT
 const Currencies = [
   {key: 1, tokenSymbol: 'aUSD', tokenImage: 'A', balance: '$452.02'},
   {key: 2, tokenSymbol: 'ACA', tokenImage: 'A', balance: '$452.02'},
