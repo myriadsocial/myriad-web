d1d2162a4dd406b6bd60f6b4357fd2ad8fdd9933
imam hermawan <immd.flores@gmail.com>
2021-09-06 18:35:19 +0700

    MYR-720: created backdrop component

---
+++ src/components-v2/atoms/Backdrop/Backdrop.stories.tsx
@@ -0,0 +1,26 @@
+import {ComponentStory, ComponentMeta} from '@storybook/react';
+
+import React from 'react';
+
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+import {BackdropComponent} from '.';
+
+export default {
+  title: 'UI Revamp v2.0/atoms/Backdrop',
+  component: BackdropComponent,
+} as ComponentMeta<typeof BackdropComponent>;
+
+const Template: ComponentStory<typeof BackdropComponent> = args => <BackdropComponent {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  open: true,
+  children: <p>backdrop</p>,
+};
+
+export const Loading = Template.bind({});
+Loading.args = {
+  open: true,
+  children: <CircularProgress color="inherit" />,
+};
