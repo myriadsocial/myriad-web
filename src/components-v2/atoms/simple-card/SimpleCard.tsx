648a7a041e42facbfc43cc71b399c513bc2fbf47
ical10 <mochhusnir@gmail.com>
2021-09-06 09:56:56 +0700

    MYR-721: initialised simple card component

---
+++ src/components-v2/atoms/simple-card/SimpleCard.tsx
@@ -0,0 +1,9 @@
+import React, {useState} from 'react';
+
+import Card from '@material-ui/core/Card';
+
+const SimpleCard = (): JSX.Element => {
+  return <Card></Card>;
+};
+
+export default SimpleCard;
