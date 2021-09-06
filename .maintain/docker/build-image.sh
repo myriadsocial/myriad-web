0c0fbee9012c68e52e9dbc6138ed055334c57fa1
Irman Nur Muhammad Alamsyah <irmannmal@gmail.com>
2021-09-06 14:17:06 +0700

    CI: updated docker

--- .maintain/docker/build-image.sh
+++ .maintain/docker/build-image.sh
@@ -9,12 +9,10 @@ PROJECT_ROOT=`git rev-parse --show-toplevel`
 cd $PROJECT_ROOT
 
 IMAGE_NAME=myriadsocial/myriad-web
-VCS_REF=`git rev-parse --short HEAD`
-BUILD_DATE=`date -u +"%Y%m%d"`
 
 # Build the image
 echo "Building ${IMAGE_NAME}:latest docker image, hang on!"
-time docker build -f ./.maintain/docker/Dockerfile --build-arg VCS_REF=${VCS_REF} --build-arg BUILD_DATE=${BUILD_DATE} -t ${IMAGE_NAME}:latest .
+time docker build -f ./.maintain/docker/Dockerfile -t ${IMAGE_NAME}:latest .
 
 # Show the list of available images for this repo
 echo "Image is ready"
