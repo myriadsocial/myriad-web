8256281c743d2b723420dbfbfffdbfd13a641c3c
imam hermawan <immd.flores@gmail.com>
2021-09-06 16:38:15 +0700

    MYR-707: created drop down component (send tip change currency)

---
+++ src/components-v2/atoms/CurrencyOption/CurrencyOption.stories.tsx
@@ -0,0 +1,17 @@
+import {ComponentStory, ComponentMeta} from '@storybook/react';
+
+import React from 'react';
+
+import {CurrencyOptionComponent} from '.';
+
+export default {
+  title: 'UI Revamp v2.0/components/Currency option',
+  component: CurrencyOptionComponent,
+} as ComponentMeta<typeof CurrencyOptionComponent>;
+
+const Template: ComponentStory<typeof CurrencyOptionComponent> = args => (
+  <CurrencyOptionComponent {...args} />
+);
+
+export const Default = Template.bind({});
+Default.args = {};
