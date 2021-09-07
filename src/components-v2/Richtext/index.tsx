bb3fc6d507cd483eb867c6e3c808d5a9e9b1d590
imam hermawan <immd.flores@gmail.com>
2021-09-07 10:49:55 +0700

    MYR-706: move style and type to own file

--- src/components-v2/Richtext/index.tsx
+++ src/components-v2/Richtext/index.tsx
@@ -5,62 +5,14 @@ import React, {useState} from 'react';
 import {IconButton} from '@material-ui/core';
 import Avatar from '@material-ui/core/Avatar';
 import TextareaAutosize from '@material-ui/core/TextareaAutosize';
-import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 
 import {acronym} from '../../helpers/string';
+import {Props} from './richText.interface';
+import {useStyles} from './richtext.style';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      position: 'relative',
-      boxSizing: 'border-box',
-      padding: '20px 30px',
-      width: '643px',
-      height: '90px',
-      borderRadius: '10px',
-      background: theme.palette.background.paper,
-    },
-    flex: {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'space-between',
-    },
-    postTextArea: {
-      background: theme.palette.background.paper,
-      fontFamily: theme.typography.fontFamily,
-      padding: theme.spacing(1),
-      lineHeight: '17.57px',
-      marginLeft: '10px',
-      resize: 'none',
-      width: '100%',
-      fontSize: 14,
-      height: 48,
-      border: 0,
-      '&:focus-visible': {
-        outline: 'none',
-      },
-    },
-    action: {
-      padding: 0,
-    },
-    screen: {
-      width: '100%',
-      height: '100%',
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      borderRadius: 8,
-      cursor: 'pointer',
-    },
-  }),
-);
-const pictURL =
-  'https://res.cloudinary.com/dsget80gs/image/upload/v1626794503/znraavovkot3qbjxqbvv.jpg';
-
-// eslint-disable-next-line @typescript-eslint/ban-types
-type Props = {};
-export const RichTextComponent: React.FC<Props> = () => {
+export const RichTextComponent: React.FC<Props> = props => {
+  const {userProfilePict} = props;
   const style = useStyles();
   const [post, setPost] = useState<string>('');
 
@@ -75,7 +27,7 @@ export const RichTextComponent: React.FC<Props> = () => {
         <div className={style.flex}>
           <Avatar
             aria-label={`avatar`}
-            src={pictURL}
+            src={userProfilePict}
             variant="circular"
             sizes="lg"
             style={{width: 50, height: 50}}>
