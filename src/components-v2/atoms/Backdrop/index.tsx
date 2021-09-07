7a354b5ca33bdcb11dad82177a07189552d0a95b
imam hermawan <immd.flores@gmail.com>
2021-09-07 09:18:21 +0700

    MYR-720: moved type and style to own file

--- src/components-v2/atoms/Backdrop/index.tsx
+++ src/components-v2/atoms/Backdrop/index.tsx
@@ -1,20 +1,9 @@
 import React from 'react';
 
 import Backdrop from '@material-ui/core/Backdrop';
-import {makeStyles, createStyles, Theme} from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    backdrop: {
-      zIndex: theme.zIndex.drawer + 1,
-      color: '#fff',
-    },
-  }),
-);
-type Props = {
-  open: boolean;
-  children: React.ReactNode;
-};
+import {Props} from './backdrop.interface';
+import {useStyles} from './backdrop.style';
 
 export const BackdropComponent: React.FC<Props> = props => {
   const {open, children} = props;
