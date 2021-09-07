b83d628206dfba4ac6fc5d7a2a3741daa0ccfa77
Mucktar Pakpahan <mucktar.pakpahan@gmail.com>
2021-09-07 21:06:27 +0700

    MYR-718: add embed component

--- src/components-v2/PostImport/PostImport.styles.ts
+++ src/components-v2/PostImport/PostImport.styles.ts
@@ -2,9 +2,13 @@ import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
 
 export const useStyles = makeStyles((theme: Theme) =>
   createStyles({
+<<<<<<< HEAD
     root: {
       width: 640,
     },
+=======
+    root: {},
+>>>>>>> 9f86ed4f (MYR-718: add embed component)
     input: {
       marginBottom: 30,
     },
