bc7aec74797ac88e5937434568e8d53511ad161c
ical10 <mochhusnir@gmail.com>
2021-09-07 18:21:59 +0700

    MYR-729: added argTypes for stories

--- src/components-v2/profile-header/index.ts
+++ src/components-v2/profile-header/index.ts
@@ -1,4 +1,6 @@
 import {default as ProfileHeader} from './ProfileHeader';
+import {ProfileHeaderProps} from './profile-header.interfaces';
 import useStyles from './profile-header.style';
 
 export {ProfileHeader, useStyles};
+export type {ProfileHeaderProps};
--- src/stories/ProfileHeader.stories.tsx
+++ src/stories/ProfileHeader.stories.tsx
@@ -3,10 +3,17 @@ import {ComponentStory, ComponentMeta} from '@storybook/react';
 import React from 'react';
 
 import {ProfileHeader} from '../../src/components-v2/profile-header/';
+import {CustomAvatarSize} from '../components-v2/atoms/avatar/';
 
 export default {
   title: 'UI Revamp v2.0/components',
   component: ProfileHeader,
+  argTypes: {
+    size: {
+      options: [CustomAvatarSize.SMALL, CustomAvatarSize.MEDIUM, CustomAvatarSize.LARGE],
+      control: {type: 'radio'},
+    },
+  },
 } as ComponentMeta<typeof ProfileHeader>;
 
 const Template: ComponentStory<typeof ProfileHeader> = args => <ProfileHeader {...args} />;
