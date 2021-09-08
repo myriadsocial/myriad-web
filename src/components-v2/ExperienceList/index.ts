de485738cb5e18e20250b5130d65a4379f86a64e
ical10 <mochhusnir@gmail.com>
2021-09-08 17:43:53 +0700

    MYR-698: modularized ExperienceList component

---
+++ src/components-v2/ExperienceList/index.ts
@@ -0,0 +1,7 @@
+import {default as ExperienceList} from './ExperienceList';
+import {ExperienceListProps} from './experience-list.interfaces';
+import {useStyles} from './experience-list.style';
+
+export {ExperienceList, useStyles};
+
+export type {ExperienceListProps};
