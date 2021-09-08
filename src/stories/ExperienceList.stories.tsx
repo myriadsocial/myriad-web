d15c82f14e6eb30de0d9c4d053de771c94d35637
ical10 <mochhusnir@gmail.com>
2021-09-08 17:45:11 +0700

    MYR-698: moved mockup data to stories

--- src/stories/ExperienceList.stories.tsx
+++ src/stories/ExperienceList.stories.tsx
@@ -12,4 +12,19 @@ export default {
 const Template: ComponentStory<typeof SimpleList> = args => <SimpleList {...args} />;
 
 export const ExperienceList = Template.bind({});
-ExperienceList.args = {};
+ExperienceList.args = {
+  experiences: [
+    {
+      title: 'Cryptowatcher',
+      creator: 'Lara Schoffield',
+      imgUrl:
+        'https://gist.githack.com/ical10/8178e2e0d345c691ceffd0b9bdb065a0/raw/e6a2e66955752fa1203667ab80b8dab0b09c9aa6/stonks.svg,',
+    },
+    {
+      title: 'Bitcoin Strategy',
+      creator: 'Jenny Chang',
+      imgUrl:
+        'https://gist.githack.com/ical10/8178e2e0d345c691ceffd0b9bdb065a0/raw/e6a2e66955752fa1203667ab80b8dab0b09c9aa6/stonks.svg',
+    },
+  ],
+};
