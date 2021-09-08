8782726cd5449379acbb86c751ad0f7cebe8c03a
ical10 <mochhusnir@gmail.com>
2021-09-08 17:43:53 +0700

    MYR-698: modularized ExperienceList component

--- src/components-v2/ExperienceList/ExperienceList.tsx
+++ src/components-v2/ExperienceList/ExperienceList.tsx
@@ -2,35 +2,11 @@ import React, {useState} from 'react';
 
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import {createStyles, Theme, makeStyles} from '@material-ui/core/styles';
 
+import {ExperienceListProps, useStyles} from '.';
 import {SimpleCard} from '../atoms/simple-card/';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      width: '100%',
-      maxWidth: 360,
-    },
-  }),
-);
-
-const experiences = [
-  {
-    title: 'Cryptowatcher',
-    creator: 'Lara Schoffield',
-    imgUrl:
-      'https://gist.githack.com/ical10/8178e2e0d345c691ceffd0b9bdb065a0/raw/e6a2e66955752fa1203667ab80b8dab0b09c9aa6/stonks.svg,',
-  },
-  {
-    title: 'Bitcoin Strategy',
-    creator: 'Jenny Chang',
-    imgUrl:
-      'https://gist.githack.com/ical10/8178e2e0d345c691ceffd0b9bdb065a0/raw/e6a2e66955752fa1203667ab80b8dab0b09c9aa6/stonks.svg',
-  },
-];
-
-const SimpleList = (): JSX.Element => {
+const ExperienceList = ({experiences}: ExperienceListProps): JSX.Element => {
   const classes = useStyles();
   const [selected, setSelected] = useState(false);
 
@@ -52,4 +28,4 @@ const SimpleList = (): JSX.Element => {
   );
 };
 
-export default SimpleList;
+export default ExperienceList;
