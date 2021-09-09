e7f4e430857b80910ca0e8527081e79a17cf1a66
ical10 <mochhusnir@gmail.com>
2021-09-09 16:16:38 +0700

    MYR-730: initialised SendTip and its stories

---
+++ src/stories/SendTip.stories.tsx
@@ -0,0 +1,15 @@
+import {ComponentStory, ComponentMeta} from '@storybook/react';
+
+import React from 'react';
+
+import {SendTip} from '../components-v2/SendTip/SendTip';
+
+export default {
+  title: 'UI Revamp v2.0/components/SendTip',
+  component: SendTip,
+} as ComponentMeta<typeof SendTip>;
+
+const Template: ComponentStory<typeof SendTip> = args => <SendTip {...args} />;
+
+export const SendTipModal = Template.bind({});
+SendTipModal.args = {};
