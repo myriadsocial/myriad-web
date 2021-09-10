aed8d1d3c5fe0a02a355e16ae27075e1be81e062
ical10 <mochhusnir@gmail.com>
2021-09-10 16:46:17 +0700

    MYR-730: fixed build error

--- src/stories/SendTip.stories.tsx
+++ src/stories/SendTip.stories.tsx
@@ -9,6 +9,7 @@ export default {
   component: SendTip,
 } as ComponentMeta<typeof SendTip>;
 
+//@ts-ignore
 const Template: ComponentStory<typeof SendTip> = args => <SendTip {...args} />;
 
 export const SendTipModal = Template.bind({});
