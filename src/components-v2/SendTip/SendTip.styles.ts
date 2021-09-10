0395967944d37429ba33d07a1513d40637ed40b9
ical10 <mochhusnir@gmail.com>
2021-09-10 16:24:46 +0700

    MYR-730: created core styles

---
+++ src/components-v2/SendTip/SendTip.styles.ts
@@ -0,0 +1,88 @@
+import MuiTableCell from '@material-ui/core/TableCell';
+import {withStyles, makeStyles, createStyles, Theme} from '@material-ui/core/styles';
+
+export const TableCell = withStyles({
+  root: {
+    borderBottom: 'none',
+  },
+})(MuiTableCell);
+
+export const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: 340,
+      position: 'relative',
+      height: 680,
+      background: '#FFFFFF',
+      boxShadow: `0px 2px 10px rgba(0, 0, 0, 0.05)`,
+      borderRadius: 10,
+    },
+    header: {
+      fontWeight: 700,
+      paddingTop: 30,
+      textAlign: 'center',
+    },
+    description: {
+      textAlign: 'center',
+      paddingTop: 12,
+    },
+    subHeader: {
+      fontWeight: 700,
+    },
+    subHeaderSection: {
+      padding: '0 8%',
+      marginTop: 30,
+    },
+    formRoot: {
+      marginTop: 12,
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      '& > p': {
+        alignSelf: 'flex-start',
+        margintop: 12,
+      },
+      '& > input:nth-child(1)': {
+        margin: 0,
+      },
+    },
+    formInput: {
+      zIndex: 10,
+      backgroundColor: '#FFF',
+    },
+    formStreak: {
+      content: '""',
+      position: 'absolute',
+      width: 340,
+      height: 24,
+      left: 0,
+      top: 228,
+
+      background: theme.palette.secondary.main,
+    },
+    formControls: {
+      marginTop: 12,
+      '& > *': {
+        marginTop: 8,
+      },
+    },
+    balanceSection: {
+      marginTop: 12,
+      display: 'flex',
+      justifyContent: 'flex-start',
+    },
+    receiverSummary: {
+      display: 'flex',
+      marginTop: 12,
+      alignItems: 'center',
+      justifyContent: 'center',
+      '& > p': {
+        marginLeft: theme.spacing(1.5),
+      },
+    },
+    clickableText: {
+      color: theme.palette.primary.main,
+    },
+  }),
+);
