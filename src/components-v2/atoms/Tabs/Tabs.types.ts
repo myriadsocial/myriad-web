1831ebfac8bf25d8caac87536406704dd9e0182b
Mucktar Pakpahan <mucktar.pakpahan@gmail.com>
2021-09-11 10:25:14 +0700

    MYR-764: tab list component

--- src/components-v2/atoms/Tabs/Tabs.types.ts
+++ src/components-v2/atoms/Tabs/Tabs.types.ts
@@ -2,7 +2,13 @@ import React from 'react';
 
 export type TabList<T> = {
   id: T;
-  title: string;
+  title?: string;
   icon?: string | React.ReactElement;
   component: React.ReactNode;
 };
+
+export type TabPosition = 'left' | 'right' | 'center' | 'space-around' | 'space-evenly';
+
+export type TabMark = 'underline' | 'cover';
+
+export type TabSize = 'small' | 'medium';
