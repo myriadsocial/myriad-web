6aed8fd9b4311cab3372f41ec346ff60f751ddc4
ical10 <mochhusnir@gmail.com>
2021-09-14 17:00:09 +0700

    MYR-806: added ExperienceDummy in index

--- src/components-v2/ExperienceList/index.ts
+++ src/components-v2/ExperienceList/index.ts
@@ -1,7 +1,7 @@
 import {default as ExperienceList} from './ExperienceList';
-import {ExperienceListProps} from './experience-list.interfaces';
+import {ExperienceDummy, ExperienceListProps} from './experience-list.interfaces';
 import {useStyles} from './experience-list.style';
 
 export {ExperienceList, useStyles};
 
-export type {ExperienceListProps};
+export type {ExperienceDummy, ExperienceListProps};
