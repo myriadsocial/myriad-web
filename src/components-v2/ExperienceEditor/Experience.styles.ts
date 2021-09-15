9f94ff722d3c614689bd5c92b1eeaf52a4eaa8ef
Mucktar Pakpahan <mucktar.pakpahan@gmail.com>
2021-09-15 16:30:47 +0700

    MYR-801: add preview selected people

--- src/components-v2/ExperienceEditor/Experience.styles.ts
+++ src/components-v2/ExperienceEditor/Experience.styles.ts
@@ -3,8 +3,15 @@ import {createStyles, makeStyles, Theme, alpha} from '@material-ui/core/styles';
 export const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     '@global': {
+      ' .MuiAutocomplete-option[aria-selected="true"]': {
+        background: 'none',
+      },
       ' .MuiAutocomplete-option[data-focus="true"]': {
         backgroundColor: alpha('#FFC857', 0.15),
+      },
+      ' .MuiAutocomplete-tag .MuiSvgIcon-root': {
+        width: 14,
+        height: 14,
       },
     },
     root: {
@@ -22,15 +29,15 @@ export const useStyles = makeStyles((theme: Theme) =>
       fontWeight: 400,
     },
     preview: {
-      display: 'flex',
-      alignItems: 'center',
-      flexDirection: 'column',
-
-      '& > img': {
-        marginBottom: 30,
-        width: 80,
-        height: 80,
-      },
+      marginBottom: 30,
+    },
+    label: {
+      background: '#FFF',
+      paddingLeft: 6,
+      paddingRight: 6,
+    },
+    social: {
+      color: theme.palette.primary.main,
     },
     people: {},
   }),
