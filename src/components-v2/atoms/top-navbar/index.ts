409d2de6b03b37e5c9835372aa13a7fb68c7157c
ical10 <mochhusnir@gmail.com>
2021-09-15 17:10:11 +0700

    MYR-806: created TopNavbar

---
+++ src/components-v2/atoms/top-navbar/index.ts
@@ -0,0 +1,6 @@
+import {TopNavbarComponent} from './TopNavbar';
+import {SectionTitle, sectionTitles, TopNavbarProps} from './top-navbar.interfaces';
+import {useStyles} from './top-navbar.styles';
+
+export {useStyles, SectionTitle, sectionTitles, TopNavbarComponent};
+export type {TopNavbarProps};
