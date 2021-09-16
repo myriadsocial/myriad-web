efeae7a05adaa5a984fc411abdc5b4ff65d7a1aa
ical10 <mochhusnir@gmail.com>
2021-09-16 12:22:09 +0700

    MYR-736: started migrating to heroicons

--- src/components-v2/atoms/Sort/index.tsx
+++ src/components-v2/atoms/Sort/index.tsx
@@ -1,16 +1,27 @@
+import {ChevronDownIcon} from '@heroicons/react/outline';
+
 import React, {useState} from 'react';
 
 import {Typography} from '@material-ui/core';
 import IconButton from '@material-ui/core/IconButton';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
-// TODO move icon to HEROICONS
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import SvgIcon from '@material-ui/core/SvgIcon';
+import {makeStyles, Theme} from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  action: {
+    '& .MuiSvgIcon-root': {
+      fill: 'none',
+    },
+  },
+}));
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 type Props = {};
 
 export const SortComponent: React.FC<Props> = () => {
+  const classes = useStyles();
   const [sort, setSort] = useState('Latest');
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -35,8 +46,13 @@ export const SortComponent: React.FC<Props> = () => {
         {sort}
       </Typography>
 
-      <IconButton onClick={handleClick} color="primary" aria-label="expand">
-        <ExpandMoreIcon />
+      <IconButton
+        disableRipple
+        className={classes.action}
+        onClick={handleClick}
+        color="primary"
+        aria-label="expand">
+        <SvgIcon component={ChevronDownIcon} viewBox="0 0 24 24" />
       </IconButton>
       <Menu
         id="friend-response-menu"
