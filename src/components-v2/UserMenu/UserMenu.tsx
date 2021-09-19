6bde235bf40c2ed922570dbe5e3e0356461d4faa
Mucktar Pakpahan <mucktar.pakpahan@gmail.com>
2021-09-19 21:49:23 +0700

    MYR-813: add user setting component

---
+++ src/components-v2/UserMenu/UserMenu.tsx
@@ -0,0 +1,39 @@
+import React, {useState} from 'react';
+
+import Paper from '@material-ui/core/Paper';
+
+import {Experience} from '../../interfaces/experience';
+import {Post} from '../../interfaces/post';
+import {SocialMedia} from '../../interfaces/social';
+import {User} from '../../interfaces/user';
+import {TabsComponent} from '../atoms/Tabs';
+import {useStyles} from './UserMenu.styles';
+import {useUserTabs, UserMenuTabs} from './hooks/user-tabs.hooks';
+
+type UserMenuProps = {
+  selected: UserMenuTabs;
+  anonymous?: boolean;
+  posts?: Post[];
+  experiences?: Experience[];
+  socials?: SocialMedia[];
+  user: User;
+};
+
+export const UserMenu: React.FC<UserMenuProps> = props => {
+  const {selected, user, posts = [], experiences = [], socials = []} = props;
+
+  const styles = useStyles();
+
+  const tabs = useUserTabs({posts, experiences, user, socials});
+  const [activeTab, setActiveTab] = useState<UserMenuTabs>(selected);
+
+  const handleTabChange = (tab: string) => {
+    setActiveTab(tab as UserMenuTabs);
+  };
+
+  return (
+    <Paper square className={styles.root}>
+      <TabsComponent tabs={tabs} active={activeTab} onChangeTab={handleTabChange} size="small" />
+    </Paper>
+  );
+};
---
+++ src/components-v2/UserSettings/UserSettings.styles.ts
@@ -0,0 +1,10 @@
+import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
+
+export const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {},
+    title: {
+      fontWeight: 700,
+    },
+  }),
+);
