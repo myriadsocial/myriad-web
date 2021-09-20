65fe905bb0bc2d684c778ecd97270de31bd62861
Mucktar Pakpahan <mucktar.pakpahan@gmail.com>
2021-09-20 08:39:51 +0700

    MYR-664: switch to express

--- server/index.ts
+++ server/index.ts
@@ -1,41 +1,39 @@
 import next from 'next';
 
 import * as dotenv from 'dotenv';
-import fs from 'fs';
-import {createServer as createHttpServer} from 'http';
-import {createServer as createHttpsServer} from 'https';
-import {parse} from 'url';
+import express from 'express';
+import path from 'path';
+import serveIndex from 'serve-index';
 
 dotenv.config();
 
 const port = parseInt(process.env.PORT || '3000', 10);
-const https = process.env.USE_HTTPS === 'true';
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({dev});
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  if (https) {
-    const httpsOptions = {
-      key: fs.readFileSync('./certs/localhost-key.pem'),
-      cert: fs.readFileSync('./certs/localhost.pem'),
-    };
+  const server = express();
 
-    createHttpsServer(https ? httpsOptions : {}, (req, res) => {
-      const parsedUrl = parse(req.url!, true);
-      handle(req, res, parsedUrl);
-    }).listen(port);
-  } else {
-    createHttpServer((req, res) => {
-      const parsedUrl = parse(req.url!, true);
-      handle(req, res, parsedUrl);
-    }).listen(port);
-  }
+  // security - like to not display the backend is built on express ;)
+  server.disable('x-powered-by');
 
-  // tslint:disable-next-line:no-console
-  console.log(
-    `> Server listening at ${https ? 'https' : 'http'}://localhost:${port} as ${
-      dev ? 'development' : process.env.NODE_ENV
-    }`,
+  server.use(
+    '/docs',
+    express.static(path.join(__dirname, '../docs')),
+    serveIndex(path.join(__dirname, '../docs')),
   );
+
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, () => {
+    //tslint:disable-next-line:no-console
+    console.log(
+      `> Server listening at 'http://localhost:${port} as ${
+        dev ? 'development' : process.env.NODE_ENV
+      }`,
+    );
+  });
 });
