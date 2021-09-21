5787025bc0f98e113b1e290659774781d3dfaa39
Mucktar Pakpahan <mucktar.pakpahan@gmail.com>
2021-09-21 20:58:15 +0700

    MYR-718: update create post styling

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,14 @@ module.exports = {
   root: true, // Make sure eslint picks up the config at the root of the directory
   parserOptions: {
     ecmaVersion: 2020, // Use the latest ecmascript standard
-    sourceType: "module", // Allows using import/export statements
+    sourceType: 'module', // Allows using import/export statements
     ecmaFeatures: {
       jsx: true, // Enable JSX since we're using React
     },
   },
   settings: {
     react: {
-      version: "detect", // Automatically detect the react version
+      version: 'detect', // Automatically detect the react version
     },
   },
   env: {
@@ -20,30 +20,27 @@ module.exports = {
     es6: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
-    "plugin:jsx-a11y/recommended",
-    "plugin:prettier/recommended", // Make this the last element so prettier config overrides other formatting rules
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended', // Make this the last element so prettier config overrides other formatting rules
   ],
-  ignorePatterns: ["**/public/*.js", "**/plugins/*.js"],
+  ignorePatterns: ['**/public/*.js', '**/plugins/*.js'],
   rules: {
-    "no-useless-escape": ["warn"],
-    "@typescript-eslint/ban-ts-comment": "off",
-    "@typescript-eslint/ban-types": ["warn"],
-    "@typescript-eslint/no-empty-interface": ["warn"],
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      { vars: "local", args: "none" },
-    ],
-    "jsx-a11y/click-events-have-key-events": ["warn"],
-    "jsx-a11y/interactive-supports-focus": ["warn"],
-    "jsx-a11y/no-static-element-interactions": ["warn"],
-    "jsx-a11y/no-noninteractive-element-interactions": ["warn"],
-    "react/prop-types": ["off"],
-    "react/display-name": ["warn"],
-    "react/no-unescaped-entities": ["warn"],
-    "prettier/prettier": ["error", {}, { usePrettierrc: true }], // Use our .prettierrc file as source
+    'no-useless-escape': ['warn'],
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/ban-types': ['warn'],
+    '@typescript-eslint/no-empty-interface': ['warn'],
+    '@typescript-eslint/no-unused-vars': ['error', {vars: 'local', args: 'none'}],
+    'jsx-a11y/click-events-have-key-events': ['warn'],
+    'jsx-a11y/interactive-supports-focus': ['warn'],
+    'jsx-a11y/no-static-element-interactions': ['warn'],
+    'jsx-a11y/no-noninteractive-element-interactions': ['warn'],
+    'react/prop-types': ['off'],
+    'react/display-name': ['warn'],
+    'react/no-unescaped-entities': ['warn'],
+    'prettier/prettier': ['error', {}, {usePrettierrc: true}], // Use our .prettierrc file as source
   },
-  plugins: ["import"],
+  plugins: ['import'],
 };
