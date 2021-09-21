6c637a5368c8277d7e8ef110698b3c0a55ec4719
ical10 <mochhusnir@gmail.com>
2021-09-21 11:58:29 +0700

    MYR-810: created balanceDetailList

--- src/components-v2/BalanceDetailList/BalanceDetailList.tsx
+++ src/components-v2/BalanceDetailList/BalanceDetailList.tsx
@@ -1,7 +1,11 @@
+import {RefreshIcon} from '@heroicons/react/outline';
+
 import React from 'react';
 
 import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
 import List from '@material-ui/core/List';
+import SvgIcon from '@material-ui/core/SvgIcon';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -11,6 +15,8 @@ import Typography from '@material-ui/core/Typography';
 import {createStyles, Theme, makeStyles} from '@material-ui/core/styles';
 
 import {BalanceDetail} from '../MyWallet/MyWallet';
+import {balanceSortOptions} from '../Timeline/default';
+import {DropdownMenu} from '../atoms/DropdownMenu/';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -33,6 +39,24 @@ const useStyles = makeStyles((theme: Theme) =>
       paddingLeft: 0,
       paddingRight: 0,
     },
+    headerActionWrapper: {
+      display: 'flex',
+      justifyContent: 'flex-start',
+      alignItems: 'center',
+      marginTop: theme.spacing(1),
+    },
+    refreshIcon: {
+      paddingRight: 0,
+      '& .MuiSvgIcon-root': {
+        fill: 'none',
+        paddingRight: theme.spacing(1),
+      },
+      '&:hover': {
+        background: 'none',
+      },
+      fontSize: 13,
+      marginLeft: 'auto',
+    },
   }),
 );
 
@@ -43,33 +67,54 @@ type BalanceDetailListProps = {
 export const BalanceDetailList: React.FC<BalanceDetailListProps> = props => {
   const {balanceDetails} = props;
 
+  const handleRefresh = () => {
+    console.log('refreshed!');
+  };
+
   const classes = useStyles();
   return (
-    <TableContainer component={List}>
-      <Table className={classes.root} aria-label="simple table">
-        <TableBody>
-          {balanceDetails.map(balanceDetail => (
-            <TableRow key={balanceDetail.id} className={classes.tableRow}>
-              <TableCell component="th" scope="row" className={classes.tableCell}>
-                <Avatar alt={balanceDetail.name} src={balanceDetail.image} />
-                <Typography variant="h5" color="textPrimary">
-                  {balanceDetail.id}
-                </Typography>
-              </TableCell>
-              <TableCell align="right">
-                <div>
-                  <Typography variant="body1" style={{fontWeight: 'bold'}}>
-                    {balanceDetail.freeBalance}
+    <>
+      <div className={classes.headerActionWrapper}>
+        <DropdownMenu title={'Sort'} options={balanceSortOptions} />
+        <IconButton
+          classes={{
+            root: classes.refreshIcon,
+          }}
+          disableRipple
+          aria-label="refresh"
+          onClick={handleRefresh}>
+          <SvgIcon component={RefreshIcon} color="primary" />
+          <Typography variant="body1" color="primary">
+            Refresh
+          </Typography>
+        </IconButton>
+      </div>
+      <TableContainer component={List}>
+        <Table className={classes.root} aria-label="simple table">
+          <TableBody>
+            {balanceDetails.map(balanceDetail => (
+              <TableRow key={balanceDetail.id} className={classes.tableRow}>
+                <TableCell component="th" scope="row" className={classes.tableCell}>
+                  <Avatar alt={balanceDetail.name} src={balanceDetail.image} />
+                  <Typography variant="h5" color="textPrimary">
+                    {balanceDetail.id}
                   </Typography>
-                  <Typography variant="caption" color="textSecondary">
-                    {'USD 15.25'}
-                  </Typography>
-                </div>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+                </TableCell>
+                <TableCell align="right">
+                  <div>
+                    <Typography variant="body1" style={{fontWeight: 'bold'}}>
+                      {balanceDetail.freeBalance}
+                    </Typography>
+                    <Typography variant="caption" color="textSecondary">
+                      {'USD 15.25'}
+                    </Typography>
+                  </div>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 };
