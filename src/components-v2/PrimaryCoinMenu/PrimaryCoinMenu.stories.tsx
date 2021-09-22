54752c8fd752fb4435e250f0432bc7af9707201b
ical10 <mochhusnir@gmail.com>
2021-09-22 13:57:43 +0700

    MYR-810: created PrimaryCoinMenu accessible from BalanceDetailList

---
+++ src/components-v2/PrimaryCoinMenu/PrimaryCoinMenu.stories.tsx
@@ -0,0 +1,15 @@
+import {ComponentStory, ComponentMeta} from '@storybook/react';
+
+import React from 'react';
+
+import {PrimaryCoinMenu} from './PrimaryCoinMenu';
+
+export default {
+  title: 'UI Revamp v2.0/components/Primary Coin Menu',
+  component: PrimaryCoinMenu,
+} as ComponentMeta<typeof PrimaryCoinMenu>;
+
+const Template: ComponentStory<typeof PrimaryCoinMenu> = args => <PrimaryCoinMenu {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
