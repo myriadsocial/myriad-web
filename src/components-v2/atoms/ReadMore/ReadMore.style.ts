da334a61d823128e7a19cea70b313c2cfc10dff5
imam hermawan <immd.flores@gmail.com>
2021-09-23 14:46:11 +0700

    MYR-829: Created wrap content readmore at comment section

---
+++ src/components-v2/atoms/ReadMore/ReadMore.style.ts
@@ -0,0 +1,9 @@
+import {makeStyles, createStyles} from '@material-ui/core/styles';
+
+export const useStyles = makeStyles(() =>
+  createStyles({
+    pointer: {
+      cursor: 'pointer',
+    },
+  }),
+);
