606befed77fa1b1ca44a30e940889cdef16bb26d
imam hermawan <immd.flores@gmail.com>
2021-09-23 14:54:46 +0700

    MYR-828: created component post NSFW

---
+++ src/components-v2/atoms/NSFW/NSFW.component.tsx
@@ -0,0 +1,40 @@
+import {EyeOffIcon} from '@heroicons/react/outline';
+
+import React from 'react';
+
+import {Button, Typography} from '@material-ui/core';
+import SvgIcon from '@material-ui/core/SvgIcon';
+
+import {useStyles} from './nsfw.style';
+
+type Props = {
+  viewContent: () => void;
+};
+export const NSFW: React.FC<Props> = props => {
+  const {viewContent} = props;
+  const style = useStyles();
+
+  const handleClick = () => {
+    viewContent();
+  };
+
+  return (
+    <div className={style.root}>
+      <SvgIcon
+        className={style.icon}
+        classes={{root: style.fill}}
+        component={EyeOffIcon}
+        viewBox="0 0 24 24"
+      />
+      <Typography className={style.title} component="p">
+        NSFW Content
+      </Typography>
+      <Typography className={style.subTitle} component="p">
+        this post may contain sensitive content
+      </Typography>
+      <Button onClick={handleClick} size="small" color="primary" variant="contained">
+        View content
+      </Button>
+    </div>
+  );
+};
