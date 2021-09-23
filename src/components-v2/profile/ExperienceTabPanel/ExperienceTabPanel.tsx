f12670057af08e8b42eee245b73ae4462003efcd
ical10 <mochhusnir@gmail.com>
2021-09-23 18:54:38 +0700

    MYR-814: rearranged component in the right-hand column

--- src/components-v2/profile/ExperienceTabPanel/ExperienceTabPanel.tsx
+++ src/components-v2/profile/ExperienceTabPanel/ExperienceTabPanel.tsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 
 import {Experience} from '../../../interfaces/experience';
 import {ExperienceList} from '../../ExperienceList/';
-import {MenuOptions} from '../../atoms/DropdownMenu/';
+import {experienceFilterOptions} from '../../Timeline/default';
 import {FilterDropdownMenu} from '../../atoms/FilterDropdownMenu/';
 
 type ExperienceTabPanelProps = {
@@ -13,16 +13,11 @@ type ExperienceTabPanelProps = {
 export const ExperienceTabPanel: React.FC<ExperienceTabPanelProps> = props => {
   const {experiences, isOnHomePage} = props;
 
-  const [sortExperienceOptions] = useState<MenuOptions[]>([
-    {id: 'all', title: 'All Experience'},
-    {id: 'personal', title: 'Personal Experience'},
-    {id: 'other', title: 'Other Experience'},
-  ]);
   const [title] = useState('Filter by');
 
   return (
     <>
-      <FilterDropdownMenu title={title} options={sortExperienceOptions} />
+      <FilterDropdownMenu title={title} options={experienceFilterOptions} />
       <ExperienceList isOnHomePage={isOnHomePage} experiences={experiences} />
     </>
   );
