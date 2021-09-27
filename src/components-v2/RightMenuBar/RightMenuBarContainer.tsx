f72afa113dbf3697582a316029593066fbc2af3e
ical10 <mochhusnir@gmail.com>
2021-09-27 13:29:02 +0700

    MYR-878: attached RightMenuBar containing experiences

---
+++ src/components-v2/RightMenuBar/RightMenuBarContainer.tsx
@@ -0,0 +1,17 @@
+import React, {useEffect} from 'react';
+
+import {RightMenuBar} from '.';
+import {useExperienceHook} from '../../hooks/use-experience-hook';
+
+export const RightMenuBarContainer: React.FC = () => {
+  // Only load experiences specific to logged user
+  const {loadExperience, experiences} = useExperienceHook();
+
+  useEffect(() => {
+    //TODO: move this to app level to avoid refetching
+    loadExperience();
+  }, []);
+
+  console.log({experiences});
+  return <RightMenuBar experiences={experiences} />;
+};
