6e7b8ac13569634d7bc9491ed0f7bd6937631f83
ical10 <mochhusnir@gmail.com>
2021-09-27 16:56:57 +0700

    MYR-878: integrated data fetching for both SB and next.js

--- src/components-v2/Trending/TrendingList.tsx
+++ src/components-v2/Trending/TrendingList.tsx
@@ -8,43 +8,12 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
 import Typography from '@material-ui/core/Typography';
-import {createStyles, Theme, makeStyles} from '@material-ui/core/styles';
 
 import {Tag} from '../../interfaces/experience';
 import {TimelineType} from '../../interfaces/timeline';
+import {useStyles} from './trending-list.styles';
 
 import {v4 as uuid} from 'uuid';
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      display: 'block',
-    },
-    list: {
-      marginLeft: theme.spacing(-2),
-      marginRight: theme.spacing(-2),
-    },
-    item: {
-      '& .MuiListItemText-primary': {
-        fontWeight: 400,
-        fontSize: 16,
-        lineHeight: '19px',
-      },
-
-      '& .MuiTypography-colorTextSecondary': {
-        color: theme.palette.primary.dark,
-      },
-    },
-    avatar: {
-      minWidth: 20,
-    },
-    text: {
-      '& > a': {
-        color: theme.palette.text.primary,
-      },
-    },
-  }),
-);
 
 interface TrendingListProps {
   trendings: Tag[];
