2c232201a0b34576ea39e3e840c9df9c75f20394
ical10 <mochhusnir@gmail.com>
2021-09-27 21:05:41 +0700

    MYR-878: Fixed myriad-web app build error

--- src/components-v2/RightMenuBar/index.ts
+++ src/components-v2/RightMenuBar/index.ts
@@ -1,7 +1,4 @@
 import {RightMenuBar} from './RightMenuBar';
-import {RightMenuBarProps} from './right-menubar.interfaces';
 import {useStyles} from './right-menubar.styles';
 
 export {useStyles, RightMenuBar};
-
-export type {RightMenuBarProps};
