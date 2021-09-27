2c437ef3394cfa40d94e550872ace9f1738bd365
ical10 <mochhusnir@gmail.com>
2021-09-27 13:29:02 +0700

    MYR-878: attached RightMenuBar containing experiences

--- src/components-v2/RightMenuBar/index.ts
+++ src/components-v2/RightMenuBar/index.ts
@@ -1,7 +1,8 @@
 import {RightMenuBar} from './RightMenuBar';
+import {RightMenuBarContainer} from './RightMenuBarContainer';
 import {RightMenuBarProps} from './right-menubar.interfaces';
 import {useStyles} from './right-menubar.styles';
 
-export {useStyles, RightMenuBar};
+export {useStyles, RightMenuBar, RightMenuBarContainer};
 
 export type {RightMenuBarProps};
