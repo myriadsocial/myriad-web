9a531c8de159b6fb98272e0914afaf9f6601fa91
Mucktar Pakpahan <mucktar.pakpahan@gmail.com>
2021-09-28 07:23:07 +0700

    MYR-868: update login ui

---
+++ src/layout/Login/Login.stories.tsx
@@ -0,0 +1,20 @@
+import {ComponentStory, ComponentMeta} from '@storybook/react';
+
+import React from 'react';
+
+import {LoginLayout as LoginLayoutComponent} from '.';
+import {Login} from '../../components-v2/Login';
+
+export default {
+  title: 'UI Revamp v2.0/Layout/Login',
+  component: LoginLayoutComponent,
+} as ComponentMeta<typeof LoginLayoutComponent>;
+
+const Template: ComponentStory<typeof LoginLayoutComponent> = args => (
+  <LoginLayoutComponent {...args} />
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  children: <Login />,
+};
