59dc8324afefdd0e4f74ccfceed29c392856622b
Mucktar Pakpahan <mucktar.pakpahan@gmail.com>
2021-09-29 09:19:21 +0700

    MYR-870: init timeline

--- src/components-v2/Timeline/TimelineContainer.tsx
+++ src/components-v2/Timeline/TimelineContainer.tsx
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import {Timeline as TimelineComponent} from '.';
-import {Timeline} from './Timeline.stories';
+import {useQueryParams} from '../../hooks/use-query-params.hooks';
+import {TimelineFilter, TimelineSortMethod, TimelineType} from '../../interfaces/timeline';
+import {useTimelineFilter} from './hooks/use-timeline-filter.hook';
+import {useTimelineHook} from './hooks/use-timeline.hook';
 
-export const TimelineContainer: React.FC = () => {
+type TimelineContainerProps = {
+  filter?: TimelineFilter;
+  anonymous?: boolean;
+};
+
+export const TimelineContainer: React.FC<TimelineContainerProps> = props => {
+  const {anonymous = false} = props;
+
+  const {posts, hasMore, nextPage} = useTimelineHook();
+  const {filterTimeline} = useTimelineFilter();
+  const {push, query} = useQueryParams();
+
+  useEffect(() => {
+    filterTimeline(query);
+  }, [query]);
+
+  const handleSortTimeline = (sort: TimelineSortMethod) => {
+    push('sort', sort);
+    // push(`?sort=${sort}`, undefined, {shallow: true});
+  };
+
+  const handleFilterTimeline = (type: TimelineType) => {
+    push('type', type);
+    // push(`?type=${type}`, undefined, {shallow: true});
+  };
+
   return (
     <TimelineComponent
-      posts={Timeline.args?.posts ?? []}
-      anonymous={Timeline.args?.anonymous ?? false}
+      posts={posts}
+      anonymous={anonymous}
+      loadNextPage={nextPage}
+      hasMore={hasMore}
+      sortTimeline={handleSortTimeline}
+      filterTimeline={handleFilterTimeline}
     />
   );
 };
