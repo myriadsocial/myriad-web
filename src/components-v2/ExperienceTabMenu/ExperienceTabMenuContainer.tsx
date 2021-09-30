e7a2c14fdeb34d65ef36777e1b116a25530c5526
imam hermawan <immd.flores@gmail.com>
2021-09-30 17:26:45 +0700

    MYR-888: handle anonymous user in wallet and experience component

--- src/components-v2/ExperienceTabMenu/ExperienceTabMenuContainer.tsx
+++ src/components-v2/ExperienceTabMenu/ExperienceTabMenuContainer.tsx
@@ -1,6 +1,9 @@
 import React, {useEffect} from 'react';
 import {useSelector} from 'react-redux';
 
+import {Typography} from '@material-ui/core';
+import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
+
 import {useExperienceHook} from '../../hooks/use-experience-hook';
 import {useQueryParams} from '../../hooks/use-query-params.hooks';
 import {TimelineType} from '../../interfaces/timeline';
@@ -9,9 +12,23 @@ import {ExperienceTabMenu} from './ExperienceTabMenu';
 import {RootState} from 'src/reducers';
 import {UserState} from 'src/reducers/user/reducer';
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      background: theme.palette.background.paper,
+      padding: theme.spacing(3),
+      borderRadius: '20px',
+      marginTop: '12px',
+      height: '500px',
+    },
+  }),
+);
+
 export const ExperienceTabMenuContainer: React.FC = () => {
-  const {user} = useSelector<RootState, UserState>(state => state.userState);
+  const {anonymous, user} = useSelector<RootState, UserState>(state => state.userState);
+  const style = useStyles();
   const {push} = useQueryParams();
+
   // Only load experiences specific to logged user
   const {loadExperience, experiences} = useExperienceHook();
 
@@ -23,6 +40,16 @@ export const ExperienceTabMenuContainer: React.FC = () => {
   const handleFilterTimeline = (type: TimelineType) => {
     push('type', type);
   };
+
+  if (anonymous)
+    return (
+      <>
+        <Typography variant={'h4'}>Experience</Typography>
+        <div className={style.root}>
+          <Typography>Please Login with polkadot account to access this feature</Typography>
+        </div>
+      </>
+    );
 
   return (
     <ExperienceTabMenu
