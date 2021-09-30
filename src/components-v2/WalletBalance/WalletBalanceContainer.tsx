bf82c4e878fb50a4602a97d735226747d7a31311
imam hermawan <immd.flores@gmail.com>
2021-09-30 17:26:45 +0700

    MYR-888: handle anonymous user in wallet and experience component

--- src/components-v2/WalletBalance/WalletBalanceContainer.tsx
+++ src/components-v2/WalletBalance/WalletBalanceContainer.tsx
@@ -1,15 +1,18 @@
 import React, {useEffect} from 'react';
 import {useSelector} from 'react-redux';
 
+import {Typography} from '@material-ui/core';
+
 import {WalletBalances as WalletBalancesComponent} from '.';
 import {usePolkadotApi} from '../../../src/hooks/use-polkadot-api.hook';
 import {RootState} from '../../reducers/';
 import {UserState} from '../../reducers/user/reducer';
+import {BoxComponent} from '../atoms/Box';
 
 import {BalanceState} from 'src/reducers/balance/reducer';
 
 export const WalletBalancesContainer: React.FC = () => {
-  const {user, currencies} = useSelector<RootState, UserState>(state => state.userState);
+  const {user, currencies, anonymous} = useSelector<RootState, UserState>(state => state.userState);
 
   const {load} = usePolkadotApi();
 
@@ -20,6 +23,13 @@ export const WalletBalancesContainer: React.FC = () => {
   }, [currencies, user]);
 
   const {balanceDetails} = useSelector<RootState, BalanceState>(state => state.balanceState);
+
+  if (anonymous)
+    return (
+      <BoxComponent title="Wallet">
+        <Typography>Please Login with polkadot account to access this feature</Typography>
+      </BoxComponent>
+    );
 
   return <WalletBalancesComponent balances={balanceDetails} />;
 };
