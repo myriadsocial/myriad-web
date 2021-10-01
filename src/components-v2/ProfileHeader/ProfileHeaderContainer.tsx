ec4da26288319de5428abcb82fdb1bfae6ecb18b
imam hermawan <immd.flores@gmail.com>
2021-10-01 23:30:12 +0700

    MYR-923: added expand component in user profile card

--- src/components-v2/ProfileHeader/ProfileHeaderContainer.tsx
+++ src/components-v2/ProfileHeader/ProfileHeaderContainer.tsx
@@ -1,19 +1,32 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
 
+import {signOut} from 'next-auth/client';
+
 import {ProfileHeader as ProfileHeaderComponent} from '.';
+import {useAuthHook} from '../../hooks/auth.hook';
 
 import {RootState} from 'src/reducers';
 import {UserState} from 'src/reducers/user/reducer';
 
 export const ProfileHeaderContainer: React.FC = () => {
-  const {user, alias} = useSelector<RootState, UserState>(state => state.userState);
+  const {logout} = useAuthHook();
+  const {user, anonymous, alias} = useSelector<RootState, UserState>(state => state.userState);
+
+  const handleSignOut = async () => {
+    if (anonymous === false) {
+      logout();
+    } else {
+      await signOut({
+        callbackUrl: process.env.NEXT_PUBLIC_APP_URL,
+        redirect: true,
+      });
+    }
+  };
 
   return (
-    <ProfileHeaderComponent
-      name={user?.name || alias}
-      username={user?.username || alias}
-      avatar={user?.profilePictureURL}
-    />
+    <>
+      {user && <ProfileHeaderComponent handleSignOut={handleSignOut} user={user} alias={alias} />}
+    </>
   );
 };
