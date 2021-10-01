de6fb06de1a76f23e59f9542ea3eede89c5a48a0
imam hermawan <immd.flores@gmail.com>
2021-10-01 14:49:11 +0700

    MYR-000: moved and duplicated component to component-v2

---
+++ src/components-v2/Profile/ProfileComponent.tsx
@@ -0,0 +1,50 @@
+import React, {useState, useEffect} from 'react';
+import {useDispatch} from 'react-redux';
+
+import {useStyles} from './profile.style';
+
+import {ProfileEditContainer} from 'src/components-v2/Profile/Edit/ProfileEditContainer';
+import {ProfileHeaderContainer} from 'src/components-v2/Profile/ProfileHeader/ProfileHeaderContainer';
+import {UserMenuContainer} from 'src/components-v2/UserMenu';
+import {TopNavbarComponent, SectionTitle} from 'src/components-v2/atoms/TopNavbar';
+import {User} from 'src/interfaces/user';
+import {fetchProfileFriend} from 'src/reducers/profile/actions';
+
+type Props = {
+  profile: User;
+  loading: boolean;
+};
+
+export const ProfileTimeline: React.FC<Props> = ({profile}) => {
+  const style = useStyles();
+  const dispatch = useDispatch();
+
+  const [isEdit, setIsEdit] = useState<boolean>(false);
+
+  useEffect(() => {
+    dispatch(fetchProfileFriend());
+
+    return undefined;
+  }, [profile.id]);
+
+  const handleOpenEdit = () => {
+    setIsEdit(true);
+  };
+
+  return (
+    <div className={style.root}>
+      <div className={style.scroll}>
+        <div className={style.mb}>
+          <TopNavbarComponent description={profile.name} sectionTitle={SectionTitle.PROFILE} />
+        </div>
+        {isEdit && <ProfileEditContainer />}
+        {!isEdit && (
+          <>
+            <ProfileHeaderContainer edit={handleOpenEdit} />
+            <UserMenuContainer />
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
