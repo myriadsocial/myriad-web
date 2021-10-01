963683665f75644a403632111ada1752e875855f
ical10 <mochhusnir@gmail.com>
2021-10-01 15:49:12 +0700

    MYR-875: added HistoryDetailList into Container

--- src/components-v2/HistoryDetailList/HistoryDetailListContainer.tsx
+++ src/components-v2/HistoryDetailList/HistoryDetailListContainer.tsx
@@ -1,22 +1,28 @@
 import React, {useEffect} from 'react';
 import {useSelector} from 'react-redux';
 
-//import {HistoryDetailList} from './HistoryDetailList';
+import {HistoryDetailList} from './HistoryDetailList';
+
 import {useTransaction} from 'src/hooks/use-transaction.hooks';
 import {RootState} from 'src/reducers';
 import {UserState} from 'src/reducers/user/reducer';
 
 export const HistoryDetailListContainer: React.FC = () => {
   const {user} = useSelector<RootState, UserState>(state => state.userState);
-  const {transactions, loadInitTransaction} = useTransaction();
+  const {transactions, inboundTxs, outboundTxs, loadTransactions} = useTransaction();
 
   if (!user) return null;
 
   useEffect(() => {
-    loadInitTransaction();
+    loadTransactions();
   }, []);
 
-  console.log({transactions});
-
-  return <p>History</p>;
+  return (
+    <HistoryDetailList
+      userId={user.id}
+      allTxs={transactions}
+      inboundTxs={inboundTxs}
+      outboundTxs={outboundTxs}
+    />
+  );
 };
