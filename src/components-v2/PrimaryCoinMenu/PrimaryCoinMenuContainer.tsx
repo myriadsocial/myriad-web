ea55c970096d321650932a8f1e7d750a21ba4c2b
ical10 <mochhusnir@gmail.com>
2021-10-04 15:40:31 +0700

    MYR-875: implemented changing defaultCurrency

--- src/components-v2/PrimaryCoinMenu/PrimaryCoinMenuContainer.tsx
+++ src/components-v2/PrimaryCoinMenu/PrimaryCoinMenuContainer.tsx
@@ -2,31 +2,29 @@ import React from 'react';
 import {useSelector} from 'react-redux';
 
 import {PrimaryCoinMenu} from '.';
+import {UserState} from '../../reducers/user/reducer';
 
 import {RootState} from 'src/reducers';
 import {BalanceState} from 'src/reducers/balance/reducer';
-import {UserState} from 'src/reducers/user/reducer';
 
 type PrimaryCoinMenuContainer = {
   togglePrimaryCoinMenu: () => void;
 };
 
 export const PrimaryCoinMenuContainer: React.FC<PrimaryCoinMenuContainer> = props => {
-  const {togglePrimaryCoinMenu} = props;
-
-  const {balanceDetails} = useSelector<RootState, BalanceState>(state => state.balanceState);
   const {user} = useSelector<RootState, UserState>(state => state.userState);
 
   if (!user) return null;
 
-  // User should have defaultCurrency
-  if (!user.defaultCurrency) return null;
+  const {togglePrimaryCoinMenu} = props;
+
+  const {balanceDetails} = useSelector<RootState, BalanceState>(state => state.balanceState);
 
   return (
     <PrimaryCoinMenu
       togglePrimaryCoinMenu={togglePrimaryCoinMenu}
       balanceDetails={balanceDetails}
-      defaultCurrency={user.defaultCurrency}
+      user={user}
     />
   );
 };
