6565245f4a709b040ddba9d2f5b5a06531a750fc
Mucktar Pakpahan <mucktar.pakpahan@gmail.com>
2021-10-05 22:56:40 +0700

    MYR-822: add social account

--- src/components-v2/PostImport/PostImport.styles.ts
+++ src/components-v2/PostImport/PostImport.styles.ts
@@ -2,7 +2,9 @@ import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
 
 export const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: {},
+    root: {
+      width: 640,
+    },
     input: {
       marginBottom: 30,
     },
