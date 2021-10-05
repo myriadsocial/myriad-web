17863a4539d94147bed92a719c744af4a33a4245
ical10 <mochhusnir@gmail.com>
2021-10-05 14:18:08 +0700

    MYR-875: added loading state and router.push to /wallet

--- src/components-v2/WalletBalance/index.tsx
+++ src/components-v2/WalletBalance/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
+import {useRouter} from 'next/router';
+
 import {Typography} from '@material-ui/core';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
 
 import {BalanceDetail} from '../../interfaces/balance';
@@ -14,19 +17,37 @@ type WalletProps = {
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {},
+    loading: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: 360,
+    },
   }),
 );
 
 export const WalletBalances: React.FC<WalletProps> = ({balances}) => {
   const styles = useStyles();
 
+  const router = useRouter();
+
+  const changeToWalletPage = () => {
+    router.push('/wallet');
+  };
+
   return (
-    <BoxComponent title="Wallet" className={styles.root}>
+    <BoxComponent title="Wallet" onClick={changeToWalletPage} className={styles.root}>
+      {/**handle loading when fetching balance**/}
+      {balances.length === 0 && (
+        <div className={styles.loading}>
+          <CircularProgress />
+        </div>
+      )}
       {balances.map(balance => (
         <ListItemComponent
-          key={balance.tokenSymbol}
-          title={balance.tokenSymbol}
-          avatar={balance.tokenImage}
+          key={balance.id}
+          title={balance.id}
+          avatar={balance.image}
           action={<Typography variant="h5">{balance.freeBalance}</Typography>}
         />
       ))}
