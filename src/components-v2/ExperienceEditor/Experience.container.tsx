8fc6c72f355ea0ff40ee33779645ec892b2bd118
imam hermawan <immd.flores@gmail.com>
2021-10-05 16:05:58 +0700

    MYR-872: wiring create experience

--- src/components-v2/ExperienceEditor/Experience.container.tsx
+++ src/components-v2/ExperienceEditor/Experience.container.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import {useExperienceHook} from '../../hooks/use-experience-hook';
 import {Experience} from '../../interfaces/experience';
+<<<<<<< HEAD
 import {ExperienceEditor} from './ExperienceEditor';
 
 import {debounce} from 'lodash';
@@ -10,6 +11,49 @@ import {useImageUpload} from 'src/hooks/use-image-upload.hook';
 export const ExperienceContainer: React.FC = () => {
   const {selectedExperience, saveExperience, searchTags, tags, searchPeople, people} =
     useExperienceHook();
+=======
+import {SocialsEnum} from '../../interfaces/social';
+import {ExperienceEditor} from './ExperienceEditor';
+
+import {useImageUpload} from 'src/hooks/use-image-upload.hook';
+
+const experience = {
+  tags: [
+    {
+      id: 'crypto',
+      count: 0,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      id: 'nsfw',
+      count: 0,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  ],
+  people: [
+    {
+      id: '1',
+      name: 'Person 1',
+      originUserId: '1',
+      platform: SocialsEnum.FACEBOOK,
+      profilePictureURL: 'https://res.cloudinary.com/dsget80gs/bd75blw2pnmpj9aqwdxm.png',
+      username: 'personone',
+    },
+    {
+      id: '2',
+      name: 'Person 2',
+      originUserId: '2',
+      platform: SocialsEnum.FACEBOOK,
+      profilePictureURL: 'https://res.cloudinary.com/dsget80gs/rvi6x1stnczatom2jq2y.jpg',
+      username: 'persontwo',
+    },
+  ],
+};
+export const ExperienceContainer: React.FC = () => {
+  const {selectedExperience, saveExperience} = useExperienceHook();
+>>>>>>> 9c84ee61 (MYR-872: wiring create experience)
   const {uploadImage} = useImageUpload();
 
   const onImageUpload = async (files: File[]) => {
@@ -22,6 +66,7 @@ export const ExperienceContainer: React.FC = () => {
     saveExperience(newExperience, newTags);
   };
 
+<<<<<<< HEAD
   const handleSearchTags = debounce((query: string) => {
     searchTags(query);
   }, 300);
@@ -38,6 +83,16 @@ export const ExperienceContainer: React.FC = () => {
       onSearchTags={handleSearchTags}
       onImageUpload={onImageUpload}
       onSearchPeople={handleSearchPeople}
+=======
+  return (
+    <ExperienceEditor
+      experience={selectedExperience}
+      tags={experience.tags}
+      people={experience.people}
+      onSearchTags={console.log}
+      onImageUpload={onImageUpload}
+      onSearchPeople={console.log}
+>>>>>>> 9c84ee61 (MYR-872: wiring create experience)
       onSave={onSave}
     />
   );
