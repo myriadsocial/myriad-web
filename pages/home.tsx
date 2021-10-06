f0955c759799b39d72383b75196e20277b852d3a
ical10 <mochhusnir@gmail.com>
2021-10-06 13:15:11 +0700

    MYR-871: moved balance fetching to component level

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,16 +1,14 @@
 import React, {useEffect} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
+import {useDispatch} from 'react-redux';
 
 import {getSession} from 'next-auth/client';
+import {useRouter} from 'next/router';
 
 import {RichTextContainer} from '../src/components-v2/Richtext/RichTextContainer';
 import {TimelineContainer} from '../src/components-v2/Timeline/TimelineContainer';
 import {SearchBoxContainer} from '../src/components-v2/atoms/Search/SearchBoxContainer';
 import {ToasterContainer} from '../src/components-v2/atoms/Toaster/ToasterContainer';
 import {DefaultLayout} from '../src/components-v2/template/Default/DefaultLayout';
-import {usePolkadotApi} from '../src/hooks/use-polkadot-api.hook';
-import {RootState} from '../src/reducers/';
-import {UserState} from '../src/reducers/user/reducer';
 
 import {healthcheck} from 'src/lib/api/healthcheck';
 import * as UserAPI from 'src/lib/api/user';
@@ -21,28 +19,24 @@ import {wrapper} from 'src/store';
 const Home: React.FC = () => {
   const dispatch = useDispatch();
 
-  const {user, currencies} = useSelector<RootState, UserState>(state => state.userState);
-
-  const {load} = usePolkadotApi();
-
-  if (!user) return null;
-
   useEffect(() => {
     dispatch(fetchConnectedSocials());
     dispatch(fetchAvailableToken());
   }, [dispatch]);
 
-  useEffect(() => {
-    load(user.id, currencies);
-  }, [currencies, user]);
-
   //TODO: any logic + components which replace
   // the middle column of home page should go here
+
+  const router = useRouter();
+
+  const changePageToSearchResult = () => {
+    router.push('/searchResult');
+  };
 
   return (
     <DefaultLayout isOnProfilePage={false}>
       <ToasterContainer />
-      <SearchBoxContainer />
+      <SearchBoxContainer onSubmitSearch={changePageToSearchResult} />
       <RichTextContainer />
       <TimelineContainer />
     </DefaultLayout>
