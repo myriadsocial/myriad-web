5db442c1ecb45af2f35bbe3cc84644b2ffbe92d1
ical10 <mochhusnir@gmail.com>
2021-10-06 13:15:11 +0700

    MYR-871: moved balance fetching to component level

--- src/components-v2/WalletBalance/WalletBalanceContainer.tsx
+++ src/components-v2/WalletBalance/WalletBalanceContainer.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useSelector} from 'react-redux';
 
 import {WalletBalances as WalletBalancesComponent} from '.';
+import {usePolkadotApi} from '../../../src/hooks/use-polkadot-api.hook';
 import {RootState} from '../../reducers/';
+import {UserState} from '../../reducers/user/reducer';
 
 import {BalanceState} from 'src/reducers/balance/reducer';
 
 export const WalletBalancesContainer: React.FC = () => {
+  const {user, currencies} = useSelector<RootState, UserState>(state => state.userState);
+
+  const {load} = usePolkadotApi();
+
+  if (!user) return null;
+
+  useEffect(() => {
+    load(user.id, currencies);
+  }, [currencies, user]);
+
   const {balanceDetails} = useSelector<RootState, BalanceState>(state => state.balanceState);
+
   return <WalletBalancesComponent balances={balanceDetails} />;
 };
