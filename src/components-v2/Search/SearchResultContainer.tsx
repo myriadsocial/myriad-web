78df17e61744ff2a88e1b817556af990755e790e
ical10 <mochhusnir@gmail.com>
2021-10-06 14:28:32 +0700

    MYR-871: wired searchExperience into container

--- src/components-v2/Search/SearchResultContainer.tsx
+++ src/components-v2/Search/SearchResultContainer.tsx
@@ -1,33 +1,63 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 import Typography from '@material-ui/core/Typography';
 
+import {useExperienceHook} from '../../hooks/use-experience-hook';
+import {TimelineContainer} from '../Timeline/';
 import {SearchBoxContainer} from '../atoms/Search/SearchBoxContainer';
+import {TabItems} from '../atoms/Tabs/';
 import {TabsComponent} from '../atoms/Tabs/';
 
 export const SearchResultContainer: React.FC = () => {
-  const [searchResultTabTexts] = useState([
+  const {searchExperience, searchedExperiences, searchPeople} = useExperienceHook();
+
+  const [submittedQuery, setSubmittedQuery] = useState('');
+
+  const [selectedTab, setSelectedTab] = useState('');
+
+  useEffect(() => {
+    switch (selectedTab) {
+      case 'people-tab': {
+        searchPeople(submittedQuery);
+        break;
+      }
+
+      case 'experience-tab': {
+        searchExperience(submittedQuery);
+        break;
+      }
+
+      default: {
+        break;
+      }
+    }
+  }, [selectedTab, submittedQuery]);
+
+  useEffect(() => {
+    console.log({searchedExperiences});
+  }, [searchedExperiences]);
+
+  const [searchResultTabTexts] = useState<TabItems<string>[]>([
     {
-      id: 'first-tab',
+      id: 'post-tab',
       title: 'Post',
-      component: <p>Posts</p>,
+      component: <TimelineContainer />,
     },
     {
-      id: 'second-tab',
+      id: 'people-tab',
       title: 'People',
       component: <p>People</p>,
     },
     {
-      id: 'third-tab',
+      id: 'experience-tab',
       title: 'Experience',
       component: <p>Experience</p>,
     },
   ]);
 
-  const [submittedQuery, setSubmittedQuery] = useState('');
-
-  const handleChangeTab = () => {
+  const handleChangeTab = (currentTab: string) => {
     console.log('changed tab!');
+    setSelectedTab(currentTab);
   };
 
   const onSubmitSearch = (query: string) => {
@@ -43,6 +73,7 @@ export const SearchResultContainer: React.FC = () => {
         active={searchResultTabTexts[0].id}
         tabs={searchResultTabTexts}
         mark="underline"
+        position="left"
         size="medium"
         onChangeTab={handleChangeTab}
       />
