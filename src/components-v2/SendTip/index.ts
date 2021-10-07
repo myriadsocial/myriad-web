51924363a2031017f44ce3558ee18374acdbac3e
ical10 <mochhusnir@gmail.com>
2021-10-07 10:26:46 +0700

    MYR-877: updated index

--- src/components-v2/SendTip/index.ts
+++ src/components-v2/SendTip/index.ts
@@ -1,3 +1,6 @@
+import {SendTip} from './SendTip';
 import {useStyles, TableCell} from './SendTip.styles';
+import {SendTipContainer} from './SendTipContainer';
 
+export {SendTip, SendTipContainer};
 export {useStyles, TableCell};
