b3472da7b24f93180cd6778bf32f0061fc0ebb72
ical10 <mochhusnir@gmail.com>
2021-10-07 10:12:17 +0700

    MYR-877: moved to a common helpers

--- src/helpers/balance.ts
+++ src/helpers/balance.ts
@@ -1,12 +1,24 @@
-import {Currency} from '../interfaces/currency';
+import {BalanceDetail} from '../interfaces/balance';
+import {Currency, CurrencyId} from '../interfaces/currency';
 
+// TODO: check if needs to be removed
 export const formatNumber = (number: number, decimals: number): number => {
   if (number.toString() === '0') return 0;
   const result = Number((Number(number.toString()) / 10 ** decimals).toFixed(5));
   return result;
 };
 
-// TODO: implement conversion logic
+// TODO: check if needs to be removed
 export const formatUsd = (value: number, currency: Currency): number => {
   return value;
+};
+
+export const removeMyriad = (balanceDetails: BalanceDetail[]): BalanceDetail[] => {
+  const newCoins = [...balanceDetails];
+
+  const myriadlessCoins = _.remove(newCoins, function (n) {
+    return n.id !== CurrencyId.MYRIA;
+  });
+
+  return myriadlessCoins;
 };
