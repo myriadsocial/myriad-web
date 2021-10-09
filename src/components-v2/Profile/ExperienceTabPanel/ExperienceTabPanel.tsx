f509139765931433567e23d8f9ca04d1a0759c33
Mucktar Pakpahan <mucktar.pakpahan@gmail.com>
2021-10-09 15:21:29 +0700

    MYR-882: init filter timeline by experience

--- src/components-v2/Profile/ExperienceTabPanel/ExperienceTabPanel.tsx
+++ src/components-v2/Profile/ExperienceTabPanel/ExperienceTabPanel.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 
-import {UserExperience} from '../../../interfaces/experience';
+import {Experience, UserExperience} from '../../../interfaces/experience';
 import {ExperienceList} from '../../ExperienceList';
 import {experienceFilterOptions} from '../../Timeline/default';
 import {FilterDropdownMenu} from '../../atoms/FilterDropdownMenu';
@@ -8,12 +8,16 @@ import {FilterDropdownMenu} from '../../atoms/FilterDropdownMenu';
 type ExperienceTabPanelProps = {
   experiences: Experience[];
   isOnHomePage?: boolean;
+  onFilter: (type: ExperienceType) => void;
+  onSelect: (type: TimelineType, experience: Experience) => void;
 };
 
 export const ExperienceTabPanel: React.FC<ExperienceTabPanelProps> = props => {
   const {experiences, isOnHomePage} = props;
 
-  const [title] = useState('Filter by');
+  const handleFilterTimeline = (type: TimelineType, experience: Experience) => {
+    onSelect(type, experience);
+  };
 
   return (
     <>
