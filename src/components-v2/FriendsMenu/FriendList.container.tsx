22584189c74d84428310bb675f74b5642d648a4f
Mucktar Pakpahan <mucktar.pakpahan@gmail.com>
2021-10-10 19:05:58 +0700

    MYR-882: update profile friends & experience

--- src/components-v2/FriendsMenu/FriendList.container.tsx
+++ src/components-v2/FriendsMenu/FriendList.container.tsx
@@ -1,8 +1,8 @@
 import React, {useEffect} from 'react';
 import {useSelector} from 'react-redux';
 
+import {FriendListComponent} from './FriendList';
 import {FriendType} from './default';
-import {FriendListComponent} from './friend-list';
 
 import {useFriendsHook} from 'src/hooks/use-friends-hook';
 import {User} from 'src/interfaces/user';
@@ -11,11 +11,12 @@ import {FriendState} from 'src/reducers/friend/reducer';
 
 type FriendListContainerProps = {
   user?: User;
+  disableFilter?: boolean;
 };
 
 export const FriendListContainer: React.FC<FriendListContainerProps> = props => {
-  const {user} = props;
-  const {loadFriends, searchFriend} = useFriendsHook();
+  const {user, disableFilter} = props;
+  const {loadFriends, searchFriend} = useFriendsHook(user);
 
   const {friends} = useSelector<RootState, FriendState>(state => state.friendState);
 
@@ -29,6 +30,7 @@ export const FriendListContainer: React.FC<FriendListContainerProps> = props => 
 
   return (
     <FriendListComponent
+      disableFilter={disableFilter}
       friends={friends}
       user={user}
       onSearch={searchFriend}
