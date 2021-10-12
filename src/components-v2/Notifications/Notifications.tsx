7ff2181a571b68417ebf9783d8563a6bffe83b34
Mucktar Pakpahan <mucktar.pakpahan@gmail.com>
2021-10-12 08:15:04 +0700

    MYR-882: update notification list

--- src/components-v2/Notifications/Notifications.tsx
+++ src/components-v2/Notifications/Notifications.tsx
@@ -2,24 +2,23 @@ import React from 'react';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import Lottie from 'react-lottie';
 
-import Link from 'next/link';
-
-import {Button, Grid, ListItemSecondaryAction} from '@material-ui/core';
+import {Badge, Button, Grid, ListItemSecondaryAction} from '@material-ui/core';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import ListItemText from '@material-ui/core/ListItemText';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import {Theme, withStyles, createStyles} from '@material-ui/core/styles';
 
 import LoadingAnimation from '../../lottie/loading.json';
 import {useStyles} from './Notifications.styles';
+import {useNotificationList} from './hooks/use-notification-list.hook';
 
 import {formatDistance, subDays} from 'date-fns';
 import {AvatarComponent} from 'src/components/common/Avatar.component';
-import ShowIf from 'src/components/common/show-if.component';
 import {acronym} from 'src/helpers/string';
-import {Notification, NotificationType} from 'src/interfaces/notification';
+import {Notification} from 'src/interfaces/notification';
 
 type NotificationsProps = {
   notifications: Notification[];
@@ -28,6 +27,19 @@ type NotificationsProps = {
   onLoadNextPage: () => void;
   onMarkAllAsRead: () => void;
 };
+
+const StyledBadge = withStyles((theme: Theme) =>
+  createStyles({
+    badge: {
+      right: 14,
+      top: 36,
+      width: 20,
+      height: 20,
+      padding: 0,
+      overflow: 'hidden',
+    },
+  }),
+)(Badge);
 
 export const Notifications: React.FC<NotificationsProps> = props => {
   const {notifications, hasMore, onLoadNextPage, onMarkAllAsRead} = props;
@@ -42,6 +54,8 @@ export const Notifications: React.FC<NotificationsProps> = props => {
       preserveAspectRatio: 'xMidYMid slice',
     },
   };
+
+  const list = useNotificationList(notifications);
 
   return (
     <Paper className={style.root}>
@@ -59,35 +73,24 @@ export const Notifications: React.FC<NotificationsProps> = props => {
           dataLength={notifications.length}
           hasMore={hasMore}
           next={onLoadNextPage}
-          loader={<Lottie options={lottieLoading} height={50} width={50} />}>
-          {notifications.map(notification => {
+          loader={<Lottie options={lottieLoading} height={50} width={50} />}
+        >
+          {list.map(notification => {
             return (
               <ListItem key={notification.id} className={style.item} alignItems="center">
                 <ListItemAvatar>
-                  <AvatarComponent
-                    className={style.avatar}
-                    src={notification.fromUserId.profilePictureURL || ''}>
-                    {acronym(notification.fromUserId.name)}
-                  </AvatarComponent>
+                  <StyledBadge badgeContent={notification.badge}>
+                    <AvatarComponent className={style.avatar} src={notification.avatar}>
+                      {acronym(notification.user)}
+                    </AvatarComponent>
+                  </StyledBadge>
                 </ListItemAvatar>
                 <ListItemText>
                   <Typography variant="h5" color="textPrimary">
-                    <ShowIf
-                      condition={[
-                        NotificationType.FRIEND_REQUEST,
-                        NotificationType.FRIEND_ACCEPT,
-                        NotificationType.POST_COMMENT,
-                      ].includes(notification.type)}>
-                      {notification.fromUserId.name}
-                    </ShowIf>
+                    {notification.user}
                   </Typography>
                   <Typography variant="body1" color="textSecondary">
-                    <Link href={`/${notification.from}`}>
-                      <a href={`/${notification.from}`} className={style.link}>
-                        {notification.fromUserId.name}
-                      </a>
-                    </Link>{' '}
-                    {notification.message}
+                    {notification.description}
                   </Typography>
                 </ListItemText>
                 <ListItemSecondaryAction>
