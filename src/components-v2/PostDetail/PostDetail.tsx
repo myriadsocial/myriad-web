57b198226582b20d6880668da47a41dc1aff3961
Mucktar Pakpahan <mucktar.pakpahan@gmail.com>
2021-10-12 19:38:07 +0700

    MYR-882: add report post

--- src/components-v2/PostDetail/PostDetail.tsx
+++ src/components-v2/PostDetail/PostDetail.tsx
@@ -232,8 +232,7 @@ export const PostDetail: React.FC<PostDetailProps> = props => {
           variant={ButtonVariant.OUTLINED}
           color={ButtonColor.SECONDARY}
           size={ButtonSize.SMALL}
-          className={styles.sendTips}
-        >
+          className={styles.sendTips}>
           Send Tip
         </Button>
       </div>
