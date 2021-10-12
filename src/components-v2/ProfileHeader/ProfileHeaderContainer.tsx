70a64bb62efa39b65da84b1e736fffd0a94672e6
imam hermawan <immd.flores@gmail.com>
2021-10-12 14:56:25 +0800

    MYR-947: added more condition notification status

--- src/components-v2/ProfileHeader/ProfileHeaderContainer.tsx
+++ src/components-v2/ProfileHeader/ProfileHeaderContainer.tsx
@@ -1,17 +1,131 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {useSelector} from 'react-redux';
+
+import {signOut} from 'next-auth/client';
+import {useRouter} from 'next/router';
+
+import {Button, Link, Typography} from '@material-ui/core';
+
+import {InjectedAccountWithMeta} from '@polkadot/extension-inject/types';
 
 import {ProfileHeader as ProfileHeaderComponent} from '.';
-import {ProfileHeader} from './ProfileHeader.stories';
+import {PolkadotAccountList} from '../PolkadotAccountList';
+
+import {useAuthHook} from 'src/hooks/auth.hook';
+import {usePolkadotExtension} from 'src/hooks/use-polkadot-app.hook';
+import {RootState} from 'src/reducers';
+import {countNewNotification} from 'src/reducers/notification/actions';
+import {NotificationState} from 'src/reducers/notification/reducer';
+import {UserState} from 'src/reducers/user/reducer';
+import {Prompt} from 'src/stories/Prompt.stories';
 
 export const ProfileHeaderContainer: React.FC = () => {
+  const {user, alias, anonymous} = useSelector<RootState, UserState>(state => state.userState);
+  const {total} = useSelector<RootState, NotificationState>(state => state.notificationState);
+
+  const router = useRouter();
+
+  const {enablePolkadotExtension, getPolkadotAccounts} = usePolkadotExtension();
+  const {logout, signInWithAccount} = useAuthHook();
+
+  const [accountListOpen, setAccountListOpen] = useState(false);
+  const [extensionInstalled, setExtensionInstalled] = useState(false);
+  const [accounts, setAccounts] = useState<InjectedAccountWithMeta[]>([]);
+
+  useEffect(() => {
+    if (extensionInstalled) {
+      getAvailableAccounts();
+    }
+  }, [extensionInstalled]);
+
+  useEffect(() => {
+    countNewNotification();
+  }, []);
+
+  const checkExtensionInstalled = async () => {
+    const installed = await enablePolkadotExtension();
+
+    setAccountListOpen(true);
+    setExtensionInstalled(installed);
+  };
+
+  const getAvailableAccounts = async () => {
+    const accounts = await getPolkadotAccounts();
+
+    setAccounts(accounts);
+  };
+
+  const handleViewProfile = () => {
+    if (user && !anonymous) {
+      router.push(`/profile/${user.id}`);
+    }
+  };
+
+  const handleSignOut = async () => {
+    if (anonymous === false) {
+      logout();
+    } else {
+      await signOut({
+        callbackUrl: process.env.NEXT_PUBLIC_APP_URL,
+        redirect: true,
+      });
+    }
+  };
+
+  const handleSwitchAccount = () => {
+    checkExtensionInstalled();
+  };
+
+  const closeAccountList = () => {
+    setAccountListOpen(false);
+  };
+
+  const handleShowNotificationList = () => {
+    if (user && !anonymous) {
+      router.push(`/notification`);
+    }
+  };
+
   return (
-    <ProfileHeaderComponent
-      name={ProfileHeader.args?.name ?? 'Name'}
-      username={ProfileHeader.args?.username ?? 'Username'}
-      avatar={
-        ProfileHeader.args?.avatar ??
-        'https://res.cloudinary.com/dsget80gs/icon/Ellipse_445aaron.svg'
-      }
-    />
+    <>
+      <ProfileHeaderComponent
+        user={user}
+        alias={alias}
+        notificationCount={total}
+        onViewProfile={handleViewProfile}
+        onSwitchAccount={handleSwitchAccount}
+        handleSignOut={handleSignOut}
+        onShowNotificationList={handleShowNotificationList}
+      />
+
+      <PolkadotAccountList
+        isOpen={accountListOpen && extensionInstalled}
+        accounts={accounts}
+        onSelect={signInWithAccount}
+        onClose={closeAccountList}
+      />
+
+      <Prompt
+        title="Account Not Found"
+        icon="warning"
+        open={accountListOpen && !extensionInstalled}
+        onCancel={closeAccountList}
+        subtitle={
+          <Typography>
+            Kindly check if you have{' '}
+            <Link
+              href="https://polkadot.js.org/extension"
+              target="_blank"
+              style={{color: 'rgb(255, 140, 0)'}}>
+              Polkadot.js
+            </Link>{' '}
+            installed on your browser
+          </Typography>
+        }>
+        <Button size="small" variant="contained" color="primary" onClick={closeAccountList}>
+          Close
+        </Button>
+      </Prompt>
+    </>
   );
 };
