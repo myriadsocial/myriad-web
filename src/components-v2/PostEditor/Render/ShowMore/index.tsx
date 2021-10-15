2222f1da3f44f0f03bb2c3bb6e6c2adddd11e83c
Mucktar Pakpahan <mucktar.pakpahan@gmail.com>
2021-10-15 07:38:20 +0700

    MYR-870: apply readmore element

---
+++ src/components-v2/PostEditor/Render/ShowMore/index.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+import {Typography} from '@material-ui/core';
+
+export const ELEMENT_SHOW_MORE = 'show_more';
+
+type ShowMoreProps = {
+  onClick: () => void;
+};
+
+export const ShowMore = (props: ShowMoreProps) => {
+  const {onClick} = props;
+
+  return (
+    <Typography onClick={onClick} component="span" color="textPrimary">
+      ...&nbsp; <span style={{color: '#7342CC', cursor: 'hand'}}>more</span>
+    </Typography>
+  );
+};
