c38c4b037cac278912676d4578e2b09746430953
ical10 <mochhusnir@gmail.com>
2021-10-16 12:58:47 +0800

    MYR-871: corrected hover state on button

--- src/components-v2/UsersList/UsersList.tsx
+++ src/components-v2/UsersList/UsersList.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
 
 import {List} from '@material-ui/core';
+import {createStyles, makeStyles, alpha} from '@material-ui/core/styles';
 
 import {acronym} from '../../helpers/string';
 import {User} from '../../interfaces/user';
 import {UsersListItem} from '../UsersList/UsersListItem';
+
+export const useStyles = makeStyles(() =>
+  createStyles({
+    root: {
+      '& .MuiListItem-button:hover': {
+        backgroundColor: alpha('#FFC857', 0.15),
+      },
+    },
+  }),
+);
 
 type UsersListProps = {
   users: User[];
 };
 
 export const UsersList: React.FC<UsersListProps> = ({users}) => {
+  const classes = useStyles();
+
   return (
-    <List>
+    <List className={classes.root}>
       {users.length &&
         users.map(user => (
           <UsersListItem
