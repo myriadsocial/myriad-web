fa4431d4d0051e12ac6f543980e6dd04de78677f
Mucktar Pakpahan <mucktar.pakpahan@gmail.com>
2021-10-16 14:35:16 +0700

    MYR-000: fix rebase issue

--- src/components-v2/Profile/ExperienceTabPanel/ExperienceTabPanel.tsx
+++ src/components-v2/Profile/ExperienceTabPanel/ExperienceTabPanel.tsx
@@ -1,24 +1,43 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 import {Experience, UserExperience} from '../../../interfaces/experience';
 import {ExperienceList} from '../../ExperienceList';
-import {experienceFilterOptions} from '../../Timeline/default';
+import {experienceFilterOptions, ExperienceType} from '../../Timeline/default';
 import {FilterDropdownMenu} from '../../atoms/FilterDropdownMenu';
+
+import {TimelineType} from 'src/interfaces/timeline';
 
 type ExperienceTabPanelProps = {
   experiences: UserExperience[];
   isOnHomePage?: boolean;
+  onFilter: (type: ExperienceType) => void;
+  onSelect: (type: TimelineType, experience: Experience) => void;
 };
 
 export const ExperienceTabPanel: React.FC<ExperienceTabPanelProps> = props => {
-  const {experiences, isOnHomePage} = props;
+  const {experiences, isOnHomePage = false, onFilter, onSelect} = props;
 
-  const [title] = useState('Filter by');
+  const handleFilterSelected = (selected: string) => {
+    onFilter(selected as ExperienceType);
+  };
+
+  const handleFilterTimeline = (type: TimelineType, experience: Experience) => {
+    onSelect(type, experience);
+  };
 
   return (
     <>
-      <FilterDropdownMenu title={title} options={experienceFilterOptions} />
-      <ExperienceList isOnHomePage={isOnHomePage} experiences={experiences} />
+      <FilterDropdownMenu
+        title="Filter by"
+        options={experienceFilterOptions}
+        onChange={handleFilterSelected}
+      />
+
+      <ExperienceList
+        isOnHomePage={isOnHomePage}
+        experiences={experiences}
+        filterTimeline={handleFilterTimeline}
+      />
     </>
   );
 };
