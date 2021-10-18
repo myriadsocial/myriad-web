da234d405806aa1c0d937ae4b453882a1942a865
imam hermawan <immd.flores@gmail.com>
2021-10-18 17:25:26 +0800

    MYR-000: added block feature in profile component

--- src/components-v2/Profile/ProfileHeader/profile-header.style.ts
+++ src/components-v2/Profile/ProfileHeader/profile-header.style.ts
@@ -125,5 +125,19 @@ export const useStyles = makeStyles((theme: Theme) =>
     delete: {
       color: '#FE3636',
     },
+    'flex-center': {
+      display: 'flex',
+      justifyContent: 'center',
+    },
+    m1: {
+      marginRight: theme.spacing(2),
+    },
+    error: {
+      background: '#FE3636',
+      color: '#FFF',
+      '&:hover': {
+        color: theme.palette.text.primary,
+      },
+    },
   }),
 );
