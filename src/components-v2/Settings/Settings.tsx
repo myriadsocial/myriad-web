fabbdfffadf3ad00d991b05cc02ca008da11138f
ical10 <mochhusnir@gmail.com>
2021-10-19 16:43:14 +0800

    MYR-873: applied proper styling on hover

--- src/components-v2/Settings/Settings.tsx
+++ src/components-v2/Settings/Settings.tsx
@@ -2,16 +2,7 @@ import {ChevronRightIcon} from '@heroicons/react/outline';
 
 import React, {useState} from 'react';
 
-import {
-  IconButton,
-  List,
-  ListItem,
-  ListItemSecondaryAction,
-  ListItemText,
-  Paper,
-  SvgIcon,
-  Typography,
-} from '@material-ui/core';
+import {List, ListItem, ListItemText, Paper, SvgIcon, Typography} from '@material-ui/core';
 
 import {useStyles} from './Settings.styles';
 import {SettingsOption, useSettingList} from './hooks/use-setting-list.hook';
@@ -24,7 +15,7 @@ export const Settings: React.FC<SettingsProps> = () => {
   const styles = useStyles();
   const settings = useSettingList();
 
-  const [selected, setSelectedSetting] = useState<SettingsOption | null>(null);
+  const [selected] = useState<SettingsOption | null>(null);
 
   return (
     <Paper className={styles.root}>
@@ -36,7 +27,7 @@ export const Settings: React.FC<SettingsProps> = () => {
         <List>
           {settings.map(item => {
             return (
-              <ListItem key={item.id} className={styles.option} alignItems="center">
+              <ListItem key={item.id} button className={styles.option} alignItems="center">
                 <ListItemText>
                   <Typography variant="h5" color="textPrimary">
                     {item.title}
@@ -45,11 +36,9 @@ export const Settings: React.FC<SettingsProps> = () => {
                     {item.subtitle}
                   </Typography>
                 </ListItemText>
-                <ListItemSecondaryAction>
-                  <IconButton onClick={() => setSelectedSetting(item)}>
-                    <SvgIcon component={ChevronRightIcon} />
-                  </IconButton>
-                </ListItemSecondaryAction>
+                <div className="hidden-button">
+                  <SvgIcon component={ChevronRightIcon} />
+                </div>
               </ListItem>
             );
           })}
