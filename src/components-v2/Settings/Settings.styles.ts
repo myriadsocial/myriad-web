8ca793fe8ea8d9b3f305b0f3d748c2056297a3aa
ical10 <mochhusnir@gmail.com>
2021-10-19 16:43:14 +0800

    MYR-873: applied proper styling on hover

--- src/components-v2/Settings/Settings.styles.ts
+++ src/components-v2/Settings/Settings.styles.ts
@@ -3,16 +3,27 @@ import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
 export const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
-      padding: 30,
+      padding: theme.spacing(3.75, 0),
     },
     title: {
       fontSize: theme.typography.h4.fontSize,
       fontWeight: 700,
+      paddingLeft: theme.spacing(3.75),
+      paddingBottom: theme.spacing(5),
     },
     option: {
-      paddingLeft: 0,
+      paddingLeft: 30,
       paddingTop: 18,
       paddingBottom: 18,
+      '&:hover ': {
+        background: `rgba(255, 200, 87, 0.15)`,
+      },
+      '& .hidden-button': {
+        display: 'none',
+      },
+      '&:hover .hidden-button': {
+        display: 'flex',
+      },
     },
   }),
 );
