9d29c4a223aacf02cb503cc81f25a23555befe69
ical10 <mochhusnir@gmail.com>
2021-10-19 10:19:04 +0800

    MYR-871: fixed types error on build

--- src/components-v2/ExperienceList/ExperienceList.stories.tsx
+++ src/components-v2/ExperienceList/ExperienceList.stories.tsx
@@ -3,6 +3,7 @@ import {ComponentStory, ComponentMeta} from '@storybook/react';
 import React from 'react';
 
 import {CurrencyId} from '../../interfaces/currency';
+import {SocialsEnum} from '../../interfaces/social';
 import SimpleList from './ExperienceList';
 
 export default {
@@ -16,57 +17,61 @@ export const ExperienceList = Template.bind({});
 ExperienceList.args = {
   experiences: [
     {
-      id: 'string',
-      experienceId: '6123614bf6c45c2728e43aca',
-      createdAt: new Date('2021-07-15T03:40:23.000Z'),
-      updatedAt: new Date('2021-09-03T06:46:39.000Z'),
+      id: '61274967e2f91547504ee93e',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      experienceId: '61274967e2f91569384ee93d',
       experience: {
-        createdAt: new Date(),
-        updatedAt: new Date('2021-09-03T06:46:39.000Z'),
-        createdBy: '6123614bf6c45c2728e43aca',
-        id: '6123614bf6c45c2728e43aca',
-        name: 'Cryptowatcher',
-        people: [],
-        tags: [],
+        id: '61274967e2f91569384ee93d',
+        name: 'to the moon crypto',
         user: {
-          defaultCurrency: CurrencyId.AUSD,
-          id: '0x76e85125c8a6997e5e40e5e1f0a64a8154b5c69308e54b426ad3a34af1545b1d',
-          name: 'Lara Schoffield',
+          id: '0x443765fd2a8eff908ffbf8b0fdb45e404fc9cf19cd0003d37203f12d81926859',
+          name: 'Myriad Enthusiast',
+          username: 'myriadenthusiast.h4revxhzk',
           profilePictureURL:
-            'https://gist.githack.com/ical10/8178e2e0d345c691ceffd0b9bdb065a0/raw/e6a2e66955752fa1203667ab80b8dab0b09c9aa6/stonks.svg',
-          bio: 'Lara Schoffield',
-          fcmTokens: [],
-          currencies: [],
-          createdAt: new Date('2021-07-15T03:40:23.000Z'),
-          updatedAt: new Date('2021-09-03T06:46:39.000Z'),
+            'https://res.cloudinary.com/dsget80gs/image/upload/v1633668205/qkqi87d7hydcwgjjn5dn.jpg',
+          bannerImageUrl:
+            'https://res.cloudinary.com/dsget80gs/image/upload/v1633668278/au2ajqyk1uc6lrgple8o.png',
+          bio: 'Hello, my name is Myriad enthusiast!',
+          onTimeline: '61274967e2f91569384ee93d',
+          defaultCurrency: CurrencyId.DOT,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          currencies: [
+            {
+              id: CurrencyId.ACA,
+              image: 'https://res.cloudinary.com/dsget80gs/coins/aca.svg',
+              decimal: 13,
+              rpcURL: 'wss://acala-mandala.api.onfinality.io/public-ws',
+              native: true,
+              createdAt: new Date(),
+              updatedAt: new Date(),
+            },
+          ],
         },
-      },
-    },
-    {
-      id: 'string',
-      experienceId: '6123614bf6c45c2728e43aca',
-      createdAt: new Date('2021-07-15T03:40:23.000Z'),
-      updatedAt: new Date('2021-09-03T06:46:39.000Z'),
-      experience: {
+        tags: [
+          {
+            id: '#cool',
+            count: 1,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+        ],
+        people: [
+          {
+            id: '60efac8c565ab8004ed28ba7',
+            name: 'Gavin Wood',
+            username: 'gavofyork',
+            platform: SocialsEnum.TWITTER,
+            originUserId: '33962758',
+            profilePictureURL:
+              'https://pbs.twimg.com/profile_images/981390758870683656/RxA_8cyN_400x400.jpg',
+          },
+        ],
+        description: 'best projects in cryptoverse',
         createdAt: new Date(),
-        updatedAt: new Date('2021-09-03T06:46:39.000Z'),
-        createdBy: '6123614bf6c45c2728e43acb',
-        id: '6123614bf6c45c2728e43aca',
-        name: 'Bitcoin Strategy',
-        people: [],
-        tags: [],
-        user: {
-          defaultCurrency: CurrencyId.AUSD,
-          id: '0x76e85125c8a6997e5e40e5e1f0a64a8154b5c69308e54b426ad3a34af1545b1d',
-          name: 'Jenny Chang',
-          profilePictureURL:
-            'https://gist.githack.com/ical10/8178e2e0d345c691ceffd0b9bdb065a0/raw/e6a2e66955752fa1203667ab80b8dab0b09c9aa6/stonks.svg',
-          bio: 'Lara Schoffield',
-          fcmTokens: [],
-          currencies: [],
-          createdAt: new Date('2021-07-15T03:40:23.000Z'),
-          updatedAt: new Date('2021-09-03T06:46:39.000Z'),
-        },
+        updatedAt: new Date(),
+        createdBy: '0x443765fd2a8eff908ffbf8b0fdb45e404fc9cf19cd0003d37203f12d81926859',
       },
     },
   ],
