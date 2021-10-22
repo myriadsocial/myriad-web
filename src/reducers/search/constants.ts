6ae97b4190cf7576dee5386ea019ec35403fb4bd
ical10 <mochhusnir@gmail.com>
2021-10-22 15:33:24 +0800

    MYR-000: created redux interface for searchReducer

--- src/reducers/search/constants.ts
+++ src/reducers/search/constants.ts
@@ -1,8 +1,11 @@
 export const RESET_SEARCH_STATE = 'RESET_SEARCH_STATE';
 export type RESET_SEARCH_STATE = typeof RESET_SEARCH_STATE;
 
-export const SEARCH_USERS = 'SEARCH_USERS';
-export type SEARCH_USERS = typeof SEARCH_USERS;
+export const LOAD_USERS = 'LOAD_USERS';
+export type LOAD_USERS = typeof LOAD_USERS;
+
+export const SET_SEARCHED_USERS = 'SET_SEARCHED_USERS';
+export type SET_SEARCHED_USERS = typeof SET_SEARCHED_USERS;
 
 export const ABORT_SEARCH = 'ABORT_SEARCH';
 export type ABORT_SEARCH = typeof ABORT_SEARCH;
