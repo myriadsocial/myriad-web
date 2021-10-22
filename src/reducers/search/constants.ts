3810979c3a393e0f2e62607bfced259dd736e5e1
ical10 <mochhusnir@gmail.com>
2021-10-22 11:05:51 +0800

    MYR-000: moved search state from context to reducer

---
+++ src/reducers/search/constants.ts
@@ -0,0 +1,8 @@
+export const RESET_SEARCH_STATE = 'RESET_SEARCH_STATE';
+export type RESET_SEARCH_STATE = typeof RESET_SEARCH_STATE;
+
+export const SEARCH_USERS = 'SEARCH_USERS';
+export type SEARCH_USERS = typeof SEARCH_USERS;
+
+export const ABORT_SEARCH = 'ABORT_SEARCH';
+export type ABORT_SEARCH = typeof ABORT_SEARCH;
