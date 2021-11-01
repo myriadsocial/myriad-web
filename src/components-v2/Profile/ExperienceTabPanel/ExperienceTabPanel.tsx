68366c76bb3711621ef4222e702931496611c0fb
ical10 <mochhusnir@gmail.com>
2021-11-01 23:29:13 +0800

    MYR-1093: changed into NonSelectableExperienceList

--- src/components-v2/Profile/ExperienceTabPanel/ExperienceTabPanel.tsx
+++ src/components-v2/Profile/ExperienceTabPanel/ExperienceTabPanel.tsx
@@ -1,28 +1,20 @@
 import React from 'react';
 
-import {Experience, UserExperience} from '../../../interfaces/experience';
-import {ExperienceList} from '../../ExperienceList';
+import {UserExperience} from '../../../interfaces/experience';
+import {NonSelectableExperienceList} from '../../ExperienceList';
 import {experienceFilterOptions, ExperienceType} from '../../Timeline/default';
 import {FilterDropdownMenu} from '../../atoms/FilterDropdownMenu';
 
-import {TimelineType} from 'src/interfaces/timeline';
-
 type ExperienceTabPanelProps = {
   experiences: UserExperience[];
-  isOnHomePage?: boolean;
   onFilter: (type: ExperienceType) => void;
-  onSelect: (type: TimelineType, experience: Experience) => void;
 };
 
 export const ExperienceTabPanel: React.FC<ExperienceTabPanelProps> = props => {
-  const {experiences, isOnHomePage = false, onFilter, onSelect} = props;
+  const {experiences, onFilter} = props;
 
   const handleFilterSelected = (selected: string) => {
     onFilter(selected as ExperienceType);
-  };
-
-  const handleFilterTimeline = (type: TimelineType, experience: Experience) => {
-    onSelect(type, experience);
   };
 
   return (
@@ -33,11 +25,7 @@ export const ExperienceTabPanel: React.FC<ExperienceTabPanelProps> = props => {
         onChange={handleFilterSelected}
       />
 
-      <ExperienceList
-        isOnHomePage={isOnHomePage}
-        experiences={experiences}
-        filterTimeline={handleFilterTimeline}
-      />
+      <NonSelectableExperienceList experiences={experiences} />
     </>
   );
 };
