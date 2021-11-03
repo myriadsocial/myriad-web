073b037e3e8aad67cab9ed0d0685d700b4a84ffe
Mucktar Pakpahan <mucktar.pakpahan@gmail.com>
2021-11-03 22:05:02 +0700

    MYR-1113: add image loading placeholder

--- src/components-v2/PostEditor/PostRender.tsx
+++ src/components-v2/PostEditor/PostRender.tsx
@@ -78,7 +78,14 @@ export const PostRender: React.FC<PostRenderProps> = props => {
   const renderElement = useCallback(
     (node, images: string[] = []) => {
       if (node.text) {
-        if (Object.keys(node).length === 1) return node.text;
+        if (Object.keys(node).length === 1) {
+          return node.text.split('\n').map((item: string, key: number) => (
+            <span key={key}>
+              {item}
+              <br />
+            </span>
+          ));
+        }
 
         return (
           <Typography
