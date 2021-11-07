03d36b756331a8f699016504ff6ef30f87152636
reigun <reinaldo@blocksphere.id>
2021-11-07 19:02:42 +0700

    MYR1122: adding telegram to various files due to linting errors

--- src/helpers/url.ts
+++ src/helpers/url.ts
@@ -6,6 +6,7 @@ const postUrlRegex = {
   [SocialsEnum.FACEBOOK]:
     /^(?:https?:\/\/)?(?:www\.|m\.|mobile\.|touch\.|mbasic\.)?(?:facebook\.com|fb(?:\.me|\.com))\/(?!$)(?:(?:\w)*#!\/)?(?:pages\/)?(?:photo\.php\?fbid=)?(?:[\w\-]*\/)*?(?:\/)?(?:profile\.php\?id=)?([^\/?&\s]*)(?:\/|&|\?)?.*$/g,
   [SocialsEnum.REDDIT]: /(?:^.+?)(?:reddit.com\/r)(?:\/[\w\d]+){2}(?:\/)([\w\d]*)/g,
+  [SocialsEnum.TELEGRAM]: /^https?:\/\/t\.me\/([a-z]+)\/([0-9]+)/,
 };
 
 export const parsePostUrl = (social: SocialsEnum, url: string): RegExpExecArray | null => {
