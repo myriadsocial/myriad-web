4b200a172fe3bc2c2fbb2082a984bc8844582072
Mucktar Pakpahan <mucktar.pakpahan@gmail.com>
2021-11-08 19:51:08 +0700

    MYR-1190: takeout non default socmed

--- src/components-v2/SocialMediaList/use-social-media-list.hook.tsx
+++ src/components-v2/SocialMediaList/use-social-media-list.hook.tsx
@@ -1,14 +1,8 @@
 import React, {useMemo} from 'react';
 
-import FourChanIcon from '../../images/socials/4chan.svg';
 import FacebookIcon from '../../images/socials/facebook.svg';
-import InstagramIcon from '../../images/socials/instagram.svg';
 import RedditIcon from '../../images/socials/reddit.svg';
-import TelegramIcon from '../../images/socials/telegram.svg';
 import TwitterIcon from '../../images/socials/twitter.svg';
-import VKIcon from '../../images/socials/vk.svg';
-import WeChatIcon from '../../images/socials/wechat.svg';
-import WeiboIcon from '../../images/socials/weibo.svg';
 import {SocialsEnum, SocialMedia} from '../../interfaces/social';
 
 export type SocialDetail = {
@@ -26,17 +20,11 @@ function enumKeys<E>(e: E): (keyof E)[] {
 export const useSocialMediaList = (connected: SocialMedia[]): SocialDetail[] => {
   const socials: SocialDetail[] = [];
 
-  const icons: Record<SocialsEnum, JSX.Element> = useMemo(
+  const icons: Partial<Record<SocialsEnum, JSX.Element>> = useMemo(
     () => ({
       [SocialsEnum.FACEBOOK]: <FacebookIcon />,
       [SocialsEnum.REDDIT]: <RedditIcon />,
       [SocialsEnum.TWITTER]: <TwitterIcon />,
-      [SocialsEnum.INSTAGRAM]: <InstagramIcon />,
-      [SocialsEnum.WECHAT]: <WeChatIcon />,
-      [SocialsEnum.TELEGRAM]: <TelegramIcon />,
-      [SocialsEnum.FOURCHAN]: <FourChanIcon />,
-      [SocialsEnum.VK]: <VKIcon />,
-      [SocialsEnum.WEIBO]: <WeiboIcon />,
     }),
     [],
   );
@@ -64,7 +52,7 @@ export const useSocialMediaList = (connected: SocialMedia[]): SocialDetail[] => 
 
     socials.push({
       id: social,
-      icon: icons[social],
+      icon: icons[social] as JSX.Element,
       originId: getSocialOrigin(social),
       connected: isSocialConnected(social),
       username: getSocialUsername(social),
