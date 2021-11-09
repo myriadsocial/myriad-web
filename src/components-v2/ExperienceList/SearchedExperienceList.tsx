68de36edba87062eeb4ccb47581b7cbab9f29fa8
ical10 <mochhusnir@gmail.com>
2021-11-09 12:36:50 +0800

    MYR-1172: added EmptyResult on experience tab

--- src/components-v2/ExperienceList/SearchedExperienceList.tsx
+++ src/components-v2/ExperienceList/SearchedExperienceList.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-import Button from '@material-ui/core/Button';
-import Link from '@material-ui/core/Link';
-import Typography from '@material-ui/core/Typography';
+import {List} from '@material-ui/core';
 
 import {SearchedExperienceListProps, useStyles} from '.';
+import ShowIf from '../../components/common/show-if.component';
+import {EmptyResult} from '../Search/EmptyResult';
 import {NonSelectableSimpleCard} from '../atoms/SimpleCard';
 
 const SearchedExperienceList: React.FC<SearchedExperienceListProps> = ({
@@ -18,39 +18,43 @@ const SearchedExperienceList: React.FC<SearchedExperienceListProps> = ({
 
   return (
     <div className={classes.root}>
-      {experiences.length === 0 && (
-        <div className={classes.empty}>
-          <Typography className={classes.title} component="p">
-            Uh-oh!
-          </Typography>
-          <Typography className={classes.subtitle} color="textSecondary" component="p">
-            No experience found.
-          </Typography>
-          <Link href={'/experience'}>
-            <Button color="primary" variant="contained" size="small">
-              Create Experience
-            </Button>
-          </Link>
-        </div>
-      )}
-      {experiences.length > 0 &&
-        experiences.map(experience => (
-          <div key={experience.id}>
-            <NonSelectableSimpleCard
-              user={user}
-              experienceId={experience.id}
-              title={experience.name ?? 'Unnamed Experience'}
-              creator={experience.user.name ?? 'Unnamed Myrian'}
-              imgUrl={
-                experience.experienceImageURL ??
-                'https://pbs.twimg.com/profile_images/1407599051579617281/-jHXi6y5_400x400.jpg'
-              }
-              onSubscribe={onSubscribe}
-              onFollow={onFollow}
-              onPreview={onPreview}
-            />
-          </div>
-        ))}
+      <ShowIf condition={experiences.length === 0}>
+        <EmptyResult
+          iconPath={'/images/no_experience_icon.svg'}
+          heading={`
+              We can’t find any related experience
+            `}
+          firstLineText={`
+                Make sure you type correctly
+              
+              `}
+          secondLineText={`
+              or try different keywords.
+              `}
+        />
+      </ShowIf>
+
+      <ShowIf condition={experiences.length > 0}>
+        <List className={classes.root}>
+          {experiences.map(experience => (
+            <div key={experience.id}>
+              <NonSelectableSimpleCard
+                user={user}
+                experienceId={experience.id}
+                title={experience.name ?? 'Unnamed Experience'}
+                creator={experience.user.name ?? 'Unnamed Myrian'}
+                imgUrl={
+                  experience.experienceImageURL ??
+                  'https://pbs.twimg.com/profile_images/1407599051579617281/-jHXi6y5_400x400.jpg'
+                }
+                onSubscribe={onSubscribe}
+                onFollow={onFollow}
+                onPreview={onPreview}
+              />
+            </div>
+          ))}
+        </List>
+      </ShowIf>
     </div>
   );
 };
