e4d3643e967cc39553e21faaa617c438aa9af59c
ical10 <mochhusnir@gmail.com>
2021-11-11 12:21:34 +0800

    MYR-1036: moved styles into dedicated file

--- src/components-v2/Notifications/sidebar/Notifications.tsx
+++ src/components-v2/Notifications/sidebar/Notifications.tsx
@@ -9,7 +9,7 @@ import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import ListItemText from '@material-ui/core/ListItemText';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
-import {Theme, withStyles, createStyles} from '@material-ui/core/styles';
+import {withStyles, createStyles} from '@material-ui/core/styles';
 
 import {useNotificationList} from '../hooks/use-notification-list.hook';
 import {useStyles} from './Notifications.styles';
@@ -25,7 +25,7 @@ type NotificationsProps = {
   onMarkAllAsRead: () => void;
 };
 
-const StyledBadge = withStyles((theme: Theme) =>
+const StyledBadge = withStyles(() =>
   createStyles({
     badge: {
       right: 5,
@@ -45,59 +45,59 @@ export const MiniNotifications: React.FC<NotificationsProps> = props => {
 
   const list = useNotificationList(notifications);
 
+  console.log({notifications});
+
   return (
     <Paper className={style.root}>
       <Grid container justifyContent="space-between" alignItems="center">
         <Typography className={style.title} color="textPrimary">
           Notifications
         </Typography>
-        <Button
-          //move this to styles.ts
-          style={{width: 'auto', fontSize: '12px', padding: 0, fontWeight: 'bold', marginRight: 24}}
-          color="primary"
-          onClick={onMarkAllAsRead}>
+        <Button className={style.button} color="primary" onClick={onMarkAllAsRead}>
           Mark all as read
         </Button>
       </Grid>
       <List className={style.list}>
         {list.map(notification => {
           return (
-            <ListItem
-              button
-              component="a"
-              ContainerComponent="div"
-              key={notification.id}
-              className={clsx({
-                [style.item]: true,
-                [style.unread]: !notification.read,
-              })}
-              alignItems="center">
-              <ListItemAvatar>
-                <StyledBadge badgeContent={notification.badge}>
-                  <AvatarComponent className={style.avatar} src={notification.avatar}>
-                    {acronym(notification.user)}
-                  </AvatarComponent>
-                </StyledBadge>
-              </ListItemAvatar>
-              <ListItemText>
-                <Typography className={style.textMain} color="textPrimary">
-                  {notification.user}
-                </Typography>
-                <Typography
-                  style={{maxWidth: '140px'}}
-                  className={style.textSecondary}
-                  color="textSecondary">
-                  {notification.description}
-                </Typography>
-              </ListItemText>
-              <ListItemSecondaryAction classes={{root: style.date}}>
-                <Typography className={style.textSecondary} color="textSecondary">
-                  {formatDistance(subDays(new Date(notification.createdAt), 0), new Date(), {
-                    addSuffix: true,
-                  })}
-                </Typography>
-              </ListItemSecondaryAction>
-            </ListItem>
+            <Link key={notification.id} href={notification.href} passHref>
+              <ListItem
+                button
+                component="a"
+                ContainerComponent="div"
+                key={notification.id}
+                className={clsx({
+                  [style.item]: true,
+                  [style.unread]: !notification.read,
+                })}
+                alignItems="center">
+                <ListItemAvatar>
+                  <StyledBadge badgeContent={notification.badge}>
+                    <AvatarComponent className={style.avatar} src={notification.avatar}>
+                      {acronym(notification.user)}
+                    </AvatarComponent>
+                  </StyledBadge>
+                </ListItemAvatar>
+                <ListItemText>
+                  <Typography className={style.textMain} color="textPrimary">
+                    {notification.user}
+                  </Typography>
+                  <Typography
+                    style={{maxWidth: '140px'}}
+                    className={style.textSecondary}
+                    color="textSecondary">
+                    {notification.description}
+                  </Typography>
+                </ListItemText>
+                <ListItemSecondaryAction classes={{root: style.date}}>
+                  <Typography className={style.textSecondary} color="textSecondary">
+                    {formatDistance(subDays(new Date(notification.createdAt), 0), new Date(), {
+                      addSuffix: true,
+                    })}
+                  </Typography>
+                </ListItemSecondaryAction>
+              </ListItem>
+            </Link>
           );
         })}
       </List>
