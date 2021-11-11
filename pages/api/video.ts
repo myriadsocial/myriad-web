55762d5144198adf76e8d328fcae23b83e651fe1
Mucktar Pakpahan <mucktar.pakpahan@gmail.com>
2021-11-11 08:05:43 +0700

    MYR-966: remove dummy code

--- pages/api/video.ts
+++ pages/api/video.ts
@@ -61,10 +61,6 @@ const handler = nextConnect()
   .post((req: NextApiRequestWithFormData, res: NextApiResponse<ResponseVideoUpload>) => {
     const {path, size} = req.file as Express.Multer.File;
 
-    return res.status(400).json({
-      error: 'Kegedean tong',
-    });
-
     cloudinary.uploader.upload_large(
       path,
       {
