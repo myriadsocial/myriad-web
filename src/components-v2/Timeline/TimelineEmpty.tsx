09a3503a9ed5a5a0c5d66ca92ef0d03371941d1f
ical10 <mochhusnir@gmail.com>
2021-11-15 16:47:19 +0800

    MYR-1203: created a component for when Timeline is empty

---
+++ src/components-v2/Timeline/TimelineEmpty.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import Image from 'next/image';
+
+import Typography from '@material-ui/core/Typography';
+import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
+
+export const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      minHeight: '66vh',
+      //height: 320,
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+  }),
+);
+
+type TimelineEmptyProps = {
+  title: string;
+  subtitle: string;
+  iconPath: string;
+  width: number;
+  height: number;
+  action?: React.ReactNode;
+};
+
+export const TimelineEmpty: React.FC<TimelineEmptyProps> = ({
+  title,
+  subtitle,
+  iconPath,
+  width,
+  height,
+  action,
+}) => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Image src={iconPath} width={width} height={height} />
+      <div style={{marginBottom: 12}}>
+        <Typography style={{fontWeight: 'bold', fontSize: 18}}>{title}</Typography>
+      </div>
+      <div style={{marginBottom: 24}}>
+        <Typography style={{fontWeight: 400, fontSize: 14}}>{subtitle}</Typography>
+      </div>
+      {action ? action : <></>}
+    </div>
+  );
+};
