269f233a6ce4993dfb2640ef2e9a6a07e467098a
Mucktar Pakpahan <mucktar.pakpahan@gmail.com>
2021-11-15 07:12:08 +0700

    MYR-1083: create custom error page

---
+++ src/components-v2/Error/Error.tsx
@@ -0,0 +1,91 @@
+import React from 'react';
+
+import {useRouter} from 'next/router';
+
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
+
+import Logo from 'src/images/Myriad_Full_Logo_Color_1-01_1.svg';
+import Illustration from 'src/images/unexpected-error.svg';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      position: 'relative',
+      textAlign: 'center',
+      background: '#FFF',
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    logo: {
+      marginBottom: 32,
+    },
+    illustration: {},
+    bar: {
+      background: '#FFC857',
+      position: 'absolute',
+      width: '1.48vw',
+      height: '100%',
+      left: 0,
+      top: 0,
+    },
+    title: {
+      lineHeight: '33.6px',
+      marginBottom: 20,
+      fontWeight: 700,
+      fontSize: 28,
+      [theme.breakpoints.down(1346)]: {
+        marginBottom: 8,
+        fontSize: 20,
+      },
+    },
+    subtitle: {
+      lineHeight: '25.1px',
+      marginBottom: 84,
+      fontSize: 20,
+      color: theme.palette.error.main,
+      [theme.breakpoints.down(1346)]: {
+        marginBottom: 42,
+        fontSize: 14,
+      },
+    },
+    button: {},
+  }),
+);
+
+type ErrorProps = {
+  statusCode?: number;
+};
+
+export const Error: React.FC<ErrorProps> = () => {
+  const style = useStyles();
+  const router = useRouter();
+
+  const handleAction = () => {
+    router.push('/home');
+  };
+
+  return (
+    <div className={style.root}>
+      <div>
+        <div className={style.bar} />
+        <div className={style.logo}>
+          <Logo />
+        </div>
+        <div className={style.illustration}>
+          <Illustration />
+        </div>
+        <Typography className={style.title}>
+          Whoops! You&rsquo;ve moved so fast and you got lost
+        </Typography>
+        <Typography className={style.subtitle}>An unexpected error has occured</Typography>
+        <Button onClick={handleAction} className={style.button} variant="contained" color="primary">
+          Back to home
+        </Button>
+      </div>
+    </div>
+  );
+};
--- src/components-v2/Error/index.ts
+++ src/components-v2/Error/index.ts
@@ -1,1 +1,2 @@
 export * from './WithError';
+export * from './Error';
