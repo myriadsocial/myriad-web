9595ae0225d402c22756c9b018ab6d903b70e43b
ical10 <mochhusnir@gmail.com>
2021-11-15 12:23:37 +0800

    MYR-1172: created a proper UI for empty state

--- src/components-v2/Search/EmptyResult.tsx
+++ src/components-v2/Search/EmptyResult.tsx
@@ -1,38 +1,63 @@
 import React from 'react';
 
-import Image from 'next/image';
-
+import Button from '@material-ui/core/Button';
+import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
+import {createStyles, makeStyles} from '@material-ui/core/styles';
 
-type EmptyResultProps = {
-  iconPath: string;
-  heading: string;
-  firstLineText: string;
-  secondLineText: string;
-};
+import {EmptyResultProps, EmptyContentEnum} from './EmptyResult.interfaces';
 
-export const EmptyResult: React.FC<EmptyResultProps> = ({
-  iconPath,
-  heading,
-  firstLineText,
-  secondLineText,
-}) => {
+export const useStyles = makeStyles(() =>
+  createStyles({
+    root: {
+      position: 'relative',
+      '& .MuiListItem-gutters': {
+        paddingLeft: 0,
+        paddingRight: 0,
+      },
+    },
+    empty: {
+      background: '#FFF',
+      height: '491px',
+      width: '100%',
+      borderRadius: 10,
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    title: {
+      fontWeight: 700,
+      fontSize: '18px',
+      marginBottom: '12px',
+    },
+    subtitle: {
+      marginBottom: '40px',
+      fontSize: '14px',
+    },
+  }),
+);
+
+export const EmptyResult: React.FC<EmptyResultProps> = ({emptyContent}) => {
+  const classes = useStyles();
+
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        rowGap: 12,
-      }}>
-      <Image src={iconPath} width={60} height={60} alt="Icon of not found" />
-      <Typography variant="h4" style={{fontWeight: 'bold'}}>
-        {heading}
+    <div className={classes.empty}>
+      <Typography className={classes.title} component="p">
+        Uh-oh!
       </Typography>
-      <div style={{textAlign: 'center', fontWeight: 'normal'}}>
-        <Typography variant="h6">{firstLineText}</Typography>
-        <Typography variant="h6">{secondLineText}</Typography>
-      </div>
+      <Typography className={classes.subtitle} color="textSecondary" component="p">
+        No {emptyContent} found.
+      </Typography>
+      {emptyContent === EmptyContentEnum.EXPERIENCE ? (
+        <Link href={'/experience'}>
+          <Button color="primary" variant="contained" size="small">
+            Create Experience
+          </Button>
+        </Link>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
