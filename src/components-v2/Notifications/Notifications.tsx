2cc5830e857873f7217195aefe5514e44df5ab41
abdulhakim2902 <hakim@blocksphere.id>
2021-12-01 17:46:31 +0800

    MYR-1272: added functionality in notification page

--- src/components-v2/Notifications/Notifications.tsx
+++ src/components-v2/Notifications/Notifications.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import InfiniteScroll from 'react-infinite-scroll-component';
 
+import Link from 'next/link';
+
 import {Badge, Button, Grid, ListItemSecondaryAction} from '@material-ui/core';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -10,11 +12,12 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import {Theme, withStyles, createStyles} from '@material-ui/core/styles';
 
+import PostAvatar from '../atoms/PostHeader/avatar/post-avatar.component';
 import {useStyles} from './Notifications.styles';
 import {useNotificationList} from './hooks/use-notification-list.hook';
 
 import clsx from 'clsx';
-import {formatDistance, subDays} from 'date-fns';
+import {formatDistanceStrict, subDays} from 'date-fns';
 import {Loading} from 'src/components-v2/atoms/Loading';
 import {AvatarComponent} from 'src/components/common/Avatar.component';
 import {acronym} from 'src/helpers/string';
@@ -48,9 +51,14 @@ export const Notifications: React.FC<NotificationsProps> = props => {
 
   const list = useNotificationList(notifications);
 
+  //TODO: this is a duplicate function on PostHeader/index.tsx
+  const handleClickAvatar = (): void => {
+    console.log('clicked!');
+  };
+
   return (
     <Paper className={style.root}>
-      <Grid container justifyContent="space-between">
+      <Grid container justifyContent="space-between" className={style.header}>
         <Typography className={style.title} color="textPrimary">
           Notifications
         </Typography>
@@ -67,36 +75,56 @@ export const Notifications: React.FC<NotificationsProps> = props => {
           loader={<Loading />}>
           {list.map(notification => {
             return (
-              <ListItem
-                key={notification.id}
-                className={clsx({
-                  [style.item]: true,
-                  [style.unread]: !notification.read,
-                })}
-                alignItems="center">
-                <ListItemAvatar>
-                  <StyledBadge badgeContent={notification.badge}>
-                    <AvatarComponent className={style.avatar} src={notification.avatar}>
-                      {acronym(notification.user)}
-                    </AvatarComponent>
-                  </StyledBadge>
-                </ListItemAvatar>
-                <ListItemText>
-                  <Typography variant="h5" color="textPrimary">
-                    {notification.user}
-                  </Typography>
-                  <Typography variant="body1" color="textSecondary">
-                    {notification.description}
-                  </Typography>
-                </ListItemText>
-                <ListItemSecondaryAction>
-                  <Typography variant="body2" color="textPrimary">
-                    {formatDistance(subDays(new Date(notification.createdAt), 0), new Date(), {
-                      addSuffix: true,
-                    })}
-                  </Typography>
-                </ListItemSecondaryAction>
-              </ListItem>
+              <Link key={notification.id} href={notification.href} passHref>
+                <ListItem
+                  key={notification.id}
+                  button
+                  component="a"
+                  ContainerComponent="div"
+                  className={clsx({
+                    [style.item]: true,
+                    [style.unread]: !notification.read,
+                  })}
+                  alignItems="center">
+                  <ListItemAvatar className={style.avatarItem}>
+                    {notification.user === 'Account unlinked' ||
+                    notification.user === 'Account linked' ? (
+                      <PostAvatar
+                        origin={notification.platform ?? 'myriad'}
+                        name={notification.avatar ?? 'Myriad'}
+                        onClick={handleClickAvatar}
+                      />
+                    ) : (
+                      <StyledBadge badgeContent={notification.badge}>
+                        <AvatarComponent className={style.avatar} src={notification.avatar}>
+                          {notification.avatar
+                            ? acronym(notification.avatar)
+                            : acronym(notification.user)}
+                        </AvatarComponent>
+                      </StyledBadge>
+                    )}
+                  </ListItemAvatar>
+                  <ListItemText className={style.textItem}>
+                    <Typography variant="h5" color="textPrimary">
+                      {notification.user}
+                    </Typography>
+                    <Typography variant="body1" color="textSecondary">
+                      {notification.description}
+                    </Typography>
+                  </ListItemText>
+                  <ListItemSecondaryAction className={style.secondaryItem}>
+                    <Typography variant="body2" color="textPrimary">
+                      {formatDistanceStrict(
+                        subDays(new Date(notification.createdAt), 0),
+                        new Date(),
+                        {
+                          addSuffix: true,
+                        },
+                      )}
+                    </Typography>
+                  </ListItemSecondaryAction>
+                </ListItem>
+              </Link>
             );
           })}
         </InfiniteScroll>
