9bcd50f4b3b0b1c92a2b1e2f9d87d042de57d006
kriptonhaz <31912747+kriptonhaz@users.noreply.github.com>
2021-12-23 11:18:12 +0700

    MYR-1422:remove image on empty post (#806)
    Co-authored-by: Irman Nur Muhammad Alamsyah <irmannmal@gmail.com>

--- src/components/Timeline/TimelineEmpty.tsx
+++ src/components/Timeline/TimelineEmpty.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import Image from 'next/image';
-
 import Typography from '@material-ui/core/Typography';
 import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
 
@@ -22,25 +20,14 @@ export const useStyles = makeStyles((theme: Theme) =>
 type TimelineEmptyProps = {
   title: string;
   subtitle: string;
-  iconPath: string;
-  width: number;
-  height: number;
   action?: React.ReactNode;
 };
 
-export const TimelineEmpty: React.FC<TimelineEmptyProps> = ({
-  title,
-  subtitle,
-  iconPath,
-  width,
-  height,
-  action,
-}) => {
+export const TimelineEmpty: React.FC<TimelineEmptyProps> = ({title, subtitle, action}) => {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
-      <Image src={iconPath} width={width} height={height} />
       <div style={{marginBottom: 12}}>
         <Typography style={{fontWeight: 'bold', fontSize: 18}}>{title}</Typography>
       </div>
