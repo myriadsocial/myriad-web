6e2bc1b64efb9f9de1ce0ec5bf90e32d95953bae
ical10 <mochhusnir@gmail.com>
2022-01-06 17:05:41 +0700

    MYR-1178: added estimateFee method

--- src/lib/services/polkadot-js.ts
+++ src/lib/services/polkadot-js.ts
@@ -5,13 +5,7 @@ import {ApiPromise, WsProvider} from '@polkadot/api';
 import {Keyring} from '@polkadot/keyring';
 
 import BN from 'bn.js';
-
-export const connectToBlockchain = async (wsProvider: string): Promise<ApiPromise> => {
-  const provider = new WsProvider(wsProvider);
-  const api: ApiPromise = new ApiPromise(options({provider}));
-  await api.isReadyOrError;
-  return api;
-};
+import {BalanceDetail} from 'src/interfaces/balance';
 
 interface signAndSendExtrinsicProps {
   from: string;
@@ -27,12 +21,85 @@ interface SignTransactionCallbackProps {
   signerOpened?: boolean;
 }
 
+interface EstimateFeeResponseProps {
+  partialFee: string | null;
+  api: ApiPromise | null;
+}
+
+export const connectToBlockchain = async (wsProvider: string): Promise<ApiPromise> => {
+  const provider = new WsProvider(wsProvider);
+  const api: ApiPromise = new ApiPromise(options({provider}));
+  await api.isReadyOrError;
+  return api;
+};
+
+export const estimateFee = async (
+  from: string,
+  to: string,
+  selectedCurrency: BalanceDetail,
+): Promise<EstimateFeeResponseProps> => {
+  try {
+    const {enableExtension} = await import('src/helpers/extension');
+
+    const allAccounts = await enableExtension();
+
+    const keyring = new Keyring();
+
+    const baseAddress = keyring.encodeAddress(from);
+
+    let partialFee: string | null = null;
+
+    let api: ApiPromise | null = null;
+
+    if (allAccounts) {
+      // We select the first account matching baseAddress
+      // `account` is of type InjectedAccountWithMeta
+      const account = allAccounts.find(account => {
+        // address from session must match address on polkadot extension
+        return account.address === baseAddress;
+      });
+
+      // if account has not yet been imported to Polkadot.js extension
+      if (!account) {
+        throw {
+          Error: 'Please import your account first!',
+        };
+      }
+
+      // otherwise if account found
+      if (account) {
+        api = await connectToBlockchain(selectedCurrency.rpcURL);
+
+        if (api) {
+          const RAND_AMOUNT = 123;
+
+          const info = selectedCurrency.image
+            ? await api.tx.balances.transfer(to, RAND_AMOUNT).paymentInfo(from)
+            : await api.tx.currencies
+                .transfer(to, {TOKEN: selectedCurrency.id}, RAND_AMOUNT)
+                .paymentInfo(from);
+
+          if (info) partialFee = info.partialFee.toHuman();
+        }
+      }
+    }
+    return {
+      partialFee,
+      api,
+    };
+  } catch (error) {
+    console.log(error);
+    Sentry.captureException(error);
+    return error;
+  }
+};
+
 export const signAndSendExtrinsic = async (
   {from, to, value, currencyId, wsAddress, native}: signAndSendExtrinsicProps,
   callback?: (param: SignTransactionCallbackProps) => void,
 ): Promise<string | null> => {
   try {
-    const {enableExtension} = await import('../../helpers/extension');
+    const {enableExtension} = await import('src/helpers/extension');
     const {web3FromSource} = await import('@polkadot/extension-dapp');
 
     const allAccounts = await enableExtension();
