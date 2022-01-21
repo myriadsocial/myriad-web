7e2426ec3318539c935234ff13988838b37e5704
ical10 <mochhusnir@gmail.com>
2022-01-21 15:56:43 +0700

    MYR-1480: added important data to access api

--- src/interfaces/session.ts
+++ src/interfaces/session.ts
@@ -2,10 +2,16 @@ export interface SignInCredential {
   name: string;
   anonymous: boolean;
   token: string;
+  nonce: number;
+  encryptionNonce: Uint8Array;
+  signature: string;
   address: string;
 }
 export interface UserSession {
   token?: string;
+  nonce?: number;
+  encryptionNonce?: Uint8Array;
+  signature?: string;
   name: string;
   profilePictureURL?: string;
   anonymous: boolean;
