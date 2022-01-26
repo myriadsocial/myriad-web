8f37ba2632ae3f2da43ade931eb1f33a22874efa
ical10 <mochhusnir@gmail.com>
2022-01-26 17:38:13 +0700

    MYR-1495: added necessary props for pagination

--- src/components/ExperienceList/SearchedExperienceListContainer.tsx
+++ src/components/ExperienceList/SearchedExperienceListContainer.tsx
@@ -7,54 +7,70 @@ import {useExperienceHook} from '../../hooks/use-experience-hook';
 
 import {useToasterSnackHook} from 'src/hooks/use-toaster-snack.hook';
 
-export const SearchedExperienceListContainer: React.FC = () => {
-  const {
-    searchedExperiences,
-    subscribeExperience,
-    unsubscribeExperience,
-    experiences: userExperience,
-  } = useExperienceHook();
-  const {openToasterSnack} = useToasterSnackHook();
-  const router = useRouter();
-
-  const handleSubsibeExperience = (experienceId: string) => {
-    if (userExperience.length === 10) {
-      openToasterSnack({
-        message: 'You can only add up to 10 experiences max',
-        variant: 'warning',
-      });
-    } else {
-      subscribeExperience(experienceId);
-    }
-  };
-
-  const handleUnsubscribeExperience = (experienceId: string) => {
-    unsubscribeExperience(experienceId);
-  };
-
-  const handleCloneExperience = (experienceId: string) => {
-    if (userExperience.length === 10) {
-      openToasterSnack({
-        message: 'You can only add up to 10 experiences max',
-        variant: 'warning',
-      });
-    } else {
-      router.push(`/experience/${experienceId}/clone`);
-    }
-  };
-
-  const handlePreviewExperience = (experienceId: string) => {
-    router.push(`/experience/${experienceId}/preview`);
-  };
-
-  return (
-    <SearchedExperienceList
-      experiences={searchedExperiences}
-      userExperience={userExperience}
-      onSubscribe={handleSubsibeExperience}
-      onUnsubscribe={handleUnsubscribeExperience}
-      onFollow={handleCloneExperience}
-      onPreview={handlePreviewExperience}
-    />
-  );
+type SearchedExperienceListContainerProps = {
+  query: string;
 };
+
+export const SearchedExperienceListContainer: React.FC<SearchedExperienceListContainerProps> =
+  props => {
+    const {query} = props;
+
+    const {
+      page,
+      searchedExperiences,
+      experiences: userExperience,
+      hasMore,
+      searchExperience,
+      subscribeExperience,
+      unsubscribeExperience,
+    } = useExperienceHook();
+    const {openToasterSnack} = useToasterSnackHook();
+    const router = useRouter();
+
+    const handleLoadNextPage = () => {
+      searchExperience(query, page + 1);
+    };
+
+    const handleSubsibeExperience = (experienceId: string) => {
+      if (userExperience.length === 10) {
+        openToasterSnack({
+          message: 'You can only add up to 10 experiences max',
+          variant: 'warning',
+        });
+      } else {
+        subscribeExperience(experienceId);
+      }
+    };
+
+    const handleUnsubscribeExperience = (experienceId: string) => {
+      unsubscribeExperience(experienceId);
+    };
+
+    const handleCloneExperience = (experienceId: string) => {
+      if (userExperience.length === 10) {
+        openToasterSnack({
+          message: 'You can only add up to 10 experiences max',
+          variant: 'warning',
+        });
+      } else {
+        router.push(`/experience/${experienceId}/clone`);
+      }
+    };
+
+    const handlePreviewExperience = (experienceId: string) => {
+      router.push(`/experience/${experienceId}/preview`);
+    };
+
+    return (
+      <SearchedExperienceList
+        loadNextPage={handleLoadNextPage}
+        hasMore={hasMore}
+        experiences={searchedExperiences}
+        userExperience={userExperience}
+        onSubscribe={handleSubsibeExperience}
+        onUnsubscribe={handleUnsubscribeExperience}
+        onFollow={handleCloneExperience}
+        onPreview={handlePreviewExperience}
+      />
+    );
+  };
