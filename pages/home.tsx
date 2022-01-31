cd48aebf488a1903c7c421f380909117529b0ba0
kriptonhaz <kriptonhaz@gmail.com>
2022-01-31 22:07:54 +0700

    myr-1612: add prompt dialog when user is banned

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import {useSelector, useDispatch} from 'react-redux';
 
-import {getSession} from 'next-auth/client';
+import {getSession, useSession} from 'next-auth/client';
 import getConfig from 'next/config';
 import Head from 'next/head';
 import {useRouter} from 'next/router';
+
+import {Button} from '@material-ui/core';
 
 import {RichTextContainer} from '../src/components/Richtext/RichTextContainer';
 import {TimelineContainer} from '../src/components/Timeline/TimelineContainer';
 import {SearchBoxContainer} from '../src/components/atoms/Search/SearchBoxContainer';
 import {DefaultLayout} from '../src/components/template/Default/DefaultLayout';
+import {PromptComponent} from '../src/components/atoms/Prompt/prompt.component';
 
 import Banner from 'src/components/atoms/BannerStatus/BannerStatus';
+import {useAuthHook} from 'src/hooks/auth.hook';
 import {setHeaders} from 'src/lib/api/base';
 import {healthcheck} from 'src/lib/api/healthcheck';
 import {getUserCurrencies} from 'src/reducers/balance/actions';
@@ -19,14 +24,24 @@ import {fetchExchangeRates} from 'src/reducers/exchange-rate/actions';
 import {fetchExperience} from 'src/reducers/experience/actions';
 import {fetchFriend} from 'src/reducers/friend/actions';
 import {countNewNotification} from 'src/reducers/notification/actions';
+import {clearUser} from 'src/reducers/user/actions';
 import {setAnonymous, fetchConnectedSocials, fetchUser} from 'src/reducers/user/actions';
+import {RootState} from 'src/reducers';
+import {UserState} from 'src/reducers/user/reducer';
 import {wrapper} from 'src/store';
 import {ThunkDispatchAction} from 'src/types/thunk';
 
 const {publicRuntimeConfig} = getConfig();
 
 const Home: React.FC = () => {
+  const dispatch = useDispatch();
   const router = useRouter();
+  const [session] = useSession();
+  const {user} = useSelector<RootState, UserState>(state => state.userState);
+  const {logout} = useAuthHook();
+  const [dialogBanned, setDialogBanned] = React.useState({
+    open: false
+  })
 
   const performSearch = (query: string) => {
     const DELAY = 300;
@@ -45,6 +60,22 @@ const Home: React.FC = () => {
     }, DELAY);
   };
 
+
+  React.useEffect(() => {
+    if(user?.deletedAt){
+      setDialogBanned({...dialogBanned, open: true});
+    }
+  }, [user])
+
+  const handleSignOut = async () => {
+    if (session) {
+      logout();
+    } else {
+      dispatch(clearUser());
+      await router.push(`/`);
+    }
+  };
+
   return (
     <DefaultLayout isOnProfilePage={false}>
       <Head>
@@ -55,6 +86,33 @@ const Home: React.FC = () => {
       <SearchBoxContainer onSubmitSearch={performSearch} />
       <RichTextContainer />
       <TimelineContainer />
+      <PromptComponent
+        title={'You have been banned'}
+        subtitle={'This account has been banned due to break our community rule. \n Please contact us if you think this was a mistake'}
+        open={dialogBanned.open}
+        icon="warning"
+        onCancel={() => {
+          setDialogBanned({...dialogBanned, open: false});
+          handleSignOut();
+        }}>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+          }}>
+          <Button
+            size="small"
+            variant="contained"
+            color="primary"
+            onClick={() => {
+              setDialogBanned({...dialogBanned, open: false});
+              handleSignOut();
+              window.open(`mailto:${publicRuntimeConfig.myriadSupportMail}?subject=Complain user banned!`, '_blank');
+            }}>
+            OK
+          </Button>
+        </div>
+      </PromptComponent>
     </DefaultLayout>
   );
 };
