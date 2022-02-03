9f8682069b720d3d0dd243466f171a6f0ba69370
immhrmwn <immd.flores@gmail.com>
2022-02-03 13:25:24 +0700

    MYR-1644: fixed blocklist

--- src/components/UserMenu/UserMenu.container.tsx
+++ src/components/UserMenu/UserMenu.container.tsx
@@ -1,9 +1,8 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {useSelector} from 'react-redux';
 
 import {UserMenu} from './UserMenu';
 
-import {useFriendsHook} from 'src/hooks/use-friends-hook';
 import {RootState} from 'src/reducers';
 import {UserState} from 'src/reducers/user/reducer';
 
@@ -15,13 +14,7 @@ type UserMenuContainerProps = {
 export const UserMenuContainer: React.FC<UserMenuContainerProps> = props => {
   const {anonymous = false, isMyriad = false} = props;
 
-  const {loadFriends} = useFriendsHook();
-
   const {user} = useSelector<RootState, UserState>(state => state.userState);
-
-  useEffect(() => {
-    loadFriends();
-  }, []);
 
   return <UserMenu selected="post" anonymous={anonymous} user={user} isMyriad={isMyriad} />;
 };
