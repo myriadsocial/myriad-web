3e1cef0ce8ee9c474cbef21021404a8b85b897bd
mhusnirizal <mochhusnir@gmail.com>
2022-02-03 14:06:20 +0700

    Handled showing balance correctly when tipping consecutively (#916)
    * MYR-1631: handled properly showing balance when tipping several times
    
    * MYR-1631: fixed tslint error
    
    * MYR-1631: handled parsing tip amount with e number
    
    * MYR-1631: removed logs
    
    Co-authored-by: kriptonhaz <31912747+kriptonhaz@users.noreply.github.com>

--- src/interfaces/balance.ts
+++ src/interfaces/balance.ts
@@ -2,5 +2,6 @@ import {Currency} from './currency';
 
 export type BalanceDetail = Omit<Currency, 'createdAt' | 'updatedAt'> & {
   freeBalance: number;
+  originBalance: number;
   previousNonce: number;
 };
