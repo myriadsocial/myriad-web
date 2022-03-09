32377fa7ed48e8d77f1c67e701b653546214923b
ical10 <mochhusnir@gmail.com>
2022-03-09 14:42:37 +0700

    MYR-1802: removed unused key

--- src/interfaces/session.ts
+++ src/interfaces/session.ts
@@ -1,5 +1,3 @@
-import {WalletTypeEnum} from 'src/lib/api/ext-auth';
-
 export interface SignInCredential {
   token: string;
   initVec: string;
@@ -8,7 +6,6 @@ export interface SignInCredential {
   name: string;
   anonymous: boolean;
   address: string;
-  walletType: WalletTypeEnum;
 }
 export interface UserSession {
   token?: string;
@@ -20,5 +17,4 @@ export interface UserSession {
   anonymous: boolean;
   welcome: boolean;
   address: string;
-  walletType: WalletTypeEnum;
 }
