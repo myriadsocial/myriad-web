37bee3f0582cbbb7d0961c5a003a25114af291f3
Mucktar Pakpahan <mucktar.pakpahan@gmail.com>
2022-03-22 22:21:56 +0700

    MYR-1863: refactor tip history component

--- src/components/TipHistory/TipHistory.container.tsx
+++ src/components/TipHistory/TipHistory.container.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
-import {useSelector} from 'react-redux';
 
+import useTipping from '../common/Tipping/use-tipping.hook';
 import {TipHistory} from './TipHistory';
 
 import {useTipHistory} from 'src/hooks/tip-history.hook';
-import {Comment} from 'src/interfaces/comment';
-import {Post} from 'src/interfaces/post';
+import {ReferenceType} from 'src/interfaces/interaction';
+import {People} from 'src/interfaces/people';
 import {User} from 'src/interfaces/user';
-import {RootState} from 'src/reducers';
+import * as WalletAPI from 'src/lib/api/wallet';
 
 type TipHistoryContainerProps = {
-  onSendTip: (reference: Post | Comment) => void;
+  referenceType: ReferenceType;
 };
 
 export const TipHistoryContainer: React.FC<TipHistoryContainerProps> = props => {
-  const {onSendTip} = props;
+  const {referenceType} = props;
 
+  const tipping = useTipping();
   const {
     isTipHistoryOpen,
     hasMore,
@@ -29,14 +30,40 @@ export const TipHistoryContainer: React.FC<TipHistoryContainerProps> = props => 
     handleLoadNextPage,
   } = useTipHistory();
 
-  const user = useSelector<RootState, User | undefined>(state => state.userState.user);
+  const handleSendTip = async () => {
+    if (!reference) return;
 
-  const handleSendTip = () => {
-    if (reference) {
-      if (user && reference.user.id !== user.id) onSendTip(reference);
+    let receiver: User | People | null = null;
+
+    // if tipping to User
+    if ('username' in reference) {
+      receiver = reference;
     }
 
-    closeTipHistory();
+    // if tipping to Comment
+    if ('section' in reference) {
+      receiver = reference.user;
+    }
+
+    // if tipping to Post
+    if ('platform' in reference) {
+      // if imported
+      if (reference.people) {
+        const {walletAddress} = await WalletAPI.getWalletAddress(reference.id);
+
+        receiver = {...reference.people, walletAddress};
+      } else {
+        receiver = reference.user;
+      }
+    }
+
+    if (!receiver) return;
+
+    tipping.send({
+      receiver,
+      reference,
+      referenceType,
+    });
   };
 
   return (
