685063897456f5329e3fffe06624e42dd72c24a7
Mucktar Pakpahan <mucktar.pakpahan@gmail.com>
2022-03-23 21:31:24 +0700

    MYR-1529: add cookies consent component

---
+++ src/components/common/CookieConsent/CookieConsent.style.ts
@@ -0,0 +1,23 @@
+import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
+
+export const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {},
+    paper: {
+      width: 560,
+      height: 120,
+      padding: 40,
+      background: '#FFF2CC',
+      display: 'flex',
+      flexWrap: 'nowrap',
+      flexDirection: 'row',
+      borderRadius: 10,
+      left: 'auto',
+      marginBottom: 60,
+      marginRight: 30,
+    },
+    link: {
+      textDecoration: 'none',
+    },
+  }),
+);
---
+++ src/components/common/CookieConsent/CookieConsent.tsx
@@ -0,0 +1,57 @@
+import React, {useEffect, useState} from 'react';
+import {useCookies} from 'react-cookie';
+
+import {Button, Drawer, Typography} from '@material-ui/core';
+
+import {useStyles} from './CookieConsent.style';
+
+type CookieConsentProps = {};
+
+const COOKIE_NAME = 'cookie-consent';
+
+export const CookieConsent: React.FC<CookieConsentProps> = props => {
+  const styles = useStyles();
+
+  const [cookies, setCookie] = useCookies([COOKIE_NAME]);
+
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    if (!cookies[COOKIE_NAME]) {
+      setOpen(true);
+    }
+  }, [cookies]);
+
+  const toggleDrawer = () => {
+    setOpen(false);
+  };
+
+  const handleAccept = () => {
+    setCookie(COOKIE_NAME, true);
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <Drawer
+        anchor="bottom"
+        open={open}
+        onClose={toggleDrawer}
+        classes={{
+          root: styles.root,
+        }}
+        PaperProps={{className: styles.paper, square: false}}
+        ModalProps={{keepMounted: true}}>
+        <Typography variant="body1">
+          This website uses cookies to enhance your online experience.
+          <Typography href="/term-of-use" component="a" color="primary" className={styles.link}>
+            Terms and Conditions
+          </Typography>
+        </Typography>
+        <Button variant="contained" size="medium" color="primary" onClick={handleAccept}>
+          Accept
+        </Button>
+      </Drawer>
+    </>
+  );
+};
