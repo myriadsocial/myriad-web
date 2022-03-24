c5ef465af88a0735027f5d88a2407e7e12380f75
Mucktar Pakpahan <mucktar.pakpahan@gmail.com>
2022-03-24 08:39:04 +0700

    MYR-1865: fetch user wallet on topic page (#1054)
    Co-authored-by: kriptonhaz <31912747+kriptonhaz@users.noreply.github.com>

--- pages/topic/[type].tsx
+++ pages/topic/[type].tsx
@@ -28,6 +28,7 @@ import {
   fetchConnectedSocials,
   fetchUser,
   fetchUserExperience,
+  fetchCurrentUserWallets,
 } from 'src/reducers/user/actions';
 import {wrapper} from 'src/store';
 import {ThunkDispatchAction} from 'src/types/thunk';
@@ -117,6 +118,7 @@ export const getServerSideProps = wrapper.getServerSideProps(store => async cont
       dispatch(fetchPopularTopic()),
       dispatch(getUserCurrencies()),
       dispatch(fetchFriend()),
+      dispatch(fetchCurrentUserWallets()),
     ]);
   }
 
