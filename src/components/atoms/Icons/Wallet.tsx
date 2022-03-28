fbe2aa84904302127ecd2fd40877b37881de29f9
mhusnirizal <mochhusnir@gmail.com>
2022-03-28 14:09:19 +0700

    Added Sender Wallet in Login screen (#1064)
    * MYR-1858: added Sender Wallet
    
    * MYR-1858: transformed ListItem into button and removed disabled css
    
    * MYR-1858: changed ListItem for NEAR into button and checked styling
    
    * MYR-1858: removed unused codes
    
    * MYR-1858: hid wallets from different networks when a network selected
    
    * MYR-1858: adjusted footer text

--- src/components/atoms/Icons/Wallet.tsx
+++ src/components/atoms/Icons/Wallet.tsx
@@ -5,10 +5,20 @@ import {SvgIcon, SvgIconProps} from '@material-ui/core';
 import Polkadot24 from 'src/images/network/Polkadot24.svg';
 import CoinbaseDisabled from 'src/images/wallet/coinbase-disabled.svg';
 import MetamaskDisabled from 'src/images/wallet/metamask-disabled.svg';
+import SenderWalletDisabled from 'src/images/wallet/sender-wallet-disabled.svg';
+import SenderWallet from 'src/images/wallet/sender-wallet.svg';
 import TrustDisabled from 'src/images/wallet/trust-disabled.svg';
 
 export const PolkadotWalletIcon: React.FC<SvgIconProps> = props => (
   <SvgIcon component={Polkadot24} viewBox="0 0 32 32" {...props} />
+);
+
+export const SenderWalletIcon: React.FC<SvgIconProps> = props => (
+  <SvgIcon component={SenderWallet} viewBox="0 0 32 32" {...props} />
+);
+
+export const SenderWalletDisabledIcon: React.FC<SvgIconProps> = props => (
+  <SvgIcon component={SenderWalletDisabled} viewBox="0 0 32 32" {...props} />
 );
 
 export const CoinbaseWalletisabledIcon: React.FC<SvgIconProps> = props => (
