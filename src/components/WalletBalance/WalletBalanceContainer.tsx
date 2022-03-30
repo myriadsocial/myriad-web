b4c5bf2bca40f669f68ce2e3a9c60c562f3497ca
kriptonhaz <kriptonhaz@gmail.com>
2022-03-30 21:32:24 +0700

    myr-1878: implement show balance near on wallet sidebar

--- src/components/WalletBalance/WalletBalanceContainer.tsx
+++ src/components/WalletBalance/WalletBalanceContainer.tsx
@@ -6,42 +6,54 @@ import {Typography} from '@material-ui/core';
 import {WalletBalances as WalletBalancesComponent} from '.';
 import {BoxComponent} from '../atoms/Box';
 
+import {useNearApi} from 'src/hooks/use-near-api.hook';
 import {usePolkadotApi} from 'src/hooks/use-polkadot-api.hook';
 import {BalanceDetail} from 'src/interfaces/balance';
+import {NetworkTypeEnum} from 'src/lib/api/ext-auth';
 import {RootState} from 'src/reducers';
 import {BalanceState} from 'src/reducers/balance/reducer';
 import {UserState} from 'src/reducers/user/reducer';
 
 export const WalletBalancesContainer: React.FC = () => {
-  const {balanceDetails} = usePolkadotApi();
+  const {balanceDetails: polkadotBalance} = usePolkadotApi();
+  const {balanceDetails: nearBalance} = useNearApi();
 
-  const {anonymous} = useSelector<RootState, UserState>(state => state.userState);
+  const {anonymous, currentWallet} = useSelector<RootState, UserState>(state => state.userState);
   const {currenciesId} = useSelector<RootState, BalanceState>(state => state.balanceState);
 
   const [filteredBalances, setFilteredBalanced] = useState<BalanceDetail[]>([]);
 
   useEffect(() => {
     handleFilterCurrencies();
-  }, [balanceDetails, currenciesId]);
+  }, [polkadotBalance, nearBalance, currenciesId]);
 
   const handleFilterCurrencies = (): void => {
     if (currenciesId.length) {
-      const data: BalanceDetail[] = [];
+      if (currentWallet?.type === NetworkTypeEnum.POLKADOT) {
+        const data: BalanceDetail[] = [];
 
-      balanceDetails.forEach(coin => {
-        data[currenciesId.indexOf(coin.id)] = coin;
-      });
+        polkadotBalance.forEach(coin => {
+          data[currenciesId.indexOf(coin.id)] = coin;
+        });
 
-      setFilteredBalanced(data);
+        setFilteredBalanced(data);
+      } else if (currentWallet?.type === NetworkTypeEnum.NEAR) {
+        //TODO need to filtered by currenciesId from backend
+        setFilteredBalanced(nearBalance);
+      }
     } else {
-      setFilteredBalanced(balanceDetails);
+      if (currentWallet?.type === NetworkTypeEnum.POLKADOT) {
+        setFilteredBalanced(polkadotBalance);
+      } else if (currentWallet?.type === NetworkTypeEnum.NEAR) {
+        setFilteredBalanced(nearBalance);
+      }
     }
   };
 
   if (anonymous)
     return (
       <BoxComponent title="Wallet">
-        <Typography>Please Login with polkadot account to access this feature</Typography>
+        <Typography>Please Login to access this feature</Typography>
       </BoxComponent>
     );
 
