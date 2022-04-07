aaccee14e9826e741e2f766ab528d9baf62a7888
kriptonhaz <kriptonhaz@gmail.com>
2022-04-07 22:20:59 +0700

    myr-1855: change format balance and currency to support near

--- src/components/common/Tipping/render/FormatCurrency.tsx
+++ src/components/common/Tipping/render/FormatCurrency.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import {Typography, TypographyProps} from '@material-ui/core';
 
-import {BN} from '@polkadot/util';
+import {BN, BN_ZERO} from '@polkadot/util';
 
 import {formatBalance} from 'src/helpers/balance';
 import {BalanceDetail} from 'src/interfaces/balance';
@@ -17,12 +17,13 @@ export const FormatCurrency: React.FC<FormatCurrencyProps> = props => {
   const {value, length = 10, currency, ...restProps} = props;
 
   const amount = formatBalance(value, currency.decimal, length);
+  const displayAmount = value.gt(BN_ZERO) ? (amount > 0 ? amount : '< 0.00000001') : 0;
 
   return (
     <Typography component="span" {...restProps}>
-      {amount}
+      {displayAmount}
       &nbsp;
-      {currency.id}
+      {currency.symbol}
     </Typography>
   );
 };
