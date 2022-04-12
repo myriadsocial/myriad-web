d6ee8bb5c5501c6b80176f1ad993997b85d02a94
Mucktar Pakpahan <mucktar.pakpahan@gmail.com>
2022-04-12 22:32:18 +0700

    MYR-000: add middleware

--- next.config.js
+++ next.config.js
@@ -8,6 +8,9 @@ const {version} = require('./package.json');
 /** @type {import('next').NextConfig} */
 const moduleExports = {
   reactStrictMode: false,
+  experimental: {
+    styledComponent: true,
+  },
   generateBuildId: async () => {
     if (process.env.BUILD_ID) {
       return process.env.BUILD_ID;
