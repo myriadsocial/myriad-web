40b3b7a1429e5044aef70dfb1188332168783da7
Mucktar Pakpahan <mucktar.pakpahan@gmail.com>
2022-04-14 09:17:28 +0700

    MYR-1948: prevent load balance multiple times (#1132)

--- src/lib/services/polkadot-js.ts
+++ src/lib/services/polkadot-js.ts
@@ -290,7 +290,7 @@ export const checkAccountBalance = async (
     free = result.data.free;
     nonce = result.nonce;
 
-    listenToSystemBalanceChange(account, currency, free as u128, callback);
+    listenToSystemBalanceChange(api, account, free as u128, callback);
   } else {
     const result = await api.query.tokens.accounts<OrmlAccountData>(account, {
       TOKEN: currency.id,
@@ -298,7 +298,7 @@ export const checkAccountBalance = async (
 
     free = result.free;
 
-    listenToTokenBalanceChange(account, currency, result.free, callback);
+    listenToTokenBalanceChange(api, account, currency, result.free, callback);
   }
 
   return {
@@ -308,13 +308,11 @@ export const checkAccountBalance = async (
 };
 
 const listenToSystemBalanceChange = async (
+  api: ApiPromise,
   account: string,
-  currency: Currency,
   previousFree: u128,
   callback: (change: BN) => void,
 ) => {
-  const api = await connectToBlockchain(currency.network.rpcURL);
-
   api.query.system.account(account, ({data: {free}, nonce}) => {
     // Calculate the delta
     const change = free.sub(previousFree);
@@ -330,13 +328,12 @@ const listenToSystemBalanceChange = async (
 };
 
 const listenToTokenBalanceChange = async (
+  api: ApiPromise,
   account: string,
   currency: Currency,
   previousFree: Balance,
   callback: (change: BN) => void,
 ) => {
-  const api = await connectToBlockchain(currency.network.rpcURL);
-
   api.query.tokens.accounts(
     account,
     {
