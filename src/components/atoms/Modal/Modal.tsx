23b55d5ca8e9ad1383c790bc8bec81401e49cf06
kriptonhaz <kriptonhaz@gmail.com>
2022-05-16 13:45:07 +0700

    myr-1851: change type subtitle on Modal component

--- src/components/atoms/Modal/Modal.tsx
+++ src/components/atoms/Modal/Modal.tsx
@@ -12,7 +12,7 @@ import {AllignTitle, TitleSize} from './Modal.types';
 
 export type ModalProps = DialogProps & {
   title: string;
-  subtitle?: string;
+  subtitle?: string | React.ReactNode;
   children: React.ReactNode;
   align?: AllignTitle;
   titleSize?: TitleSize;
@@ -43,10 +43,12 @@ export const Modal: React.FC<ModalProps> = props => {
     <Dialog onClose={handleClose} {...otherProps} className={styles.root} disableEnforceFocus>
       <DialogTitle disableTypography className={[styles.title, className].join(' ')}>
         <Typography variant={titleSize === 'small' ? 'h5' : 'h4'}>{title}</Typography>
-        {subtitle && (
-          <Typography variant="subtitle1" className={styles.subtitle}>
+        {subtitle && (typeof subtitle === 'string' || subtitle instanceof String) ? (
+          <Typography variant="subtitle1" display="block" className={styles.subtitle}>
             {subtitle}
           </Typography>
+        ) : (
+          <>{subtitle}</>
         )}
         <IconButton
           color="secondary"
