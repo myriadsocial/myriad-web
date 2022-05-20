d304460b6dc62bd56fa4df5fa0937c6a2828ed0b
imam hermawan <73300321+immhrmwn@users.noreply.github.com>
2022-05-20 14:58:07 +0700

    MYR-000: added translation format date (#1190)

--- src/components/ProfileHeader/render/Website/Website.tsx
+++ src/components/ProfileHeader/render/Website/Website.tsx
@@ -1,6 +1,7 @@
 import {CalendarIcon, GlobeAltIcon} from '@heroicons/react/outline';
 
 import React from 'react';
+import {useSelector} from 'react-redux';
 
 import {Grid, SvgIcon, Link, Typography} from '@material-ui/core';
 
@@ -9,6 +10,9 @@ import {useStyles} from './Webstite.style';
 import ShowIf from 'src/components/common/show-if.component';
 import {formatDate} from 'src/helpers/date';
 import {forceHttps} from 'src/helpers/url';
+import {LanguageSettingType} from 'src/interfaces/setting';
+import {RootState} from 'src/reducers';
+import {ConfigState} from 'src/reducers/config/reducer';
 
 type WebsiteProps = {
   url?: string;
@@ -16,9 +20,20 @@ type WebsiteProps = {
 };
 
 export const Website: React.FC<WebsiteProps> = props => {
-  const style = useStyles();
-
   const {url, joinDate} = props;
+  const style = useStyles();
+  const {settings} = useSelector<RootState, ConfigState>(state => state.configState);
+  const [langValue, setLangValue] = React.useState(settings.language);
+
+  React.useEffect(() => {
+    const langStorage = localStorage.getItem('i18nextLng');
+
+    if (langStorage) {
+      setLangValue(langStorage as LanguageSettingType);
+    } else {
+      setLangValue(settings.language);
+    }
+  }, [settings]);
 
   return (
     <Grid container alignItems="center" className={style.root}>
@@ -43,7 +58,7 @@ export const Website: React.FC<WebsiteProps> = props => {
         viewBox="0 0 24 24"
       />
       <Typography variant="body1" className={style.website}>
-        {formatDate(joinDate)}
+        {formatDate(joinDate, langValue)}
       </Typography>
     </Grid>
   );
