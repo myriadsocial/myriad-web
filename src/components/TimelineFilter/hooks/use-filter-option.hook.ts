0792f5f1edfdab1735e4bd3acc0470618fc60983
immhrmwn <immd.flores@gmail.com>
2022-05-23 14:08:32 +0700

    MYR-2071: added translation in sorting post

--- src/components/TimelineFilter/hooks/use-filter-option.hook.ts
+++ src/components/TimelineFilter/hooks/use-filter-option.hook.ts
@@ -4,6 +4,7 @@ import {MenuOptions} from 'src/components/atoms/DropdownMenu';
 import {TabListItem} from 'src/components/atoms/TabList';
 import {TimelineOrderType, TimelineType, PostOriginType} from 'src/interfaces/timeline';
 import {SortType} from 'src/lib/api/interfaces/pagination-params.interface';
+import i18n from 'src/locale';
 import {RootState} from 'src/reducers';
 import {ProfileState} from 'src/reducers/profile/reducer';
 import {UserState} from 'src/reducers/user/reducer';
@@ -16,63 +17,66 @@ export const useFilterOption = () => {
   const originFilterOptions: MenuOptions<PostOriginType>[] = [
     {
       id: 'all',
-      title: 'All Posts',
+      title: i18n.t('Post_Sorting.Origin_Filter.All'),
     },
     {
       id: 'myriad',
-      title: !anonymous && people?.id == user?.id ? 'Myriad Posts' : `${people?.name} Post`,
+      title:
+        !anonymous && people?.id == user?.id
+          ? i18n.t('Post_Sorting.Origin_Filter.Myriad', {name: 'Myriad'})
+          : i18n.t('Post_Sorting.Origin_Filter.Myriad', {name: people?.name}),
     },
     {
       id: 'imported',
-      title: 'Imported Posts',
+      title: i18n.t('Post_Sorting.Origin_Filter.Imported'),
     },
   ];
 
   const typeFilterOptions: TabListItem<TimelineType>[] = [
     {
       id: TimelineType.ALL,
-      title: 'All',
+      title: i18n.t('Post_Sorting.Type_Filter.All'),
     },
     {
       id: TimelineType.FRIEND,
-      title: 'Friend',
+      title: i18n.t('Post_Sorting.Type_Filter.Friend'),
     },
     {
       id: TimelineType.TRENDING,
-      title: 'Trending',
+      title: i18n.t('Post_Sorting.Type_Filter.Trending'),
     },
     {
       id: TimelineType.EXPERIENCE,
-      title: 'Experience',
+      title: i18n.t('Post_Sorting.Type_Filter.Experience'),
     },
   ];
 
   const orderOptions: MenuOptions<TimelineOrderType>[] = [
     {
       id: TimelineOrderType.LATEST,
-      title: 'Latest',
+      title: i18n.t('Post_Sorting.Order.Latest'),
     },
     {
       id: TimelineOrderType.POPULAR,
-      title: 'Popularity',
+      title: i18n.t('Post_Sorting.Order.Popular'),
     },
     {
       id: TimelineOrderType.UPVOTE,
-      title: 'Most liked',
+      title: i18n.t('Post_Sorting.Order.Liked'),
     },
     {
       id: TimelineOrderType.COMMENT,
-      title: 'Most commented',
+      title: i18n.t('Post_Sorting.Order.Comment'),
     },
     {
       id: TimelineOrderType.TIP,
-      title: 'Most Tipped',
+      title: i18n.t('Post_Sorting.Order.Tip'),
     },
   ];
 
   const sortOptions: MenuOptions<SortType>[] = [
-    {id: 'DESC', title: 'Latest'},
-    {id: 'ASC', title: 'Oldest'},
+    {id: 'DESC', title: i18n.t('Post_Sorting.Sort.Latest')},
+    {id: 'ASC', title: i18n.t('Post_Sorting.Sort.Oldest')},
   ];
 
   return {
