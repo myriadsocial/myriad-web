466223e1b42fe2876415ef80e77d70024347983f
kriptonhaz <kriptonhaz@gmail.com>
2022-05-23 15:08:28 +0700

    myr-000: fix translation navbar

--- src/components/atoms/TopNavbar/TopNavbar.tsx
+++ src/components/atoms/TopNavbar/TopNavbar.tsx
@@ -13,9 +13,6 @@ import Typography from '@material-ui/core/Typography';
 import {TopNavbarProps} from './TopNavbar.interfaces';
 import {useStyles} from './TopNavbar.styles';
 
-import {SectionTitle} from 'src/components/atoms/TopNavbar';
-import i18n from 'src/locale';
-
 const MenuDrawerComponent = dynamic(() => import('src/components/Mobile/MenuDrawer/MenuDrawer'), {
   ssr: false,
 });
@@ -46,58 +43,6 @@ export const TopNavbarComponent: React.FC<TopNavbarProps> = props => {
     }
   };
 
-  const convertToLocale = (sectionTitle: SectionTitle) => {
-    switch (sectionTitle) {
-      case SectionTitle.FRIENDS: {
-        return i18n.t('TopNavbar.Title.Friends');
-      }
-
-      case SectionTitle.SOCIAL_MEDIA: {
-        return i18n.t('TopNavbar.Title.Social_Media');
-      }
-
-      case SectionTitle.WALLET: {
-        return i18n.t('TopNavbar.Title.Wallet');
-      }
-
-      case SectionTitle.EXPERIENCE: {
-        return i18n.t('TopNavbar.Title.Experience');
-      }
-
-      case SectionTitle.PROFILE: {
-        return i18n.t('TopNavbar.Title.Profile');
-      }
-
-      case SectionTitle.SETTINGS: {
-        return i18n.t('TopNavbar.Title.Settings');
-      }
-
-      case SectionTitle.NOTIFICATION: {
-        return i18n.t('TopNavbar.Title.Notification');
-      }
-
-      case SectionTitle.NFT: {
-        return i18n.t('TopNavbar.Title.NFT');
-      }
-
-      case SectionTitle.TIMELINE: {
-        return i18n.t('TopNavbar.Title.Timeline');
-      }
-
-      case SectionTitle.SOCIAL_TOKEN: {
-        return i18n.t('TopNavbar.Title.Social_Token');
-      }
-
-      case SectionTitle.TRENDS: {
-        return i18n.t('TopNavbar.Title.Trends');
-      }
-
-      default: {
-        return '';
-      }
-    }
-  };
-
   return (
     <Paper className={classes.root}>
       <IconButton
@@ -115,7 +60,7 @@ export const TopNavbarComponent: React.FC<TopNavbarProps> = props => {
 
       <Grid container direction={reverse ? 'column-reverse' : 'column'}>
         <Typography className={classes.sectionTitle} color="primary">
-          {convertToLocale(sectionTitle as SectionTitle)}
+          {sectionTitle}
         </Typography>
         <Typography className={classes.description}>{description}</Typography>
       </Grid>
