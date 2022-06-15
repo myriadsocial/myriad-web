d8036301fee2a929f3e1162d04eb0f560efe4e87
Mucktar Pakpahan <mucktar.pakpahan@gmail.com>
2022-06-15 15:11:36 +0700

    MYR-1733: reset api auth header for anonyous user

--- src/lib/api/base.ts
+++ src/lib/api/base.ts
@@ -12,36 +12,41 @@ let API: AxiosInstance;
 
 const {publicRuntimeConfig} = getConfig();
 
+const setupAPIClient = () => {
+  API = axios.create({
+    baseURL: publicRuntimeConfig.appAuthURL + '/api',
+  });
+
+  API.interceptors.response.use(
+    response => {
+      return response;
+    },
+    error => {
+      if (error.response) {
+        console.error(
+          JSON.stringify({
+            name: '[myriad-api][error]',
+            detail: error.response?.data,
+          }),
+        );
+      } else {
+        console.error('[error]', error);
+      }
+
+      Sentry.captureException(error);
+
+      return Promise.reject(error);
+    },
+  );
+};
+
 export const initialize = (params?: MyriadAPIParams): AxiosInstance => {
   // always create new axios instance when cookie changed
-  if (params?.cookie || !API) {
-    API = axios.create({
-      baseURL: publicRuntimeConfig.appAuthURL + '/api',
-    });
-
-    API.interceptors.response.use(
-      response => {
-        return response;
-      },
-      error => {
-        if (error.response) {
-          console.error(
-            JSON.stringify({
-              name: '[myriad-api][error]',
-              detail: error.response?.data,
-            }),
-          );
-        } else {
-          console.error('[error]', error);
-        }
-
-        Sentry.captureException(error);
-
-        return Promise.reject(error);
-      },
-    );
+  if (params?.cookie || !API || typeof window === 'undefined') {
+    setupAPIClient();
   }
 
+  // add auth header
   if (params?.cookie) {
     API.interceptors.request.use(config => {
       config.headers = {
