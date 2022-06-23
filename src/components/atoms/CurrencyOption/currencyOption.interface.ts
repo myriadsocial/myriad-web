1d319aa7a7f5f63f04ef649866990c010b5adbe5
abdulhakim2902 <hakim@blocksphere.id>
2022-06-23 15:15:16 +0700

    fix: removed field from currency option props

--- src/components/atoms/CurrencyOption/currencyOption.interface.ts
+++ src/components/atoms/CurrencyOption/currencyOption.interface.ts
@@ -3,5 +3,4 @@ import {BalanceDetail} from 'src/interfaces/balance';
 export type CurrencyOptionProps = {
   balanceDetails: BalanceDetail[];
   onSelect: (selected: BalanceDetail) => void;
-  isOtherTippingCurrencyDisabled: boolean;
 };
