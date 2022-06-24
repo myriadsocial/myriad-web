cbd080a85de6a1420b302bd07f5e47b9292e0721
kriptonhaz <31912747+kriptonhaz@users.noreply.github.com>
2022-06-24 09:23:52 +0700

    add check new posts on timeline (#1303)
    * myr-2187: add action checkNewTimeline
    
    * myr-2187: add use scroll hook
    
    * myr-2187: add fetch interval timeline every 2 minutes
    
    * myr-2187: set style cursor pointer on load more post

---
+++ src/hooks/use-scroll.hook.ts
@@ -0,0 +1,30 @@
+import {useEffect, useState} from 'react';
+
+import {debounce} from 'lodash';
+
+export const useScroll = () => {
+  const [isScroll, setIsScroll] = useState<boolean>(false);
+  const [scrollPosition, setScrollPosition] = useState<number>(0);
+
+  useEffect(() => {
+    window.addEventListener('scroll', listenToScroll);
+    return () => {
+      window.removeEventListener('scroll', listenToScroll);
+    };
+  }, []);
+
+  const listenToScroll = debounce(() => {
+    const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+    const scrolled = winScroll / height;
+    setScrollPosition(scrolled);
+    setIsScroll(scrolled > 0);
+  }, 200);
+
+  return {
+    isScroll,
+    scrollPosition,
+  };
+};
