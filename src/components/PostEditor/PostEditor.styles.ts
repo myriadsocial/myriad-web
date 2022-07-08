43a4e2cf2ba70909e367a5d46e23bb286dfc7178
Mucktar Pakpahan <mucktar.pakpahan@gmail.com>
2022-07-08 13:42:24 +0700

    Update post editor style (#1348)
    MYR-2281: parse filter post on topic page

--- src/components/PostEditor/PostEditor.styles.ts
+++ src/components/PostEditor/PostEditor.styles.ts
@@ -8,15 +8,12 @@ export const useStyles = makeStyles((theme: Theme) =>
       border: '1px solid #F6F6F6',
       borderRadius: 10,
 
-      [theme.breakpoints.down('xs')]: {
-        width: '100%',
-      },
-
       '& .MuiSvgIcon-root': {
         fill: 'currentColor',
       },
 
       [theme.breakpoints.down('xs')]: {
+        width: '100%',
         '& .slate-ToolbarButton:not(.slate-ToolbarButton-active):hover': {
           color: 'inherit!important',
         },
