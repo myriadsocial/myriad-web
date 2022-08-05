c9ba3beca58ea3dc75e6807220786711e58dcb9e
kriptonhaz <kriptonhaz@gmail.com>
2022-08-05 17:14:26 +0700

    myr-1998: fix export module jest config

--- jest.config.js
+++ jest.config.js
@@ -22,4 +22,4 @@ const customJestConfig = {
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-export default createJestConfig(customJestConfig);
+module.exports = createJestConfig(customJestConfig);
