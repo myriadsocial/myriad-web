fa26dbad642ee61b3e31f1bff078c9ff12fe73eb
kriptonhaz <kriptonhaz@gmail.com>
2022-08-05 16:55:12 +0700

    myr-1998: fix import module nextJest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
-import nextJest from 'next/jest';
+/* eslint-disable @typescript-eslint/no-var-requires */
+const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
