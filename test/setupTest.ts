ff687d0020a6863ea0c126c7ea5d599e33fb3e83
kriptonhaz <kriptonhaz@gmail.com>
2022-08-12 16:23:35 +0700

    myr-2431: fix setup test jest

--- test/setupTest.ts
+++ test/setupTest.ts
@@ -1,3 +1,12 @@
-import '@testing-library/jest-dom';
+// Optional: configure or set up a testing framework before each test.
+// If you delete this file, remove `setupFilesAfterEnv` from `jest.config.js`
+// Used for __tests__/testing-library.js
+// Learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom/extend-expect';
 
-export {};
+import {setConfig} from 'next/config';
+
+import config from '../next.config';
+
+// Make sure you can use "publicRuntimeConfig" within tests.
+setConfig(config);
