cb72e82d38b399d49206186325f0a23c4ade4bea
abdulhakim2902 <hakim@blocksphere.id>
2022-08-18 09:27:02 +0700

    fix: added new mynearwallet icon

--- src/components/atoms/Icons/Wallet.tsx
+++ src/components/atoms/Icons/Wallet.tsx
@@ -5,12 +5,17 @@ import {SvgIcon, SvgIconProps} from '@material-ui/core';
 import Polkadot24 from 'src/images/network/Polkadot24.svg';
 import CoinbaseDisabled from 'src/images/wallet/coinbase-disabled.svg';
 import MetamaskDisabled from 'src/images/wallet/metamask-disabled.svg';
+import MyNearWallet from 'src/images/wallet/my-near-wallet.svg';
 import SenderWalletDisabled from 'src/images/wallet/sender-wallet-disabled.svg';
 import SenderWallet from 'src/images/wallet/sender-wallet.svg';
 import TrustDisabled from 'src/images/wallet/trust-disabled.svg';
 
 export const PolkadotWalletIcon: React.FC<SvgIconProps> = props => (
   <SvgIcon component={Polkadot24} viewBox="0 0 32 32" {...props} />
+);
+
+export const MyNearWalletIcon: React.FC<SvgIconProps> = props => (
+  <SvgIcon component={MyNearWallet} viewBox="0 0 32 32" {...props} />
 );
 
 export const SenderWalletIcon: React.FC<SvgIconProps> = props => (
