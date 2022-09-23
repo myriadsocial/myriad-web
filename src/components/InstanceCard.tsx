fead60fef204a4d78e6de6061832d3c8a9982af8
ical10 <mochhusnir@gmail.com>
2022-09-23 17:15:36 +0700

    feat: created ListServer dialog with populated data

---
+++ src/components/InstanceCard.tsx
@@ -0,0 +1,82 @@
+import {useState} from 'react';
+
+import Image from 'next/image';
+
+import {
+  Box,
+  Typography,
+  Card,
+  CardMedia,
+  CardActionArea,
+  CardContent,
+  ListItem,
+} from '@material-ui/core';
+import {alpha, createStyles, makeStyles} from '@material-ui/core/styles';
+
+import {ServerListProps} from 'src/interfaces/server-list';
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    root: {
+      '&:hover': {
+        backgroundColor: alpha('#FFC857', 0.15),
+        '& .MuiCardActionArea-focusHighlight': {
+          opacity: 0,
+        },
+      },
+    },
+  }),
+);
+
+type InstanceCardProps = {
+  server: ServerListProps;
+};
+
+const InstanceCard = ({server}: InstanceCardProps) => {
+  const styles = useStyles();
+
+  const text = server.detail?.description ?? 'Empty description';
+
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpand = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <ListItem key={server.id}>
+      <Card className={styles.root}>
+        <CardActionArea style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+          <CardMedia style={{padding: 16}}>
+            {server.detail?.serverImageURL && (
+              <Image
+                alt={server.detail?.id}
+                src={server.detail?.serverImageURL ?? ''}
+                placeholder="empty"
+                height={80}
+                width={80}
+              />
+            )}
+          </CardMedia>
+          <CardContent>
+            <Typography style={{fontWeight: 600, fontSize: 16}}>{server.detail?.name}</Typography>
+            <Typography style={{maxWidth: 368, fontWeight: 400, fontSize: 12}}>
+              {text.split(' ').length > 10 && !expanded
+                ? `${text.split(' ').slice(0, 10).join(' ')}...`
+                : text}
+            </Typography>
+            <Box
+              fontSize={10}
+              fontWeight="fontWeightBold"
+              style={{color: '#6E3FC3', textAlign: 'end'}}
+              onClick={handleExpand}>
+              {!expanded ? 'See more' : 'See less'}
+            </Box>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </ListItem>
+  );
+};
+
+export default InstanceCard;
--- src/components/SelectServer.tsx
+++ src/components/SelectServer.tsx
@@ -3,6 +3,10 @@ import {ChevronDownIcon} from '@heroicons/react/outline';
 import {useState, useMemo} from 'react';
 
 import {
+  Typography,
+  Card,
+  CardActionArea,
+  CardContent,
   Box,
   ButtonBase,
   SvgIcon,
@@ -12,10 +16,13 @@ import {
   List,
   ListItem,
 } from '@material-ui/core';
+import {Skeleton} from '@material-ui/lab';
 
+import InstanceCard from './InstanceCard';
 import useStyles from './SelectServer.styles';
 
 import useMyriadInstance from 'src/components/common/Blockchain/use-myriad-instance.hooks';
+import ShowIf from 'src/components/common/show-if.component';
 import {useInstances} from 'src/hooks/use-instances.hooks';
 import MyriadCircle from 'src/images/Icons/myriad-circle.svg';
 
@@ -67,12 +74,33 @@ const SelectServer = () => {
       </div>
 
       <Dialog open={open} onClose={handleClose} aria-labelledby="list-server-dialog">
-        <DialogTitle id="list-server-dialog-title">Select Instance</DialogTitle>
+        <DialogTitle id="list-server-dialog-title">
+          <Typography style={{fontWeight: 600, fontSize: 16, textAlign: 'center'}}>
+            Select Instance
+          </Typography>
+        </DialogTitle>
         <DialogContent>
           <List>
-            {servers.map(server => (
-              <ListItem key={server.id}>{server.id}</ListItem>
-            ))}
+            <ShowIf condition={servers.length === 0}>
+              <ListItem>
+                <Card>
+                  <CardActionArea>
+                    <CardContent>
+                      <Skeleton />
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              </ListItem>
+            </ShowIf>
+            <ShowIf condition={servers.length > 0}>
+              {servers
+                .filter(server => server.detail)
+                .map(server => (
+                  <ListItem key={server.id}>
+                    <InstanceCard server={server} />
+                  </ListItem>
+                ))}
+            </ShowIf>
           </List>
         </DialogContent>
       </Dialog>
