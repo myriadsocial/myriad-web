2d2049853ce97dbce35dc9a39605a4d76c327f78
ical10 <mochhusnir@gmail.com>
2022-10-17 17:09:25 +0700

    chore: undone delete file

--- test/test-utils.tsx
+++ test/test-utils.tsx
@@ -1,18 +1,18 @@
-import React, { PropsWithChildren } from 'react'
-import { render } from '@testing-library/react'
-import type { RenderOptions } from '@testing-library/react'
-import type { PreloadedState } from '@reduxjs/toolkit'
-import { Provider } from 'react-redux'
+import type {PreloadedState} from '@reduxjs/toolkit';
+import {render} from '@testing-library/react';
+import type {RenderOptions} from '@testing-library/react';
 
-import type { AppStore, RootState} from 'src/reducers';
+import React, {PropsWithChildren} from 'react';
+import {Provider} from 'react-redux';
+
+import type {AppStore, RootState} from 'src/reducers';
 import {setupStore} from 'src/reducers';
-
 
 // This type interface extends the default options for render from RTL, as well
 // as allows the user to specify other things such as initialState, store.
 interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
-  preloadedState?: PreloadedState<RootState>
-  store?: AppStore
+  preloadedState?: PreloadedState<RootState>;
+  store?: AppStore;
 }
 
 export function renderWithProviders(
@@ -22,12 +22,12 @@ export function renderWithProviders(
     // Automatically create a store instance if no store was passed in
     store = setupStore(preloadedState),
     ...renderOptions
-  }: ExtendedRenderOptions = {}
+  }: ExtendedRenderOptions = {},
 ) {
-  function Wrapper({ children }: PropsWithChildren<{}>): JSX.Element {
-    return <Provider store={store}>{children}</Provider>
+  function Wrapper({children}: PropsWithChildren<{}>): JSX.Element {
+    return <Provider store={store}>{children}</Provider>;
   }
 
   // Return an object with the store and all of RTL's query functions
-  return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) }
+  return {store, ...render(ui, {wrapper: Wrapper, ...renderOptions})};
 }
