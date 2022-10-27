e9e25ca35beb9da44c61f49a2b9ae6c4234f67e6
ical10 <mochhusnir@gmail.com>
2022-10-27 14:31:18 +0700

    feat: added LoginByEmail as a new Route in Login

---
+++ src/components/Login/render/Email/LoginByEmail.tsx
@@ -0,0 +1,15 @@
+import {Typography} from '@material-ui/core';
+
+const LoginByEmail = () => {
+  return (
+    <div>
+      <Typography>Sign-in Methods</Typography>
+      <Typography>Subtitle: Choose your sign-in methods</Typography>
+      <div>
+        <div>this is login by email card</div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginByEmail;
