33fc053ccc046c21769eeae96fe3791f2c56ae8b
Rofi <rofisudyono@gmail.com>
2022-10-27 10:00:05 +0700

    fix: navigate to web 3 login (#1504)
    fix navigate to web 3 login

--- src/components/Login/render/SignInMethod/SigninMethod.tsx
+++ src/components/Login/render/SignInMethod/SigninMethod.tsx
@@ -1,3 +1,6 @@
+import {useState} from 'react';
+import {useNavigate} from 'react-router';
+
 import {IconButton, Tooltip, Typography, withStyles} from '@material-ui/core';
 
 import {useStyles} from './SigninMethod.style';
@@ -5,9 +8,20 @@ import {useStyles} from './SigninMethod.style';
 import {IcInfo, LoginWeb2, LoginWeb3} from 'src/images/illustration';
 
 export default function SigninMethod() {
+  const navigate = useNavigate();
   const styles = useStyles();
+  const [methodSelected, setMethodSelected] = useState<string>('');
 
-  const CardSign = ({image, title, desc, onClick, tooltip}) => {
+  const handleSelected = ({method}: {method: string}) => {
+    setMethodSelected(method);
+    if (method === 'web2') {
+      navigate('/');
+    } else {
+      navigate('/options');
+    }
+  };
+
+  const CardSign = ({image, title, desc, onClick, tooltip, selected}) => {
     const BlueOnGreenTooltip = withStyles({
       tooltip: {
         color: 'black',
@@ -16,8 +30,11 @@ export default function SigninMethod() {
     })(Tooltip);
     return (
       <div className={styles.wrapperCard}>
-        <div className={styles.card}>
-          <button onClick={onClick} style={{border: 0, background: 'white'}}>
+        <div className={selected ? styles.cardSelected : styles.card}>
+          <button
+            type="button"
+            onClick={onClick}
+            style={{border: 0, background: 'white', cursor: 'pointer'}}>
             {image}
             <Typography className={styles.textMethod}>{title}</Typography>
           </button>
@@ -40,18 +57,20 @@ export default function SigninMethod() {
       <Typography className={styles.textSubtitle}>Subtitle: Choose your sign-in methods</Typography>
       <div className={styles.wrapperCards}>
         <CardSign
+          selected={methodSelected === 'web3'}
           title={'Web 3.0'}
           desc="Sign in via Web 3.0"
           image={<LoginWeb3 />}
-          onClick={() => undefined}
           tooltip="Sign-in with a crypto wallet to unlock all the features of Myriad-Social."
+          onClick={() => handleSelected({method: 'web3'})}
         />
-        <div style={{fontSize: 14, color: 'black', marginLeft: 12, marginRight: 12}}>or</div>
+        <div className={styles.textOr}>or</div>
         <CardSign
+          selected={methodSelected === 'web2'}
           title={'Web 2.0'}
           desc="Sign in via Email"
           image={<LoginWeb2 />}
-          onClick={() => console.log('web 2')}
+          onClick={() => handleSelected({method: 'web2'})}
           tooltip="Signing-in via email lets you use many of Myriad’s features. You can get the real deal later by adding a crypto wallet address in your wallet settings. We will not use your email for commercial purposes."
         />
       </div>
