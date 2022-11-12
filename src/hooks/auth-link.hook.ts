4ae1d52d7e2e0100aba2c252e1604fd331985596
ical10 <mochhusnir@gmail.com>
2022-11-12 16:32:00 +0700

    fix: removed comments

--- src/hooks/auth-link.hook.ts
+++ src/hooks/auth-link.hook.ts
@@ -6,10 +6,6 @@ import getConfig from 'next/config';
 import * as AuthLinkAPI from 'src/lib/api/auth-link';
 import {SignupWithEmailProps} from 'src/lib/api/auth-link';
 
-//3. When clicking on link, parse the query on /login
-//4. Create api call for POST /login/otp with useEffect when parsing query on /login
-//5. Call signIn with otp and email, check if it exists and no signature is required, and then call the api call in #4
-
 const {publicRuntimeConfig} = getConfig();
 
 export const useAuthLinkHook = () => {
