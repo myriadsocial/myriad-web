b83c3df5c2532f35c010606e5f0cd463d254b121
Rofi <rofisudyono@gmail.com>
2022-11-17 06:51:21 +0700

    implement new empty state screen friend menu

--- src/components/atoms/Empty/Empty.tsx
+++ src/components/atoms/Empty/Empty.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Image from 'next/image';
+
 import {Typography} from '@material-ui/core';
 
 import {useStyles} from './Empty.styles';
@@ -9,20 +11,45 @@ export type EmptyProps = {
   subtitle?: string;
   margin?: boolean;
   height?: boolean;
+  withImage?: boolean;
+  image?: string;
 };
 
 export const Empty: React.FC<EmptyProps> = props => {
-  const {title, subtitle, children} = props;
+  const {title, subtitle, children, withImage, image} = props;
   const styles = useStyles({...props});
 
   return (
     <div className={styles.root}>
-      <Typography variant="h4" className={styles.title} component="p">
-        {title}
-      </Typography>
-      <Typography variant="body1" className={styles.subtitle} color="textSecondary" component="p">
-        {subtitle}
-      </Typography>
+      {withImage ? (
+        <>
+          <Image src={image} alt="" height={151} width={218} />
+          <Typography variant="h4" className={styles.title} component="p">
+            {title}
+          </Typography>
+          <Typography
+            variant="body1"
+            className={styles.subtitle}
+            color="textSecondary"
+            component="p">
+            {subtitle}
+          </Typography>
+        </>
+      ) : (
+        <>
+          <Typography variant="h4" className={styles.title} component="p">
+            {title}
+          </Typography>
+          <Typography
+            variant="body1"
+            className={styles.subtitle}
+            color="textSecondary"
+            component="p">
+            {subtitle}
+          </Typography>
+        </>
+      )}
+
       {children}
     </div>
   );
