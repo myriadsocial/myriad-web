6f181273fdf911dfc709eedb8c6eed49d676e120
Labib Imam Hidayat <labib.imam.hidayat15@gmail.com>
2022-12-02 20:53:07 +0700

    feat: add pwa (#1591)
    * add next pwa
    
    * add assets and pwa support
    
    * delete
    
    * modify next js config for pwa
    
    * remove console.log
    
    * update prettier ignore

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,6 +19,15 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;400;500;600;700&display=swap"
             rel="stylesheet"
           />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="application-name" content="Myriad" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff"></meta>
+          <meta name="apple-mobile-web-app-title" content="Myriad" />
         </Head>
         <body>
           <Main />
