5b1b5113f6ebb6e7e46f8964a3019e9a428fcd00
Agustinus Theodorus <agustinustheoo@gmail.com>
2023-09-30 08:22:01 +0700

    refactor: myriad network naming

--- src/helpers/wallet.ts
+++ src/helpers/wallet.ts
@@ -39,6 +39,14 @@ export const formatNetworkTitle = (
     case NetworkIdEnum.NEAR:
       return id.toUpperCase();
     default:
+      if (id.includes('_')) {
+        return id
+          .split('_')
+          .map(
+            word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase(),
+          )
+          .join(' ');
+      }
       return capitalize(id ?? 'unknown');
   }
 };
