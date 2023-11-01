3f2dd4de97553c2afb2c0b613d096503f52106a6
RXRD <lilixinxin@protonmail.com>
2023-11-01 14:22:42 +0700

    Update SharingSettings.tsx

--- src/components/Settings/SharingSettings.tsx
+++ src/components/Settings/SharingSettings.tsx
@@ -1,45 +1,25 @@
 /* eslint-disable jsx-a11y/interactive-supports-focus */
 
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import { XIcon } from '@heroicons/react/outline';
 
-import { useState, useEffect, useRef } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-
-import getConfig from 'next/config';
-import { useRouter } from 'next/router';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
 
 import {
   Button,
   TextField,
   Paper,
-  IconButton,
-  SvgIcon,
   Grid,
   CircularProgress,
   Typography,
 } from '@material-ui/core';
 
 import { useStyles } from './Settings.styles';
-
-import { PromptComponent } from 'src/components/atoms/Prompt/prompt.component';
-import { useUserHook } from 'src/hooks/use-user.hook';
 import i18n from 'src/locale';
 import { RootState } from 'src/reducers';
-import {
-  sendVerificationEmail,
-  updateEmail,
-  deleteEmail,
-} from 'src/reducers/config/actions';
-import validator from 'validator';
-
-const { publicRuntimeConfig } = getConfig();
-const countDownTime = 60;
 
 const SharingSetting = () => {
   const styles = useStyles();
-  const { query, push } = useRouter();
-  const dispatch = useDispatch();
   const loading = useSelector<RootState>(
     ({ configState: { loading } }) => loading,
   );
